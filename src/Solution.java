342b92111977693afffb0ecf3ecda40bc6242b2f
mc227 <markcostales.mark@gmail.com>
2022-02-16 21:01:19 -0500

    task 1627

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,139 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Oh Hi Mark");
+    public static void main(String[] args) throws InterruptedException {
+        OnlineGame onlineGame = new OnlineGame();
+        onlineGame.start();
+    }
+
+    public static class OnlineGame extends Thread {
+        public static volatile boolean isWinnerFound = false;
+
+        public static List<String> actions = new ArrayList<>();
+
+        static {
+            actions.add("Start game");
+            actions.add("Gather resources");
+            actions.add("Grow economy");
+            actions.add("Kill enemies");
+        }
+
+        protected Gamer gamer1 = new Gamer("Smith", 3);
+        protected Gamer gamer2 = new Gamer("Jones", 1);
+        protected Gamer gamer3 = new Gamer("Gates", 5);
+
+        public void run() {
+            gamer1.start();
+            gamer2.start();
+            gamer3.start();
+
+            while (!isWinnerFound) {
+            }
+            gamer1.interrupt();
+            gamer2.interrupt();
+            gamer3.interrupt();
+        }
+    }
+
+    public static class Gamer extends Thread {
+        private int rating;
+
+        public Gamer(String name, int rating) {
+            super(name);
+            this.rating = rating;
+        }
+
+        @Override
+        public void run() {
+            //write your code here
+            for(String action: OnlineGame.actions){
+                try {
+                    Thread.sleep((long) 1000/rating);
+                    System.out.println(getName()+":"+action);
+                } catch (InterruptedException e) {
+                    System.out.println(getName()+":lost");
+                    return;
+                }
+            }
+            if (OnlineGame.isWinnerFound == false) {
+                OnlineGame.isWinnerFound = true;
+                System.out.println(getName()+":won!");
+            }
+        }
     }
 }
+
+
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Solution {
+//    public static void main(String[] args) throws InterruptedException {
+//        OnlineGame onlineGame = new OnlineGame();
+//        onlineGame.start();
+//    }
+//
+//    public static class OnlineGame extends Thread {
+//        public static volatile boolean isWinnerFound = false;
+//
+//        public static List<String> actions = new ArrayList<>();
+//
+//        static {
+//            actions.add("Start game");
+//            actions.add("Gather resources");
+//            actions.add("Grow economy");
+//            actions.add("Kill enemies");
+//        }
+//
+//        protected Gamer gamer1 = new Gamer("Smith", 3);
+////        protected Gamer gamer2 = new Gamer("Jones", 1);
+////        protected Gamer gamer3 = new Gamer("Gates", 5);
+//
+//        public void run() {
+//            gamer1.start();
+////            gamer2.start();
+////            gamer3.start();
+//
+//            while (!isWinnerFound) {
+//            }
+//            gamer1.interrupt();
+////            gamer2.interrupt();
+////            gamer3.interrupt();
+//        }
+//    }
+//
+//    public static class Gamer extends Thread {
+//        private int rating;
+//
+//        public Gamer(String name, int rating) {
+//            super(name);
+//            this.rating = rating;
+//        }
+//
+//        @Override
+//        public void run() {
+//            //write your code here
+//            for(String action: OnlineGame.actions){
+//                try {
+//                    Thread.sleep((long) 1000/rating);
+//                    System.out.println(getName()+":"+action);
+//                } catch (InterruptedException e) {
+//                    return;
+//                }
+//            }
+//            OnlineGame.isWinnerFound = true;
+//            System.out.println(getName()+":won!");
+//        }
+//    }
+//}
+//
+
+//public class Solution {
+//    public static void main(String[] args) {
+//        System.out.println("Oh Hi Mark");
+//    }
+//}
 
 //import java.io.BufferedReader;
 //import java.io.IOException;
