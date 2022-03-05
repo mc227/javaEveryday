fac240f6ad0f7e5f0f36a6ca229fe32a943ca370
mc227 <markcostales.mark@gmail.com>
2022-03-05 07:59:36 -0500

    RR h

---
+++ src/RR.java
@@ -0,0 +1,10 @@
+/*clarifying my understanding of the super key word
+* 030520220757 why do i see super(name) writtien in places
+* specifically in task1606*/
+
+// Program to illustrate this keyword
+// is used to refer current class
+public class RR {
+    // instance variable
+    //* stopped here 030520220759MC
+}
