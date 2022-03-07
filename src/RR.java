9925ec3e409c197cd7aa0c08852340a142af8f43
mc227 <markcostales.mark@gmail.com>
2022-03-06 20:44:05 -0500

    running geeks for geeks code

--- src/RR.java
+++ src/RR.java
@@ -7,4 +7,24 @@
 public class RR {
     // instance variable
     //* stopped here 030520220759MC
+    // instance variable
+    int a = 10;
+
+    // static variable
+    static int b = 20;
+
+    void GFG() {
+        this.a = 100;
+        System.out.println(a);
+
+        this.b = 600;
+        System.out.println(b);
+        System.out.println("Oh Hi Mark");
+        System.out.println(this);
+    }
+
+    public static void main(String[] args) {
+//        this.a = 700;
+        new RR().GFG();
+    }
 }
