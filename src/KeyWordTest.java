f4379b225834b3948d3f69be0684df08eef5568a
mc227 <markcostales.mark@gmail.com>
2022-03-21 21:31:52 -0400

    I fixed the manifest issue in the gradle tasks

--- src/KeyWordTest.java
+++
@@ -1,9 +0,0 @@
-public class KeyWordTest {
-    private String name;
-    private int age;
-
-    public KeyWordTest(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-}
