af549a090a157a2c21ba7626647bdd418a477992
mc227 <markcostales.mark@gmail.com>
2022-03-26 05:25:54 -0400

    task1618

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,56 +1,15 @@
-
-/*
-Countdown at the races
-
-*/
-
 public class Solution {
-    public static volatile int numSeconds = 3;
-//    public static volatile int numSeconds = 4;
-
-    public static void main(String[] args) throws InterruptedException {
-        RacingClock clock = new RacingClock();
-        //write your code here
-        Thread.sleep(3500);
-        clock.interrupt();
+    public static void main(String[] args) throws InterruptedException{
+        TestThread testThread = new TestThread();
+        testThread.start();
+        testThread.interrupt();
     }
 
-    public static class RacingClock extends Thread {
-        public RacingClock() {
-            start();
-        }
-
+    public static class TestThread extends Thread {
+        @Override
         public void run() {
-            //write your code here
-            if (numSeconds < 3.5) {
-                while(numSeconds >=0) {
-                    try {
-                        Thread.sleep(1000);
-                        if(numSeconds ==0) {
-                            System.out.print("Go!");
-                        } else {
-                            System.out.print(numSeconds+" ");
-                        }
-                        numSeconds--;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                while(numSeconds >=0) {
-                    try {
-                        Thread.sleep(1000);
-                        if(numSeconds ==0) {
-                            System.out.print("Interrupted!");
-                        } else {
-                            System.out.print(numSeconds+" ");
-                        }
-                        numSeconds--;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+            super.run();
+            System.out.println("Oh Hi Mark");
         }
     }
 }
