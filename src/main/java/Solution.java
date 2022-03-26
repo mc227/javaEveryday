d0a17fa7083ec1989f64d1aad2a7d0b1a996cae1
mc227 <markcostales.mark@gmail.com>
2022-03-26 05:09:23 -0400

    next

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Deprecation warning is bothering me\nI feel like I have to fix it");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.start();
+
+        bufferedReader.readLine();
+        stopwatch.interrupt();
+        bufferedReader.close();
+    }
+
+    public static class Stopwatch extends Thread {
+        private int seconds;
+
+        public void run() {
+            try {
+                while (true) {
+                    Thread.sleep(1000);
+                    seconds++;
+                }
+            } catch (InterruptedException e) {
+                System.out.println(seconds);
+            }
+        }
     }
 }
