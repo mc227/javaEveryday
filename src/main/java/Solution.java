58995f15e251e96064ad632ed9f43fada0117631
mc227 <markcostales.mark@gmail.com>
2022-03-27 16:24:54 -0400

    no interrupt, no dice

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,15 +1,57 @@
+
 public class Solution {
-    public static void main(String[] args) throws InterruptedException{
-        TestThread testThread = new TestThread();
-        testThread.start();
-        testThread.interrupt();
+    private static boolean isCancel;
+    //    private boolean isCancel = false;
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterrupt();
     }
 
-    public static class TestThread extends Thread {
-        @Override
+    public static void ourInterrupt() {
+        isCancel = true;
+
+    }
+
+    public static class TestThread implements Runnable {
         public void run() {
-            super.run();
-            System.out.println("Oh Hi Mark");
+            while (!isCancel) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
         }
     }
 }
+
+
+//public class Solution {
+//    private static boolean isCancel;
+//
+//    public static void main(String[] args) throws InterruptedException {
+//        Thread t =  new Thread(new TestThread());
+//        t.start();
+//        Thread.sleep(3000);
+//        outInterrupt();
+//    }
+//
+//    private static void outInterrupt() {
+//        isCancel = true;
+//    }
+//
+//    public static class TestThread implements Runnable{
+//        public void run() {
+//            try {
+//                while (!isCancel) {
+//                    System.out.println("he-he");
+//                    Thread.sleep(500);
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
