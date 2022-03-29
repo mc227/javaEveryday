9cf5b8fe9ce296acc8bb9074dcf51be153962b3b
mc227 <markcostales.mark@gmail.com>
2022-03-29 17:36:48 -0400

    HelloWorldApp Oracle

---
+++ src/main/java/Random1.java
@@ -0,0 +1,19 @@
+//public class Random1
+//{
+//    public static void main(String[] args)
+//    {
+//
+//        for ( int i = 0 ; i < 10; i++ )
+//        {
+//            System.out.println( Math.random() );    // Print a random number
+//        }
+//    }
+//}
+
+public class Random1 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+            System.out.println(Math.random());
+        }
+    }
+}
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("oh hi Mark");
+        int[] myIntArray = {3, 5, 9};
+        System.out.println(Arrays.toString(twoNumberSum(new int[] {3, 5, -4, 8, 4, 1, -1, 6},11)));
+    }
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        for(int i = 0; i < array.length - 1; i++) {
+            int firstNum = array[i];
+            for (int j = i + i; j < array.length; j++) {
+                int secondNum = array[j];
+                if(firstNum + secondNum == targetSum) {
+                    return new int[] {firstNum, secondNum};
+                }
+            }
+        }
+        return new int[0];
     }
 }
