6f3fb88bba9c89502ea2770a45d3b2513459cfd2
mc227 <markcostales.mark@gmail.com>
2022-03-31 14:22:06 -0400

    nextDouble

--- src/main/java/HelloWorldApp.java
+++ src/main/java/HelloWorldApp.java
@@ -1,5 +1,21 @@
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Oh Hi " +  args[0]);
+//        System.out.println("oh Hai, " + args[0]);
+//        while(true) {
+//            // Do nothing
+//        }
+//        Thread currentThread = Thread.currentThread();
+//        ThreadGroup threadGroup = currentThread.getThreadGroup();
+//        System.out.println("Thread: " + currentThread.getName());
+//        System.out.println("Thread Group: " + threadGroup.getName());
+//        System.out.println("Parent Group: " + threadGroup.getParent().getName());
+        Thread th = Thread.currentThread();
+        th.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("An error occurred: " + e.getMessage());
+            }
+        });
+        System.out.println(2/0);
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,21 +1,9 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] myIntArray = {3, 5, 9};
-        System.out.println(Arrays.toString(twoNumberSum(new int[] {3, 5, -4, 8, 4, 1, -1, 6},11)));
-    }
-
-    public static int[] twoNumberSum(int[] array, int targetSum) {
-        for(int i = 0; i < array.length - 1; i++) {
-            int firstNum = array[i];
-            for (int j = i + i; j < array.length; j++) {
-                int secondNum = array[j];
-                if(firstNum + secondNum == targetSum) {
-                    return new int[] {firstNum, secondNum};
-                }
-            }
-        }
-        return new int[0];
+        Random r = new Random();
+        System.out.println(r.nextDouble());
     }
 }
