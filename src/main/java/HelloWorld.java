8bc2f983105dbc15956991f8a00374828325591f
mc227 <markcostales.mark@gmail.com>
2022-04-02 13:41:01 -0400

    today's push

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld{
     public static void main(String []args){
         Object object = new Object();
         synchronized(object) {
-            System.out.println("Hello World");
+            System.out.println("There's no easy way out");
+            System.out.println("There's shortcut home");
         }
     }
 }
