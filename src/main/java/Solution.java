7e8f67b5f213474adbe5133fa7f2ab2b3069628d
mc227 <markcostales.mark@gmail.com>
2022-04-04 19:37:03 -0400

    just taking a step back

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Solution {
-    public static void main(String[] args) {
-        Random random = new Random();
-        random.setSeed(3L);
-        for (int i=0; i < 10; i++) {
-//            System.out.println(random.nextDouble());
-            System.out.println(random.nextInt());
-        }
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("I'm gonna step back and take it easy\nIt's ok to take your time");
+        System.out.println("it's better to understand it thorougly");
+        System.out.println("i'm even thinking about going at it a bit slower ");
+        System.out.println("what I'm doing now is going to the part where I understand");
+        System.out.println("so that I can ease into the parts I don't understand");
+        System.out.println("I'm even thinking about doing that thing with the course");
+        System.out.println("it'll be a good excuse to take that thing i wanted to take");
     }
 }
