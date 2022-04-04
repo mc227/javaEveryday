52f5ebd7412d02be09f651f5e90649f5a457bf16
mc227 <markcostales.mark@gmail.com>
2022-04-04 19:45:58 -0400

    really long path

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,12 +3,18 @@ import java.util.Random;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("I'm gonna step back and take it easy\nIt's ok to take your time");
-        System.out.println("it's better to understand it thorougly");
-        System.out.println("i'm even thinking about going at it a bit slower ");
-        System.out.println("what I'm doing now is going to the part where I understand");
-        System.out.println("so that I can ease into the parts I don't understand");
-        System.out.println("I'm even thinking about doing that thing with the course");
-        System.out.println("it'll be a good excuse to take that thing i wanted to take");
+        System.out.println("https://codegym.cc/groups/posts/108-multithreading-in-java-what-it-is-its-benefits-and-common-pitfalls");
+        System.out.println("https://codegym.cc/groups/posts/109-multithreading-what-the-methods-of-the-thread-class-do");
+        System.out.println("https://codegym.cc/groups/posts/111-managing-threads-the-volatile-keyword-and-the-yield-method");
+        System.out.println("https://codegym.cc/groups/posts/266-better-together-java-and-the-thread-class-part-i--threads-of-execution");
+        System.out.println("https://codegym.cc/groups/posts/267-better-together-java-and-the-thread-class-part-ii--synchronization");
+        System.out.println("https://codegym.cc/groups/posts/268-better-together-java-and-the-thread-class-part-iii--interaction");
+        System.out.println("https://codegym.cc/groups/posts/270-better-together-java-and-the-thread-class-part-iv--callable-future-and-friends");
+        System.out.println("https://codegym.cc/groups/posts/271-better-together-java-and-the-thread-class-part-v--executor-threadpool-forkjoin");
+        System.out.println("https://codegym.cc/groups/posts/272-better-together-java-and-the-thread-class-part-vi--fire-away");
+        System.out.println("take Mastering Threads in Java");
+        System.out.println("Java Concurrency");
+        System.out.println("finish level 17");
+        System.out.println("it's actually way more than i expected");
     }
 }
