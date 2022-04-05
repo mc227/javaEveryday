935ac0ca076ec6eea253aa81b0f8d270a31fd102
mc227 <markcostales.mark@gmail.com>
2022-04-05 09:15:51 -0400

    test

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,18 +3,6 @@ import java.util.Random;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("https://codegym.cc/groups/posts/108-multithreading-in-java-what-it-is-its-benefits-and-common-pitfalls");
-        System.out.println("https://codegym.cc/groups/posts/109-multithreading-what-the-methods-of-the-thread-class-do");
-        System.out.println("https://codegym.cc/groups/posts/111-managing-threads-the-volatile-keyword-and-the-yield-method");
-        System.out.println("https://codegym.cc/groups/posts/266-better-together-java-and-the-thread-class-part-i--threads-of-execution");
-        System.out.println("https://codegym.cc/groups/posts/267-better-together-java-and-the-thread-class-part-ii--synchronization");
-        System.out.println("https://codegym.cc/groups/posts/268-better-together-java-and-the-thread-class-part-iii--interaction");
-        System.out.println("https://codegym.cc/groups/posts/270-better-together-java-and-the-thread-class-part-iv--callable-future-and-friends");
-        System.out.println("https://codegym.cc/groups/posts/271-better-together-java-and-the-thread-class-part-v--executor-threadpool-forkjoin");
-        System.out.println("https://codegym.cc/groups/posts/272-better-together-java-and-the-thread-class-part-vi--fire-away");
-        System.out.println("take Mastering Threads in Java");
-        System.out.println("Java Concurrency");
-        System.out.println("finish level 17");
-        System.out.println("it's actually way more than i expected");
+
     }
 }
