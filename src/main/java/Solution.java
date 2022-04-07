df43e5f53903c8c66d6a22d228d5c714cfd9639c
mc227 <markcostales.mark@gmail.com>
2022-04-06 21:06:44 -0400

    reviewing old articles

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,6 +3,11 @@ import java.util.Random;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        Thread.sleep(3000);
+
+        System.out.println(" - How long did I sleep? \n - " + ((System.currentTimeMillis()-start)) / 1000 + " seconds");
 
     }
 }
