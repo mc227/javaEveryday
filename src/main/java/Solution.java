17ee98c316c0a8dd54f497700ea13a11931f7da0
mc227 <markcostales.mark@gmail.com>
2022-04-08 23:00:28 -0400

    linked list

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,8 +1,17 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello, " + args[0]);
+    public static void main(String[] args){
+
+        // create linkedlist
+        LinkedList<String> animals = new LinkedList<>();
+
+        // Add elements to LinkedList
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Cow");
+        System.out.println("LinkedList: " + animals);
     }
 }
