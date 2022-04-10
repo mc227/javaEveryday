a045c974ba991a54d0a39627ebbbf5cb630707b1
mc227 <markcostales.mark@gmail.com>
2022-04-10 16:04:01 -0400

    Synchronized (waiting for a lock)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,14 +3,23 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class Solution {
-    public static void main(String[] args) {
-        Thread th = Thread.currentThread();
-        th.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                System.out.println("An error occurred: " + e.getMessage());
+    public static void main(String[] args) throws InterruptedException {
+        Object lock = new Object();
+
+        Runnable task = () -> {
+            synchronized(lock) {
+                System.out.println("thread");
             }
-        });
-        System.out.println(2/0);
+        };
+
+        Thread th1 = new Thread(task);
+        th1.start();
+        synchronized(lock) {
+            for (int i = 0; i < 4; i++) {
+                Thread.currentThread().sleep(1000);
+                System.out.print(" " + i);
+            }
+            System.out.println(" ...");
+        }
     }
 }
