44f61fd1c7de1222bd8e4bad1cae19812111644b
mc227 <markcostales.mark@gmail.com>
2022-04-09 23:56:56 -0400

    string hash set

--- src/main/java/HelloWorldApp.java
+++ src/main/java/HelloWorldApp.java
@@ -1,15 +1,20 @@
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 class HelloWorldApp {
     public static void main(String []args) {
-        Runnable task = () -> {
-            while(!Thread.currentThread().isInterrupted()) {
-                // Do some work
-            }
-            System.out.println("Finished");
-        };
-        Thread thread = new Thread(task);
-        thread.start();
-        thread.interrupt();
+        HashSet<String> set=new HashSet();
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+        set.add("Four");
+        set.add("Five");
+        Iterator<String> i=set.iterator();
+        while(i.hasNext())
+        {
+            System.out.println(i.next());
+        }
     }
 }
+
