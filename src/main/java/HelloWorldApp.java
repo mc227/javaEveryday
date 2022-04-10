ed9352e81d00967618b2810b490f5abb90d73902
mc227 <markcostales.mark@gmail.com>
2022-04-10 15:52:01 -0400

    studying monitoring and locks

--- src/main/java/HelloWorldApp.java
+++ src/main/java/HelloWorldApp.java
@@ -1,19 +1,13 @@
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 class HelloWorldApp {
     public static void main(String []args) {
-        HashSet<String> set=new HashSet();
-        set.add("One");
-        set.add("Two");
-        set.add("Three");
-        set.add("Four");
-        set.add("Five");
-        Iterator<String> i=set.iterator();
-        while(i.hasNext())
-        {
-            System.out.println(i.next());
+        Object object = new Object();
+        synchronized(object) {
+            System.out.println("Hello World");
         }
     }
 }
