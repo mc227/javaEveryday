3b11b53c08fe847d6d4f35662027c039edcd036b
mc227 <markcostales.mark@gmail.com>
2022-04-11 08:40:37 -0400

    algo expert Two Number sum question O(n^2) T | O(1) S

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,23 +2,28 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Program {
-    // this is the algoexpert solution
+    public static void main(String[] args) {
+        // my test case
+        int[] intArray = new int[] {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(twoNumberSum(intArray,7)));
+
+        /*Test Case 1*/
+
+    }
+    // this is the algoexpert's solution to Two Number Sum
     // O(n^2) time | O(1) space
     public static int[] twoNumberSum(int[] array, int targetSum) {
-        for(int i = 0; i < array.length - 1; i++) {
-            int firstNum = array[i];
-            for (int j = i + 1; j < array.length; j++) {
-                int secondNum = array[j];
-                if (firstNum + secondNum == targetSum) {
-                    return new int[] {firstNum, secondNum};
+        // loop through array once starting index 0
+        for(int i = 0; i < array.length; i++) {
+            int firstArray = array[i];
+            // loop through again but this time start after
+            for (int j = i + 1; j <  array.length; j++) {
+                int secondArray = array[j];
+                if (firstArray + secondArray == targetSum) {
+                    return new int[] {firstArray, secondArray};
                 }
             }
         }
         return new int[0];
-    }
-
-    public static void main(String[] args) {
-        int[] intArray = new int[] {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(twoNumberSum(intArray,3)));
     }
 }
