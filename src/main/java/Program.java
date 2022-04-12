da9ac42428c22facc89b2c4ea813318ab2dfebf5
mc227 <markcostales.mark@gmail.com>
2022-04-12 15:05:19 -0400

    I have to get good at data structures and algorithms

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,29 +5,21 @@ import java.util.Set;
 
 public class Program {
     public static void main(String[] args) {
-//        /*Test Case 1*/
-//        int[] intArray = new int[] {3, 5, -4, 8, 11, 1, -1, 6};
-//        System.out.println(Arrays.toString(twoNumberSum(intArray,10)));
-        /*Test Case */
         int[] intArray = new int[] {1,2,3,4,5,6,7,8,9};
-        System.out.println(Arrays.toString(twoNumberSum(intArray,10)));
+        System.out.println(Arrays.toString(twoNumberSum(intArray,17)));
 
     }
-    // this is the algoexpert's first solution to Two Number Sum
+    // this is the algoexpert's second solution to Two Number Sum
     // working on muscle memory
-    // O(n^2)) time | O(1) space
+    // O(n) time | O(n) space
     public static int[] twoNumberSum(int[] array, int targetSum) {
-        //loop through the array
-        //loop through it again
-        // add the two arrays
-        // if it's equal the target sum print the two arrays
-        for(int i = 0; i < array.length; i++) {
-            int firstItem = array[i];
-            for(int j = i + 1; j < array.length; j++) {
-                int secondItem = array[j];
-                if(firstItem+secondItem == targetSum) {
-                    return new int[] {firstItem, secondItem};
-                }
+        Set<Integer> set = new HashSet<>();
+        for(int num:array){
+            int potentialMatch = targetSum - num;
+            if (set.contains(potentialMatch)) {
+                return new int[] {potentialMatch, num};
+            } else {
+                set.add(num);
             }
         }
         return new int[0];
