38af1be9920112185a6484635912ab9c632a22b0
mc227 <markcostales.mark@gmail.com>
2022-04-11 21:23:30 -0400

    this is the modified version of solution 2 of twoNumberSum

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,27 +1,31 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Program {
     public static void main(String[] args) {
-        // my test case
-        int[] intArray = new int[] {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(twoNumberSum(intArray,7)));
-
         /*Test Case 1*/
+        int[] intArray = new int[] {3, 5, -4, 8, 11, 1, -1, 6};
+        System.out.println(Arrays.toString(twoNumberSum(intArray,10)));
 
     }
-    // this is the algoexpert's solution to Two Number Sum
-    // O(n^2) time | O(1) space
+    // this is the algoexpert's second solution to Two Number Sum
+    // O(n) time | O(n) space
     public static int[] twoNumberSum(int[] array, int targetSum) {
-        // loop through array once starting index 0
+        // create a hash set to store stuff and to also check stuff
+        Set<Integer> set = new HashSet<>();
+        // loop through every item in the array
         for(int i = 0; i < array.length; i++) {
-            int firstArray = array[i];
-            // loop through again but this time start after
-            for (int j = i + 1; j <  array.length; j++) {
-                int secondArray = array[j];
-                if (firstArray + secondArray == targetSum) {
-                    return new int[] {firstArray, secondArray};
-                }
+            // define potential match as the target sum minus the item in the array
+            int potentialMatch = targetSum - array[i];
+            // if the set contain the potential match which is the difference between the target sum and the array item
+            // then return potential match and array in the loop
+            // else put it into the hash map
+            if (set.contains(potentialMatch)) {
+                return new int[] {potentialMatch, array[i]};
+            } else {
+                set.add(array[i]);
             }
         }
         return new int[0];
