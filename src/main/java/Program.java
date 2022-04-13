57be9aa4312a4145ad7a6b6894149d9baf873697
mc227 <markcostales.mark@gmail.com>
2022-04-12 21:25:59 -0400

    second solution two Number Sum

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -8,23 +8,22 @@ public class Program {
         int[] intArray = new int[] {3,5,-4,8,11,1,-1,6};
         System.out.println(Arrays.toString(twoNumberSum(intArray,8)));
     }
-    /**Solution 1
-     * O(n^2)T | O(1)S*/
+    /**Solution 2
+     * O(n)T | O(2)S
+     * */
     public static int[] twoNumberSum(int[] array, int targetSum) {
-        // loop through starting from the beginning of the array
-        // then inside do another for loop but start with array[1]
-        // set first after the first for loop and second after the next for loop starts
-        // return the first and second pair that results in the targetSum
-        // at the end of the two for loops return an empty array if nothing comes up
-        for(int i = 0; i < array.length; i++) {
-            int first = array[0];
-            for(int j = i+1; j < array.length; j++) {
-                int second = array[j];
-                if(first + second == targetSum) {
-                    return new int[] {first, second};
-                }
+        // create a hashset of Integers
+        // just use one for loop
+        // compare each number to a complement of the targetSum
+        // meaning targetSum - the number array[i]
+        Set<Integer> set = new HashSet<>();
+        for(int num:array) {
+            int potentialMatch = targetSum - num;
+            if(set.contains(potentialMatch)){
+                return new int[] {potentialMatch, num};
+            } else {
+                set.add(num);
             }
-            return new int[0];
         }
         return new int[0];
     }
