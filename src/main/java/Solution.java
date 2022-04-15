d5fba73a81b350ca79edee072548f6bd0b794fb8
mc227 <markcostales.mark@gmail.com>
2022-04-14 21:48:07 -0400

    Creating threads recursively

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,42 +1,36 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Random;
-import java.util.concurrent.locks.LockSupport;
+/*
+Creating threads recursively
+
+*/
+
+import java.sql.SQLOutput;
 
 public class Solution {
-    //task1622
-    public volatile static int COUNT = 4;
-
-    public static void main(String[] args) throws InterruptedException{
-        for(int i = 0; i < COUNT; i++) {
-            new SleepingThread().join();
-        }
+    static int count = 15;
+    static volatile int createdThreadCount;
+    public static void main(String[] args) {
+        System.out.println(new GenerateThread());
     }
 
-    public static class SleepingThread extends Thread{
-        private static volatile int threadCount = 0;
-        private volatile int countdownIndex = COUNT;
+    public static class GenerateThread extends Thread{
 
-        public SleepingThread() {
-            super(String.valueOf(++threadCount));
+        public GenerateThread() {
+            super(String.valueOf(++createdThreadCount));
             start();
-        }
-
-        public void run() {
-            while(true) {
-                System.out.println(this);
-                if(--countdownIndex == 0) return;
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread interrupted");
-                }
-            }
         }
 
         @Override
         public String toString() {
-            return "#" + getName() + ":" + countdownIndex;
+            return getName() + " created";
+        }
+
+        @Override
+        public void run() {
+
+            if(createdThreadCount < Solution.count) {
+                System.out.println(new GenerateThread());
+            }
+
         }
     }
 }
