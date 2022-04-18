076880118dfce5532f707a544a2d66933e8bd6ab
mc227 <markcostales.mark@gmail.com>
2022-04-17 20:48:39 -0400

    testing out 1702

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,82 +1,55 @@
-//package com.codegym.task.task16.task1628;
+//package com.codegym.task.task17.task1702;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
+/*
+Faster together? We'll see :)
+
+*/
 
 public class Solution {
-    public static volatile AtomicInteger readStringCount = new AtomicInteger(0);
-    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    // 2021.03.24.16.34.26
-    private static final SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+    public static int threadCount = 10;
+    public static int[] testArray = new int[1000];
 
-    // 2021-03-24T16:44:39.083+08:00
-    private static final SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-
-    // 2021-03-24 16:48:05
-    private static final SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-
-    public static void main(String[] args) throws IOException {
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-
-        // Read string count
-        int count = Integer.parseInt(reader.readLine());
-
-        // Init threads
-        ReaderThread consoleReader1 = new ReaderThread();
-        ReaderThread consoleReader2 = new ReaderThread();
-        ReaderThread consoleReader3 = new ReaderThread();
-
-        consoleReader1.start();
-        consoleReader2.start();
-        consoleReader3.start();
-
-        while (count > readStringCount.get()) {
-
+    static {
+        for (int i = 0; i < Solution.testArray.length; i++) {
+            testArray[i] = i;
         }
-
-        consoleReader1.interrupt();
-        System.out.println(timestamp);
-        consoleReader2.interrupt();
-        System.out.println(timestamp);
-        consoleReader3.interrupt();
-        System.out.println(timestamp);
-        System.out.println("#1:" + consoleReader1);
-        System.out.println(timestamp);
-        System.out.println("#2:" + consoleReader2);
-        System.out.println(timestamp);
-        System.out.println("#3:" + consoleReader3);
-        System.out.println(timestamp);
-
-        reader.close();
     }
 
-    public static class ReaderThread extends Thread {
-        private List<String> result = new ArrayList<>();
-
-        public void run() {
-            //write your code here
-            while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    String line = reader.readLine();
-                    if (line !=null) {
-                        result.add(line);
-                        readStringCount.addAndGet(1);
-                    }
-                } catch (IOException e) {}
-            }
+    public static void main(String[] args) throws InterruptedException {
+        StringBuffer expectedResult = new StringBuffer();
+        for (int i = 1000 - 1; i >= 0; i--) {
+            expectedResult.append(i).append(" ");
         }
 
-        @Override
-        public String toString() {
-            return result.toString();
+        initThreads();
+
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < testArray.length; i++) {
+            result.append(testArray[i]).append(" ");
+        }
+        System.out.println(result);
+        System.out.println((result.toString()).equals(expectedResult.toString()));
+    }
+
+    public static void initThreads() throws InterruptedException {
+        List<Thread> threads = new ArrayList<Thread>(threadCount);
+        for (int i = 0; i < threadCount; i++) threads.add(new SortThread());
+        for (Thread thread : threads) thread.start();
+        for (Thread thread : threads) thread.join();
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int k = array[i];
+                    array[i] = array[j];
+                    array[j] = k;
+                }
+            }
         }
     }
 }
