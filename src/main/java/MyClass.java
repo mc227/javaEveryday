26be10941d3e96f2edfd3e96038b4134b561fc8d
mc227 <markcostales.mark@gmail.com>
2022-04-17 20:47:07 -0400

    add MyClass

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    private String name1 = "Ally";
+    private String name2 = "Lena";
+    public void swap()
+    {
+        String s = name1;
+        name1 = name2;
+        name2 = s;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
