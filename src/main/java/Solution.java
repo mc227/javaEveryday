bf3c17a958324008cb5b0fa56eb785e8601d439e
mc227 <markcostales.mark@gmail.com>
2022-04-21 17:47:15 -0400

    i didn't know that, that was how you instantiate a singleton. lucky i guess

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,38 +1,14 @@
-//package com.codegym.task.task17.task1705;
+//package com.codegym.task.task17.task1706;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/* 
-Time in the garden
+/*
+Synchronized president
 
 */
 
 public class Solution {
-
     public static void main(String[] args) {
-
-    }
-
-    public static class Garden {
-
-        public final List<String> fruits = new ArrayList<>();
-        public final List<String> vegetables = new ArrayList<>();
-
-        public synchronized void addFruit(int index, String fruit) {
-            fruits.add(index, fruit);
-        }
-
-        public synchronized void removeFruit(int index) {
-            fruits.remove(index);
-        }
-
-        public synchronized void addVegetable(int index, String vegetable) {
-            vegetables.add(index, vegetable);
-        }
-
-        public synchronized void removeVegetable(int index) {
-            vegetables.remove(index);
-        }
+        OurPresident expectedPresident = OurPresident.getOurPresident();
+        OurPresident ourPresident = OurPresident.getOurPresident();
+        System.out.println(expectedPresident == ourPresident );
     }
 }
