c6ba0decdd7a340c3d489ddb47009c48e72284f7
mc227 <markcostales.mark@gmail.com>
2022-04-23 14:52:10 -0400

    task 1708

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,14 +1,33 @@
-//package com.codegym.task.task17.task1706;
+//package com.codegym.task.task17.task1708;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
-Synchronized president
+Notes for all
 
 */
 
 public class Solution {
+
     public static void main(String[] args) {
-        IMF fund = IMF.getFund();
-        IMF anotherFund = IMF.getFund();
-        System.out.println(fund == anotherFund );
+
+    }
+
+    public static class Note {
+
+        public volatile List<String> notes = new ArrayList<>();
+
+        public void addNote(int index, String note) {
+            System.out.println("A note [" + note + "] will now be added at position " + index);
+            notes.add(index, note);
+            System.out.println("The note [" + note + "] has already been added");
+        }
+
+        public void removeNote(int index) {
+            System.out.println("A note will now be deleted from position " + index);
+            String note = notes.remove(index);
+            System.out.println("The note [" + note + "] has already been deleted from position " + index);
+        }
     }
 }
