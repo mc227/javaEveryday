482e60df9a167ef5ef9b06c9c266a8b29dfd67ad
mc227 <markcostales.mark@gmail.com>
2022-05-01 18:59:25 -0400

    I'm on the right track

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -13,34 +13,7 @@ CRUD
 */
 
 public class Solution {
-//    public static List<Person> allPeople = new ArrayList<>();
-//
-//    static {
-//        allPeople.add(Person.createMale("Donald Chump", new Date()));  // id=0
-//        allPeople.add(Person.createMale("Larry Gates", new Date()));  // id=1
-//    }
-//
-//    public static void main(String[] args) throws IOException, ParseException {
-//        // Start here
-//        SimpleDateFormat formatter = new SimpleDateFormat("mm, dd, yyyy", Locale.ENGLISH);
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        String inputString = bufferedReader.readLine();
-//        if (inputString.startsWith("-c")) {
-//            String value = inputString.replace("-c ", "");
-//            System.out.println(Arrays.toString(value.split(" ")));
-//            System.out.println(value.split(" ")[0]);
-//            String name = value.split(" ")[0];
-//            String gender = value.split(" ")[1];
-////            String dateInString = value.split(" ")[1];
-////            String dateInString = String.copyValueOf(value.split(" "), 2, 3);
-////            System.out.println(String.copyValueOf(, 2, 3););
-////            Date date = formatter.parse(dateInString);
-//
-////            if (gender.equals("m")) {
-////                System.out.println("here");
-////                allPeople.add(Person.createMale(name, date));  // id=0
-////            }
-
-        }
+    public static void main(String[] args) {
+        // -c Washington m "04 15 1990"
     }
 }
