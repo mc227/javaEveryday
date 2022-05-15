47bba8d9a073293d0658afc84ba1325e436a9d3b
mc227 <markcostales.mark@gmail.com>
2022-05-15 10:52:50 -0400

    it's getting there;

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,36 +1,13 @@
-import com.google.common.collect.Lists;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
-import org.apache.commons.collections4.ListUtils;
 
 public class Solution {
-    // one-dimensional array of strings is of type String[]
+    private static List<Long> original = new ArrayList<>();
+
     public static void main(String[] args) {
-
-
-        // convert one-dimensional array of strings to ArrayList of Strings
-        // added them one by one because adding them at the same time using ... causes errors
-        ArrayList<String> wordList = new ArrayList<>();
-        for (int i = 0; i < args.length; i++) {
-            wordList.add(args[i]);
-        }
-
-        // then here I remove the tag
-        wordList.remove("-c");
-//        for (String e : wordList) {
-//            System.out.println(e);
-//        }
-
-
-        int targetSize = 3;
-        List<String> largeListString = wordList;
-        List<List<String>> output = ListUtils.partition(largeListString, targetSize);
-
-        System.out.println(output.get(0).get(0));
-
+        original.add(1L);
+        original.add(2L);
+        System.out.println(original.isEmpty());
+        System.out.println("Oh HI Mark");
     }
 }
---
+++ src/main/java/com/codegym/task/task17/task1713/Solution.java
@@ -0,0 +1,136 @@
+package com.codegym.task.task17.task1713;
+
+import java.util.*;
+
+/*
+Shared list
+
+*/
+
+public class Solution implements List<Long>{
+    private ArrayList<Long> original = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public synchronized int size() {
+        return original.size();
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        if (original.size() >0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public synchronized boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public synchronized Iterator<Long> iterator() {
+        return null;
+    }
+
+    @Override
+    public synchronized Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public synchronized <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public synchronized boolean add(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean addAll(Collection<? extends Long> c) {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean addAll(int index, Collection<? extends Long> c) {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public synchronized void clear() {
+
+    }
+
+    @Override
+    public synchronized Long get(int index) {
+        return null;
+    }
+
+    @Override
+    public synchronized Long set(int index, Long element) {
+        original.set(index,element);
+        return null;
+    }
+
+    @Override
+    public synchronized void add(int index, Long element) {
+
+    }
+
+    @Override
+    public synchronized Long remove(int index) {
+        return null;
+    }
+
+    @Override
+    public synchronized int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public synchronized int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public synchronized ListIterator<Long> listIterator() {
+        return null;
+    }
+
+    @Override
+    public synchronized ListIterator<Long> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public synchronized List<Long> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
