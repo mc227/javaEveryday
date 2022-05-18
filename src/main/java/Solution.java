a4956cf318e5d5c60f4af04bf2978193cc710108
mc227 <markcostales.mark@gmail.com>
2022-05-18 16:21:25 -0400

    FileInputStream implementation

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,13 +1,25 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    private static List<Long> original = new ArrayList<>();
+    public static void main(String[] args) throws Exception
+    {
+        //Create a FileInputStream object bound to «c:/data.txt».
+        FileInputStream inputStream = new FileInputStream("data.txt");
+        long sum = 0;
 
-    public static void main(String[] args) {
-        original.add(1L);
-        original.add(2L);
-        System.out.println(original.isEmpty());
-        System.out.println("Oh HI Mark");
+        while (inputStream.available() > 0) //as long as there are unread bytes
+        {
+            int data = inputStream.read(); //Read the next byte
+            System.out.println(data);
+            sum +=  data; //Add it to the running total
+        }
+        inputStream.close(); // Close the stream
+
+        System.out.println(sum); // Display the sum on the screen.
     }
+
 }
