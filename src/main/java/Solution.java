87d934888e29fdab41bb9f1d5e1f31e5e0cc1e75
mc227 <markcostales.mark@gmail.com>
2022-05-24 01:30:06 -0400

    that was hard

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,30 +2,34 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Lena").append('\n');
-        sb.append("Olya").append('\n');
-        sb.append("Anya").append('\n');
-        String data = sb.toString();
-
-        InputStream is = new ByteArrayInputStream(data.getBytes());
-
-        System.setIn(is);
-
-        readAndPrintLine();
-    }
-
-    public static void readAndPrintLine() throws IOException {
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(isr);
-
-        while (true)
-        {
-            String line = reader.readLine();
-            if (line == null) break;
-            System.out.println(line);
-        }
-        reader.close();
-        isr.close();
+        int yourInt = 33;
+        char ch = (char) yourInt;
+        System.out.println(yourInt); // 33
+        System.out.println(ch); // !
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("Lena").append('\n');
+//        sb.append("Olya").append('\n');
+//        sb.append("Anya").append('\n');
+//        String data = sb.toString();
+//
+//        InputStream is = new ByteArrayInputStream(data.getBytes());
+//
+//        System.setIn(is);
+//
+//        readAndPrintLine();
+//    }
+//
+//    public static void readAndPrintLine() throws IOException {
+//        InputStreamReader isr = new InputStreamReader(System.in);
+//        BufferedReader reader = new BufferedReader(isr);
+//
+//        while (true)
+//        {
+//            String line = reader.readLine();
+//            if (line == null) break;
+//            System.out.println(line);
+//        }
+//        reader.close();
+//        isr.close();
     }
 }
