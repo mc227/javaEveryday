69ad814919c3e581db27c813b541e4ec23971fcf
mc227 <markcostales.mark@gmail.com>
2022-05-25 00:33:31 -0400

    1821 from scratch

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,35 +1,66 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
+/*
+reviewing com.codegym.task.task18.task1821;
+Symbol frequency
+The program is started with one argument: the name of a file that contains text.
+Calculate how often each symbol is encountered.
+Sort the results by increasing ASCII code (read about it online).
+
+Example:
+','=44, 's'=115, 't'=116.
+
+Display the sorted results:
+[symbol1] frequency1
+[symbol2] frequency2
+Close the streams.
+
+Example output:
+, 19
+- 7
+f 361
+
+
+Requirements:
+1. You don't need to read anything from the console.
+2. Create a stream to read from the file passed as the first argument of the main method.
+3. You need to calculate the frequency of each symbol in the file and display the results.
+4. The displayed results should be sorted by ascending ASCII code.
+5. The stream used to read the file must be closed.
+* */
 public class Solution {
     public static void main(String[] args) throws Exception {
-        int yourInt = 33;
-        char ch = (char) yourInt;
-        System.out.println(yourInt); // 33
-        System.out.println(ch); // !
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("Lena").append('\n');
-//        sb.append("Olya").append('\n');
-//        sb.append("Anya").append('\n');
-//        String data = sb.toString();
-//
-//        InputStream is = new ByteArrayInputStream(data.getBytes());
-//
-//        System.setIn(is);
-//
-//        readAndPrintLine();
-//    }
-//
-//    public static void readAndPrintLine() throws IOException {
-//        InputStreamReader isr = new InputStreamReader(System.in);
-//        BufferedReader reader = new BufferedReader(isr);
-//
-//        while (true)
-//        {
-//            String line = reader.readLine();
-//            if (line == null) break;
-//            System.out.println(line);
-//        }
-//        reader.close();
-//        isr.close();
+
+        // Create a stream to read from the file passed
+        // as the first argument of the main method.
+        FileInputStream fileInputStream = new FileInputStream(args[0]);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        byte[] contents = new byte[1024];
+        int bytesRead = 0;
+        String strContent = "";
+        // read all the bytes into strContent
+        while((bytesRead = bufferedInputStream.read(contents)) != -1) {
+            strContent += new String(contents, 0, bytesRead);
+        }
+//        System.out.println(strContent);
+        fileInputStream.close();
+        bufferedInputStream.close();
+
+        byte[] foo = strContent.getBytes("US-ASCII");
+        Map<Byte, Integer> map = new TreeMap<>();
+        for(Byte item: foo) {
+            if (map.containsKey(item)){
+                map.put(item, map.get(item)+1);
+            } else {
+                map.put(item, 1);
+            }
+        }
+        for (Map.Entry<Byte,Integer> entry : map.entrySet()){
+            System.out.println((char)entry.getKey().byteValue() + " " + entry.getValue());
+        }
     }
 }
