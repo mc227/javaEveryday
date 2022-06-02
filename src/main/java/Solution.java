e7d8b5f6758b57c8157f3944e9e243bc2d7ce875
mc227 <markcostales.mark@gmail.com>
2022-06-01 23:30:19 -0400

    in the middle of solving symbolic occurences

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,35 +1,44 @@
-
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.PrintStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
     /*
     Symbol frequency
-    The program is started with one argument: the name of a file that contains text.
-    Calculate how often each symbol is encountered.
-    Sort the results by increasing ASCII code (read about it online).
-
-    Example:
-    ','=44, 's'=115, 't'=116.
-
-    Display the sorted results:
-    [symbol1] frequency1
-    [symbol2] frequency2
-    Close the streams.
-
-    Example output:
-    , 19
-    - 7
-    f 361
 
 
-    Requirements:
-    1. You don't need to read anything from the console.
-    2. Create a stream to read from the file passed as the first argument of the main method.
+
+
     3. You need to calculate the frequency of each symbol in the file and display the results.
     4. The displayed results should be sorted by ascending ASCII code.
     5. The stream used to read the file must be closed.
     * */
         // sympbol frequency from scratch
+        // The program is started with one argument: the name of a file that contains text.
+//        System.out.println(args[0]);
+        // Calculate how often each symbol is encountered.
+        // there are three symbols in file1.txt
+        // i recreated the 'example' given
+        // Sort the results by increasing ASCII code (read about it online).
+        //    Example:
+        //    ','=44, 's'=115, 't'=116.
+        //    Display the sorted results:
+        //    [symbol1] frequency1
+        //    [symbol2] frequency2
+        //    Close the streams.
+        //
+        //    Example output:
+        //    , 19
+        //    - 7
+        //    f 361
+        // Requirements:
+        //    1. You don't need to read anything from the console.
+        // 2. Create a stream to read from the file passed as the first argument of the main method.
+        FileInputStream fileInputStream = new FileInputStream(args[0]);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        byte[] byteArray = new byte[bufferedInputStream.available()];
+        PrintStream console =  System.out;
 
     }
 }
