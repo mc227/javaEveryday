392555270c26d685f36fb52922d752b5b8588eea
mc227 <markcostales.mark@gmail.com>
2022-06-02 19:52:02 -0400

    putting reps in

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,19 +1,19 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // what are you doing?
-        // I saw how files
-        // bytes in a file were summed up so
-        // here i am kinda doing it from scratch.
-        FileInputStream fileInputStream = new FileInputStream("data.txt");
+        /*Sum up all the bytes in a file*/
+        FileInputStream fileInputStream = new FileInputStream("file1.txt");
         long sum = 0;
+
         while(fileInputStream.available() > 0) {
             int foo = fileInputStream.read();
             sum += foo;
         }
-        fileInputStream.close();
         System.out.println(sum);
+        fileInputStream.close();
     }
 }
