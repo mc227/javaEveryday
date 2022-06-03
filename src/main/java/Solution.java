9a391dec19b16909e1aa839632cdfc7357d7a385
mc227 <markcostales.mark@gmail.com>
2022-06-02 21:33:12 -0400

    Minimum byte

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,28 +2,26 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        /*Maximum byte*/
+        /*Minimum byte */
 //        Enter a file name from the console.
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//                Find the maximum byte in the file. Display it on the screen.
+//                Find the minimum byte in the file. Display it on the screen.
         FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine());
-        byte max = 0;
+
+        int min = Integer.MAX_VALUE;
         while(fileInputStream.available() > 0) {
-            int data = fileInputStream.read();
-            System.out.println(data);
-            if(data > max) {
-                max = (byte) data;
+            int foo = fileInputStream.read();
+            if (foo < min) {
+                min = foo;
             }
         }
-        System.out.println(max);
+        System.out.println(min);
 //                Close the IO stream.
-        fileInputStream.close();
-        bufferedReader.close();
 //
 //                Requirements:
 //•	The program should read a file name from the console.
 //•	Use a FileInputStream to read from the file.
-//•	The program should display the maximum byte read from the file.
+//•	The program should display minimum byte read from the file.
 //•	The stream used to read the file must be closed.
 
     }
