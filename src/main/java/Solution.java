b57fe493e1e75e22faac05928b57fffe42b110a5
mc227 <markcostales.mark@gmail.com>
2022-06-02 20:29:00 -0400

    maximum byte

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,19 +1,30 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        /*Copy a file on a disk*/
-        FileInputStream fileInputStream = new FileInputStream("file1.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream("result.txt");
-
+        /*Maximum byte*/
+//        Enter a file name from the console.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//                Find the maximum byte in the file. Display it on the screen.
+        FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine());
+        byte max = 0;
         while(fileInputStream.available() > 0) {
-            int foo = fileInputStream.read();
-            fileOutputStream.write(foo);
+            int data = fileInputStream.read();
+            System.out.println(data);
+            if(data > max) {
+                max = (byte) data;
+            }
         }
+        System.out.println(max);
+//                Close the IO stream.
         fileInputStream.close();
-        fileOutputStream.close();
+        bufferedReader.close();
+//
+//                Requirements:
+//•	The program should read a file name from the console.
+//•	Use a FileInputStream to read from the file.
+//•	The program should display the maximum byte read from the file.
+//•	The stream used to read the file must be closed.
+
     }
 }
