fa525cd455136822599f2ed57438feaddafa9f9b
mc227 <markcostales.mark@gmail.com>
2022-06-05 19:27:39 -0400

    Download Exception

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,42 +1,70 @@
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import java.io.*;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+///*
+//DownloadException
+//1 Read file names from the console.
+//2 If the file is less than 1000 bytes, then:
+//2.1 Close the file stream.
+//2.2. Throw a DownloadException.
+//
+//Requirements:
+//•	The program should read file names from the console.
+//•	Use a FileInputStream to read from the files.
+//•	The program should work until the entered file is less than 1000 bytes.
+//•	The program should terminate with a DownloadException.
+//•	The FileInputStream must be closed.
+//* */
+//public class Solution {
+//    private static final Logger logger = LoggerFactory.getLogger(Solution.class);
+//    public static void main(String[] args) throws IOException, DownloadException {
+//        logger.info("oh hi Mark");
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        while(true) {
+//            FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine());
+//            if(fileInputStream.available() < 1000) {
+//                fileInputStream.close();
+//                throw new DownloadException();
+//            }
+//        }
+//
+//    }
+//    public static class DownloadException extends Exception {
+//
+//    }
+//}
 
 /*
-Read 2 file names from the console: file1, file2.
-Write all the bytes in file1 to file2, but in the reverse order.
-Close the streams.
+DownloadException
 
-Requirements:
-•	The program should read a file name twice from the console.
-•	Use FileInputStream to read from a file, and use FileOutputStream to write to a file.
-•	In the second file, you need to write all the bytes from the first file in the reverse order.
-•	The FileInputStream and FileOutputStream must be closed.
-* */
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    private static final Logger logger = LoggerFactory.getLogger(Solution.class);
-    public static void main(String[] args) throws IOException {
-        logger.info("oh hi Mark");
-        FileInputStream fileInputStream =  new FileInputStream("data.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream("result.txt");
-        ArrayList<Integer> arrayList = new ArrayList<>();
+    public static void main(String[] args) throws DownloadException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        while(fileInputStream.available() > 0) {
-            int count = fileInputStream.read();
-            arrayList.add(count);
+        while(true) {
+
+            FileInputStream inputStream = new FileInputStream(reader.readLine());
+            if(inputStream.available()<1000) {
+                inputStream.close();
+                throw new DownloadException();
+            }
         }
 
-        Collections.reverse(arrayList);
+    }
 
-        for(int i = 0; i < arrayList.size(); i++) {
-            fileOutputStream.write(arrayList.get(i));
-        }
+    public static class DownloadException extends Exception {
 
-        fileInputStream.close();
-        fileOutputStream.close();
     }
 }
