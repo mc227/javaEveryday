6b26ff0a33e78fe7a4e3a62ba3a16ea7fd2c477e
mc227 <markcostales.mark@gmail.com>
2022-06-05 22:41:23 -0400

    bubble sort with logger initial commit

--- src/main/java/CatWrapper.java
+++ src/main/java/CatWrapper.java
@@ -1,31 +1,34 @@
-public class CatWrapper extends Cat
-{
+public class CatWrapper extends Cat {
+    // 1) Create our own wrapper class and inherit from the same class/interface as the object to be wrapped.
+/*
+* */
     private Cat original;
-    public CatWrapper (Cat cat)
-    {
+
+    public CatWrapper(Cat cat) {
+        // 2) Pass the object to be wrapped to our class's constructor.
         super(cat.getName());
         this.original = cat;
     }
 
-    public String getName()
-    {
+    @Override
+    // 3) Override all methods in our new class. Invoke the wrapped object's methods inside each of the overridden methods.
+    public String getName() {
+        // 4) Make whatever changes you want: change what the method calls do, change their parameters, and/or do something else.
         return "A cat named " + original.getName();
     }
 
-    public void setName(String name)
-    {
+    @Override
+    public void setName(String name) {
         original.setName(name);
     }
 
-    public static void main(String[] args)
-    {
-        Cat cat = new Cat("Oscar");
-        Cat catWrap = new CatWrapper (cat);
-        printName(catWrap);
+    public static void printName(Cat named) {
+        System.out.println(named.getName());
     }
 
-    public static void printName(Cat named)
-    {
-        System.out.println(named.getName());
+    public static void main(String[] args) {
+        Cat cat = new Cat("Oscar");
+        Cat catWrap = new CatWrapper(cat);
+        printName(catWrap);
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,70 +1,24 @@
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import java.io.*;
-//import java.util.ArrayList;
-//import java.util.Collections;
-//
-///*
-//DownloadException
-//1 Read file names from the console.
-//2 If the file is less than 1000 bytes, then:
-//2.1 Close the file stream.
-//2.2. Throw a DownloadException.
-//
-//Requirements:
-//•	The program should read file names from the console.
-//•	Use a FileInputStream to read from the files.
-//•	The program should work until the entered file is less than 1000 bytes.
-//•	The program should terminate with a DownloadException.
-//•	The FileInputStream must be closed.
-//* */
-//public class Solution {
-//    private static final Logger logger = LoggerFactory.getLogger(Solution.class);
-//    public static void main(String[] args) throws IOException, DownloadException {
-//        logger.info("oh hi Mark");
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        while(true) {
-//            FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine());
-//            if(fileInputStream.available() < 1000) {
-//                fileInputStream.close();
-//                throw new DownloadException();
-//            }
-//        }
-//
-//    }
-//    public static class DownloadException extends Exception {
-//
-//    }
-//}
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /*
-DownloadException
-
-*/
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+Bubble sort
+compare two numbers;
+if the number on the left is larger, then swap them;
+move one position to the right.
+* */
 public class Solution {
-    public static void main(String[] args) throws DownloadException, IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        while(true) {
-
-            FileInputStream inputStream = new FileInputStream(reader.readLine());
-            if(inputStream.available()<1000) {
-                inputStream.close();
-                throw new DownloadException();
-            }
-        }
-
-    }
-
-    public static class DownloadException extends Exception {
-
+    private static final Logger logger = LoggerFactory.getLogger(Solution.class);
+    public static void main(String[] args) {
+        logger.info("Bubble Sort");
+        int[] foo = new int[]{3,2,1};
+        System.out.println(Arrays.toString(foo));
     }
 }
+
