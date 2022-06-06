4b22f923ba797dda242d03e56cb8245dd7b7c53a
mc227 <markcostales.mark@gmail.com>
2022-06-05 22:48:17 -0400

    bubble sort, bubble sort

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -17,8 +17,22 @@ public class Solution {
     private static final Logger logger = LoggerFactory.getLogger(Solution.class);
     public static void main(String[] args) {
         logger.info("Bubble Sort");
-        int[] foo = new int[]{3,2,1};
-        System.out.println(Arrays.toString(foo));
+        int[] foo = new int[]{6,3,8,2,6,9,4,11,1};
+        bubbleSort(foo);
+        for(Integer item: foo) {
+            System.out.print(item + " ");
+        }
+    }
+    public static void bubbleSort(int[] foo) {
+        for(int i = foo.length-1; i>=0; i--){
+            for(int j = 0; j < i; j++) {
+                if(foo[j] > foo[j+1]) {
+                    int temp = foo[j];
+                    foo[j] = foo[j+1];
+                    foo[j+1] = temp;
+                }
+            }
+        }
     }
 }
 
