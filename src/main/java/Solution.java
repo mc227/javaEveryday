643b21cc019d17ead4b1ad4e68f232630824ed68
mc227 <markcostales.mark@gmail.com>
2022-06-16 17:01:14 -0400

    BufferedInputStream example

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,40 +1,14 @@
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
-        new Thread(new DecoratorRunnableImpl(new DecoratorMyRunnableImpl(new RunnableImpl()))).start();
-    }
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("data.txt");
 
-    public static class RunnableImpl implements Runnable {
-        @Override
-        public void run() {
-            System.out.println("RunnableImpl body");
-        }
-    }
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, 200);
 
-    public static class DecoratorRunnableImpl implements Runnable {
-        private Runnable component;
-
-        public DecoratorRunnableImpl(Runnable component) {
-            this.component = component;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("DecoratorRunnableImpl body");
-            component.run();
-        }
-    }
-
-    public static class DecoratorMyRunnableImpl implements Runnable {
-        private Runnable component;
-
-        public DecoratorMyRunnableImpl(Runnable component) {
-            this.component = component;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("DecoratorMyRunnableImpl body");
-            component.run();
+        int i;
+        while ((i = bufferedInputStream.read()) != -1){
+            System.out.print((char) i);
         }
     }
 }
