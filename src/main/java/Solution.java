53c3ad78e97dd5e80446e4f2968a2c84a346693a
mc227 <markcostales.mark@gmail.com>
2022-06-16 17:20:45 -0400

    more printf stuff

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,13 +1,19 @@
 import java.io.*;
+import java.util.Locale;
 
 public class Solution {
+    private static final String GREETINGS_MESSAGE = "My name is %s. I am %d!";
+
     public static void main(String[] args) throws IOException {
-        PrintStream printStream = new PrintStream("mark1.txt");
+        PrintStream printStream = new PrintStream("mark2.txt");
 
         printStream.println("Hello!");
-        printStream.println("I'm a robot!");
+        printStream.println("We are robots!");
 
-        printStream.printf("My name is %s. I am %d!", "Amigo", 18);
+
+        printStream.printf(GREETINGS_MESSAGE, "Amigo", 18);
+        printStream.printf(GREETINGS_MESSAGE, "R2-D2", 35);
+        printStream.printf(GREETINGS_MESSAGE, "C-3PO", 35);
 
         printStream.close();
     }
