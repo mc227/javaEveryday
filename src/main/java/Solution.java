5f66010541f2a968e51856bf61cbd9ea9505b3e8
mc227 <markcostales.mark@gmail.com>
2022-06-16 17:12:18 -0400

    printstream example

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,13 +2,9 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        FileInputStream fileInputStream = new FileInputStream("data.txt");
-
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, 200);
-
-        int i;
-        while ((i = bufferedInputStream.read()) != -1){
-            System.out.print((char) i);
-        }
+        PrintStream filePrintStream = new PrintStream(new File("mark.txt"));
+        filePrintStream.println(222);
+        filePrintStream.println("Hello World");
+        filePrintStream.println(false);
     }
 }
