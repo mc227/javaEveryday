dba0a3fac6e49706962e3b4e6941376a77993255
mc227 <markcostales.mark@gmail.com>
2022-06-17 07:24:53 -0400

    using system.in to read the lines one at at time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,21 +5,10 @@ public class Solution {
 
 
     public static void main(String[] args) throws IOException {
-        String greetings = "A stream is, in essence, a moving piece of data.\nIn other words, in programming,\nit " +
-                "isn't water that flows - but rather data in the form of bytes and characters";
-        byte[] bytes = greetings.getBytes();
 
-        InputStream inputStream = new ByteArrayInputStream(bytes);
-
-        System.setIn(inputStream);
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        String str;
-
-        while ((str = reader.readLine())!= null) {
-
-            System.out.println(str);
+        while(true) {
+            int x = System.in.read();
+            System.out.println(x);
         }
 
     }
