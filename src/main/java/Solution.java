dbdca29d62dfb0b5463a73bad64201f43c4d620e
mc227 <markcostales.mark@gmail.com>
2022-06-17 21:04:31 -0400

    delete System.in.read()

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,10 +2,7 @@ import java.io.*;
 
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        while (true) {
-            int x = System.in.read();
-            System.out.println(x);
-        }
+    public static void main(String[] args) {
+
     }
 }
