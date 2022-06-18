98e4eed1b3a66d1cc8d83b24aa6c27d6165a5f68
mc227 <markcostales.mark@gmail.com>
2022-06-17 21:07:35 -0400

    System.in.read() with try catch

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,6 +3,14 @@ import java.io.*;
 public class Solution {
 
     public static void main(String[] args) {
-
+        while(true) {
+            int x = 0;
+            try {
+                x = System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(x);
+        }
     }
 }
