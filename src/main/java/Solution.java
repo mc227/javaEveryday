0caa89a59321427024eb7873d2114631fbd3ebe8
mc227 <markcostales.mark@gmail.com>
2022-06-19 16:44:42 -0400

    this is way harder than i remember

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,95 +1,60 @@
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Solution {
 /*
-Two in one
+Combining files
 
-mark5.txt
-mark6.txt
-mark7.txt
 */
-//    public static void main(String[] args) throws IOException {
-//
-//        try {
-//            // * Read 3 file names from the console.
-//            // •	The program should read a file name three times from the console.
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//            // •	Create an output stream for the first file. For the other two, create input streams.
-//            String fileString = bufferedReader.readLine();
-//            FileOutputStream fileOutputStream = new FileOutputStream(fileString);
-//            // Write the contents of the second file to the first file, and then append the contents of the third
-//            // file to the first file.
-//            //        Close the streams.
-//            FileInputStream fileInputStream1 = new FileInputStream(bufferedReader.readLine());
-//            FileInputStream fileInputStream2 = new FileInputStream(bufferedReader.readLine());
-//            Path p = Paths.get(fileString);
-//            String s = System.lineSeparator() + "New Line!";
-//
-//            // •	The contents of the second file must be copied to the first file.
-//
-//            while(fileInputStream1.available() > 0) {
-//                int data = fileInputStream1.read();
-//                fileOutputStream.write(data);
-//            }
-//            fileOutputStream.write(10);
-//            while(fileInputStream2.available() > 0) {
-//                int data = fileInputStream2.read();
-//                fileOutputStream.write(data);
-//            }
-//
-//            fileOutputStream.close();
-//            fileInputStream1.close();
-//            fileInputStream2.close();
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        /*
-//
-//
-//
-//        •	The contents of the third file must be appended to the first file (after the second file has been written to the first file).
-//        •	The file streams must be closed.*/
-//    }
-public static void main(String[] args) throws IOException {
-    // Read 3 file names from the console.
+    public static void main(String[] args) throws IOException {
+        /*
+        * Read 2 file names from the console.
+        •	The program should read a file name twice from the console.
+        */
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = bufferedReader.readLine();
+        String file2 = bufferedReader.readLine();
+        /*
+        Write the contents of the second file to the beginning of the first file so that the files are combined.
 
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    String string1 = bufferedReader.readLine();
-    String string2 = bufferedReader.readLine();
-    String string3 = bufferedReader.readLine();
+        •	The program should read a file name twice from the console.
+        •	Create an input stream for the first file and read its contents.
+        */
+        // Requirement:Then create an output stream for the first file. And an input stream for the second file.
+        FileOutputStream fileOutputStream = new FileOutputStream(file1);
+        FileInputStream fileInputStream1 = new FileInputStream(file1);
+        // •	The contents of the first and second files must be combined in the first file.
+        FileInputStream fileInputStream2 = new FileInputStream(file2);
+        FileOutputStream fileOutputStream2 = new FileOutputStream(file1,true);
 
-    FileOutputStream outputStream1 = new FileOutputStream(string1);
-    //Create a stream to write bytes to a file
-    FileInputStream inputStream2 = new FileInputStream(string2);
-    FileInputStream inputStream3 = new FileInputStream(string3);
-    // Write the contents of the second
-    // file to the first file,
-    // and then append the
-    // contents of the third file to the first file.
-    while (inputStream2.available() > 0) {
-        // Read the entire file in one batch
-        byte[] buffer = new byte[1000];
-        int count = inputStream2.read(buffer);
-        outputStream1.write(buffer, 0, count);
+        // •	The contents of the second file should go first, then the contents of the first file.
+
+
+        byte[] buffer1 = new byte[fileInputStream1.available()];
+        int count1 = fileInputStream2.read(buffer1);
+        System.out.println(Arrays.toString(buffer1));
+
+
+        byte[] buffer2 = new byte[fileInputStream2.available()];
+        int count2 = fileInputStream2.read(buffer2);
+        fileOutputStream.write(buffer2,0, count2);
+        fileOutputStream2.write(buffer1, 0, count1);
+
+//        byte[] buffer1 = new byte[fileInputStream1.available()];
+//        int count1 = fileInputStream1.read(buffer1);
+//        fileOutputStream.write(buffer1,0,count1);
+
+
+
+
+        /*
+        Close the streams.
+        •	The file streams must be closed.
+        */
+        fileOutputStream.close();
+        fileInputStream2.close();
+//        fileInputStream1.close();
+        bufferedReader.close();
+
     }
-
-    while (inputStream3.available() > 0) {
-        // Read the entire file in one batch
-        byte[] buffer = new byte[1000];
-        int count = inputStream3.read(buffer);
-        outputStream1.write(buffer, 0, count);
-    }
-
-    inputStream2.close();
-    outputStream1.close();
-    inputStream3.close();
-
-    // Close the streams.
-
-}
 }
