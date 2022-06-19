cbf82639dbca607fe2b4dd9a67e0abcc5175dce0
mc227 <markcostales.mark@gmail.com>
2022-06-19 10:14:39 -0400

    review com.codegym.task.task18.task1816 ABCs

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Solution {
 /*
@@ -13,8 +18,26 @@ Requirements:
 •	You must count both uppercase and lowercase letters.
 •	The stream used to read the file must be closed.
 * */
-    public static void main(String[] args) {
-        // The first parameter of the main method is a file name.
-        System.out.println(args[0]);
+    public static void main(String[] args) throws IOException {
+        /*The first parameter of the main method is a file name.*/
+        //System.out.println(args[0]);
+        /*Count the letters of the English alphabet in the file.*/
+        FileInputStream fileInputStream = new FileInputStream(args[0]);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int count = 0;
+
+        while(fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            arrayList.add(data);
+        }
+//        System.out.println(arrayList);
+        for(Integer item: arrayList) {
+            if((item >=65 && item <=90)||(item >=97 && item <=122)) {
+                count++;
+            }
+        }
+        /*Display the number of letters.*/
+        System.out.println(count);
+        fileInputStream.close();
     }
 }
