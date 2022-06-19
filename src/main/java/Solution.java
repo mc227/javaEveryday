b5e7747e5859441f289c8a65985a1157b91cf48b
mc227 <markcostales.mark@gmail.com>
2022-06-19 14:13:47 -0400

    they yield the same answer

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,29 +2,35 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Solution {
 /*
+Spaces
+
 The first parameter of the main method is a file name.
-Count the letters of the English alphabet in the file.
-Display the number of letters.
-Close the streams.
+Display the ratio of the number of spaces to the number of all characters. For example, 10.45.
+1. Count all the characters (n1).
+2. Count the spaces (n2).
+3. Display n2/n1*100, rounding to 2 decimal places.
+4. Close the streams.
 
 Requirements:
 •	You don't need to read anything from the console.
 •	Create a stream to read from the file passed as the first argument of the main method.
-•	You need to count the letters of the English alphabet in the file and display the number.
-•	You must count both uppercase and lowercase letters.
+•	Calculate and display the ratio of the spaces to all characters in the file.
+•	The displayed value must be rounded to 2 decimal places.
 •	The stream used to read the file must be closed.
 * */
     public static void main(String[] args) throws IOException {
         /*The first parameter of the main method is a file name.*/
         //System.out.println(args[0]);
-        /*Count the letters of the English alphabet in the file.*/
+        /*Display the ratio of the number of spaces to the number of all characters. For example, 10.45.*/
         FileInputStream fileInputStream = new FileInputStream(args[0]);
         ArrayList<Integer> arrayList = new ArrayList<>();
-        int count = 0;
+        int characters = 0;
+        int spaces = 0;
 
         while(fileInputStream.available() > 0) {
             int data = fileInputStream.read();
@@ -33,11 +39,49 @@ Requirements:
 //        System.out.println(arrayList);
         for(Integer item: arrayList) {
             if((item >=65 && item <=90)||(item >=97 && item <=122)) {
-                count++;
+                characters++;
+            }
+            if(item.equals(32)) {
+                spaces++;
             }
         }
-        /*Display the number of letters.*/
-        System.out.println(count);
+
+//        System.out.println(characters);
+        System.out.printf("%.2f",((float) spaces/characters)*100);
         fileInputStream.close();
     }
+
+    /*compared below to above. below was the one i did a few weeks ago*/
+//    private static final DecimalFormat df = new DecimalFormat("0.00");
+//    public static void main(String[] args) throws IOException {
+//
+////        The first parameter of the main method is a file name.
+//        // 1. Count all the characters (n1).
+//
+//        FileInputStream inputStream = new FileInputStream(args[0]);
+//        ArrayList<Integer> list = new ArrayList<>();
+//        while(inputStream.available()>0){
+//            int temp = inputStream.read();
+//            list.add(temp);
+//        }
+//        int n1=0;
+//
+//        for(int i = 0; i < list.size(); i++) {
+//            if(list.get(i) > 32 && list.get(i) < 127) {
+//                n1++;
+//            }
+//        }
+////        2. Count the spaces (n2).
+//        int n2 = 0;
+//        for(int i = 0; i < list.size(); i++) {
+//            if(list.get(i) == 32) {
+//                n2++;
+//            }
+//        }
+////        System.out.println(n1);
+////        System.out.println(n2);
+//        float value = (float) n2/(n1)*100;
+//        System.out.println(df.format(value));
+//        inputStream.close();
+//    }
 }
