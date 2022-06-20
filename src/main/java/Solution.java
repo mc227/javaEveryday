ff36780b7c98e17c7768f3eb3b192817e439aded
mc227 <markcostales.mark@gmail.com>
2022-06-20 12:09:13 -0400

    Rounding numbers 1 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,30 +1,29 @@
+
+
 import java.io.*;
 
+/* 
+Rounding numbers
+
+*/
+
 public class Solution {
-    // Combining files
     public static void main(String[] args) throws IOException {
-        // Read 2 file names from the console.
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String filename1 = bufferedReader.readLine();
         String filename2 = bufferedReader.readLine();
-        // Write the contents of the second file to the beginning of the first file so that the files are combined.
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        try(FileInputStream fileInputStream1 = new FileInputStream(filename1);
-            FileInputStream fileInputStream2 = new FileInputStream(filename2)) {
-            while(fileInputStream2.available() > 0) {
-                byteArrayOutputStream.write(fileInputStream2.read());
+
+        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename1));
+            PrintWriter printWriter = new PrintWriter(new FileWriter(filename2))) {
+
+            while(bufferedFileReader.ready()) {
+                String[] splittedLine = bufferedFileReader.readLine().split(" ");
+                for(String item: splittedLine) {
+                    double parsedDouble = Double.parseDouble(item);
+                    long roundedItem = Math.round(parsedDouble);
+                    printWriter.print(roundedItem + " ");
+                }
             }
-            while(fileInputStream1.available() > 0) {
-                byteArrayOutputStream.write(fileInputStream1.read());
-            }
-            fileInputStream1.close();
-            fileInputStream2.close();
         }
-        try (FileOutputStream fileOutputStream = new FileOutputStream(filename1)) {
-            byteArrayOutputStream.writeTo(fileOutputStream);
-        }
-        bufferedReader.close();
-        byteArrayOutputStream.close();
-        // Close the streams.
     }
 }
