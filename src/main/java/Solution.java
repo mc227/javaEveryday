03afea69ffc0655eb3f58062367dd6d9b996d732
mc227 <markcostales.mark@gmail.com>
2022-06-20 15:23:43 -0400

    Symbol frequency

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,30 +4,26 @@ import java.io.*;
 import java.util.Arrays;
 
 /*
-Rounding Numbers
+Symbol frequency
 
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // Read 2 file names from the console.
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String filename1 = bufferedReader.readLine();
-        String filename2 = bufferedReader.readLine();
-        //The first file contains real (fractional) numbers, separated by spaces. For example, 3.1415.
-        //Round the numbers to integers and write them, separated by spaces, to the second file.
-        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename1));
-            PrintWriter printWriter = new PrintWriter(new FileWriter(filename2))) {
-            while(bufferedFileReader.ready()) {
-                String[] splittedLine = bufferedFileReader.readLine().split(" ");
-                for(String item: splittedLine) {
-                    double parsedDouble = Double.parseDouble(item);
-                    long roundNumber = Math.round(parsedDouble);
-                    printWriter.print(roundNumber + " ");
-
+        //The program is started with one argument: the name of a file that contains text.
+        //Calculate how often each symbol is encountered.
+        try(FileReader fileReader = new FileReader(args[0])) {
+            int[] aSCII = new int[128];
+            while(fileReader.ready()) {
+                aSCII[fileReader.read()]++;
+            }
+            for(int i = 0; i < aSCII.length; i++){
+                if(aSCII[i] != 0) {
+                    System.out.println((char) i + " " + aSCII[i]);
                 }
             }
         }
-        //Close the streams.
+
+        //Sort the results by increasing ASCII code (read about it online).
     }
 }
