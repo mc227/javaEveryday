0264077c5495a512ac88f2500b5f31223559a63a
mc227 <markcostales.mark@gmail.com>
2022-06-20 15:09:44 -0400

    Combining files 4 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,26 +4,30 @@ import java.io.*;
 import java.util.Arrays;
 
 /*
-Symbol frequency
+Combining files
 
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // The program is started with one argument: the name of a file that contains text.
-        // Calculate how often each symbol is encountered.
-        // Sort the results by increasing ASCII code (read about it online).
-        int[] aSCII = new int[128];
-        FileReader fileReader = new FileReader(args[0]);
-        while(fileReader.ready()) {
-            int data = fileReader.read();
-            aSCII[data]++;
-        }
-
-        for(int i = 0; i < aSCII.length; i++) {
-            if (aSCII[i] != 0) {
-                System.out.println( (char)i + " " + aSCII[i] );
+//        Read 2 file names from the console.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename1 = bufferedReader.readLine();
+        String filename2 = bufferedReader.readLine();
+//                Write the contents of the second file to the beginning of the first file so that the files are combined.
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try(FileInputStream fileInputStream1 = new FileInputStream(filename1);
+            FileInputStream fileInputStream2 = new FileInputStream(filename2)) {
+            while(fileInputStream2.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream2.read());
+            }
+            while(fileInputStream1.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream1.read());
             }
         }
+        try(FileOutputStream fileOutputStream = new FileOutputStream(filename1)){
+            byteArrayOutputStream.writeTo(fileOutputStream);
+        }
+//                Close the streams.
     }
 }
