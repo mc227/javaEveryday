fa6b7704d9f1d41efb3610065f72d2d2c4d6abf9
mc227 <markcostales.mark@gmail.com>
2022-06-20 14:07:49 -0400

    Combining Files 4 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,14 +3,32 @@
 import java.io.*;
 
 /* 
+Combining files
 
-Read 2 file names from the console.
-Write the contents of the second file to the beginning of the first file so that the files are combined.
+
 Close the streams.
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+        // Read 2 file names from the console.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename1 = bufferedReader.readLine();
+        String filename2 = bufferedReader.readLine();
+        // Write the contents of the second file to the beginning of the first file so that the files are combined.
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try(FileInputStream fileInputStream1 = new FileInputStream(filename1);
+            FileInputStream fileInputStream2 = new FileInputStream(filename2)) {
+            while(fileInputStream2.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream2.read());
+            }
+            while(fileInputStream1.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream1.read());
+            }
+        }
+        try(FileOutputStream fileOutputStream = new FileOutputStream(filename1)) {
+            byteArrayOutputStream.writeTo(fileOutputStream);
+        }
 
     }
 }
