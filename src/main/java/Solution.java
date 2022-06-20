6430276242b22399977868b58346c7afeff5f692
mc227 <markcostales.mark@gmail.com>
2022-06-20 14:16:42 -0400

    Rounding numbers 2 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,11 +2,11 @@
 
 import java.io.*;
 
-/* 
-Combining files
-
+/*
+Rounding numbers
 
 Close the streams.
+
 */
 
 public class Solution {
@@ -15,20 +15,18 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String filename1 = bufferedReader.readLine();
         String filename2 = bufferedReader.readLine();
-        // Write the contents of the second file to the beginning of the first file so that the files are combined.
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        try(FileInputStream fileInputStream1 = new FileInputStream(filename1);
-            FileInputStream fileInputStream2 = new FileInputStream(filename2)) {
-            while(fileInputStream2.available() > 0) {
-                byteArrayOutputStream.write(fileInputStream2.read());
-            }
-            while(fileInputStream1.available() > 0) {
-                byteArrayOutputStream.write(fileInputStream1.read());
+        // The first file contains real (fractional) numbers, separated by spaces. For example, 3.1415.
+        //Round the numbers to integers and write them, separated by spaces, to the second file.
+        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename1));
+            PrintWriter printWriter = new PrintWriter(new FileWriter(filename2))) {
+            while(bufferedFileReader.ready()) {
+                String[] splittedLine = bufferedFileReader.readLine().split(" ");
+                for(String item: splittedLine) {
+                    double parsedDouble = Double.parseDouble(item);
+                    long roundNumber = Math.round(parsedDouble);
+                    printWriter.print(roundNumber + " ");
+                }
             }
         }
-        try(FileOutputStream fileOutputStream = new FileOutputStream(filename1)) {
-            byteArrayOutputStream.writeTo(fileOutputStream);
-        }
-
     }
 }
