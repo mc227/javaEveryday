033cd5e5bd9625258a032117a63373a229df1a75
mc227 <markcostales.mark@gmail.com>
2022-06-20 10:20:01 -0400

    Combining files 2 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,32 +1,34 @@
 import java.io.*;
 
-// Combining files
 public class Solution {
-
+    // Combining files
+//    Read 2 file names from the console.
+//    Write the contents of the second file to the beginning of the first file so that the files are combined.
+//    Close the streams.
     public static void main(String[] args) throws IOException {
-        //    Read 2 file names from the console.
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String file1 = bufferedReader.readLine();
-        String file2 = bufferedReader.readLine();
-        //    Write the contents of the second file to the beginning of the first file so that the files are combined.
+        String filename1 = bufferedReader.readLine();
+        String filename2 = bufferedReader.readLine();
+
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        try(FileInputStream fileInputStream1 = new FileInputStream(file1);
-            FileInputStream fileInputStream2 = new FileInputStream(file2)){
+        try(FileInputStream fileInputStream1 = new FileInputStream(filename1);
+            FileInputStream fileInputStream2 = new FileInputStream(filename2)){
             while(fileInputStream2.available() > 0) {
                 byteArrayOutputStream.write(fileInputStream2.read());
+
             }
-            while (fileInputStream1.available() > 0) {
+
+            while(fileInputStream1.available() > 0) {
                 byteArrayOutputStream.write(fileInputStream1.read());
+
             }
-            fileInputStream1.close();
             fileInputStream2.close();
+            fileInputStream1.close();
         }
-        try(FileOutputStream fileOutputStream = new FileOutputStream(file1)) {
+        try(FileOutputStream fileOutputStream = new FileOutputStream(filename1)) {
             byteArrayOutputStream.writeTo(fileOutputStream);
-            byteArrayOutputStream.close();
+            fileOutputStream.close();
         }
         bufferedReader.close();
-
     }
-
 }
