b41857860cfbbf4141e3ab0a3aa9e367faf3168a
mc227 <markcostales.mark@gmail.com>
2022-06-20 11:25:31 -0400

    Combining files 3 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,28 +1,30 @@
 import java.io.*;
-import java.util.Arrays;
-// 3.49 3.50 3.51 -3.49 -3.50 -3.51
-// 3 4 4 -3 -3 -4
+
 public class Solution {
-    // Rounding numbers
-    // Read 2 file names from the console.
-    // The first file contains real (fractional) numbers, separated by spaces. For example, 3.1415.
-    // Round the numbers to integers and write them, separated by spaces, to the second file.
-    // Close the streams.
+    // Combining files
     public static void main(String[] args) throws IOException {
+        // Read 2 file names from the console.
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String filename1 = bufferedReader.readLine();
         String filename2 = bufferedReader.readLine();
-
-        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename1));
-            PrintWriter printWriter = new PrintWriter(new FileWriter(filename2))){
-            while (bufferedFileReader.ready()) {
-                String[] splittedFile = bufferedFileReader.readLine().split(" ");
-                for (String item: splittedFile) {
-                    Double parsedDouble = Double.parseDouble(item);
-                    int a = (int) Math.round(parsedDouble);
-                    printWriter.print(a+" ");
-                }
+        // Write the contents of the second file to the beginning of the first file so that the files are combined.
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try(FileInputStream fileInputStream1 = new FileInputStream(filename1);
+            FileInputStream fileInputStream2 = new FileInputStream(filename2)) {
+            while(fileInputStream2.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream2.read());
             }
+            while(fileInputStream1.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream1.read());
+            }
+            fileInputStream1.close();
+            fileInputStream2.close();
         }
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filename1)) {
+            byteArrayOutputStream.writeTo(fileOutputStream);
+        }
+        bufferedReader.close();
+        byteArrayOutputStream.close();
+        // Close the streams.
     }
 }
