4dfdbfa6feaf1807aaf1a39b65adbfc67d39af42
mc227 <markcostales.mark@gmail.com>
2022-06-20 13:59:12 -0400

    just putting in the reps

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,27 +3,14 @@
 import java.io.*;
 
 /* 
-Rounding numbers
 
+Read 2 file names from the console.
+Write the contents of the second file to the beginning of the first file so that the files are combined.
+Close the streams.
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String filename1 = bufferedReader.readLine();
-        String filename2 = bufferedReader.readLine();
 
-        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename1));
-            PrintWriter printWriter = new PrintWriter(new FileWriter(filename2))) {
-
-            while(bufferedFileReader.ready()) {
-                String[] splittedLine = bufferedFileReader.readLine().split(" ");
-                for(String item: splittedLine) {
-                    double parsedDouble = Double.parseDouble(item);
-                    long roundedItem = Math.round(parsedDouble);
-                    printWriter.print(roundedItem + " ");
-                }
-            }
-        }
     }
 }
