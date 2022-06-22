fedc513daf6530efb37f1a1f922d472217c4d976
mc227 <markcostales.mark@gmail.com>
2022-06-21 22:49:27 -0400

    Finding data inside a file 1 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,36 +1,41 @@
-
-
 import java.io.*;
-import java.util.Arrays;
 
 /*
-Symbol frequency
+Finding data inside a file
+
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //The program is started with one argument: the name of a file that contains text.
-        //Calculate how often each symbol is encountered.
-        //Sort the results by increasing ASCII code (read about it online).
-        FileReader fileReader = new FileReader(args[0]);
-        int[] aSCII = new int[128];
-        while(fileReader.ready()) {
-            aSCII[fileReader.read()]++;
-        }
-        for(int i = 0; i < aSCII.length; i++) {
-            if(aSCII[i]!=0){
-                System.out.println((char)i + " " +aSCII[i]);
+        //Read a file name from the console.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //Search the file for information related to the specified id. Display it in the format used in the file.
+        //The program is started with one argument: id (an int).
+        String str;
+        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(bufferedReader.readLine()))){
+            while((str = bufferedFileReader.readLine())!=null) {
+                if(str.startsWith(args[0] + " ")) {
+                    System.out.println(str);
+                    break;
+                }
             }
         }
-        //
-        //Example:
-        //','=44, 's'=115, 't'=116.
-        //
-        //Display the sorted results:
-        //[symbol1] frequency1
-        //[symbol2] frequency2
         //Close the streams.
         //
-        //Example output:
+        //The file data is separated by spaces and stored in the following order:
+        //id productName price quantity
+        //where id is an int
+        //productName is a String — it can contain spaces
+        //price is a double
+        //quantity is an int
+        //
+        //The information for each product is stored on a separate line.
+        //
+        //Requirements:
+        //•	The program should read a file name from the console.
+        //•	Create an input stream for the file.
+        //•	The program should search the file and display information related to the specified id passed as the first
+        // argument.
+        //•	The stream used to read the file must be closed.
     }
 }
