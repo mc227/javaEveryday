f659546e519db222d10b20ea16c8114076f84aff
mc227 <markcostales.mark@gmail.com>
2022-06-21 22:19:20 -0400

    Rounding numbers 4 time

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Arrays;
 
 /*
-Combining files
+Rounding numbers
 */
 
 public class Solution {
@@ -13,29 +13,19 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String filename1 = bufferedReader.readLine();
         String filename2 = bufferedReader.readLine();
-        //Write the contents of the second file to the beginning of the first file so that the files are combined.
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        try(FileInputStream fileInputStream1 = new FileInputStream(filename1);
-            FileInputStream fileInputStream2 = new FileInputStream(filename2))  {
-            while(fileInputStream2.available() > 0) {
-                byteArrayOutputStream.write(fileInputStream2.read());
+        //The first file contains real (fractional) numbers, separated by spaces. For example, 3.1415.
+        //Round the numbers to integers and write them, separated by spaces, to the second file.
+        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename1));
+            PrintWriter printWriter =  new PrintWriter(new FileWriter(filename2))) {
+            while(bufferedFileReader.ready()) {
+                String[] splittedLine = bufferedFileReader.readLine().split(" ");
+                for(String item: splittedLine) {
+                    double parsedDouble = Double.parseDouble(item);
+                    long roundNumber = Math.round(parsedDouble);
+                    printWriter.print(roundNumber + " ");
+                }
             }
-            while(fileInputStream1.available() > 0) {
-                byteArrayOutputStream.write(fileInputStream1.read());
-            }
-        }
-        try(FileOutputStream fileOutputStream = new FileOutputStream(filename1)) {
-            byteArrayOutputStream.writeTo(fileOutputStream);
         }
         //Close the streams.
-        bufferedReader.close();
-        //
-        //Requirements:
-        //•	The program should read a file name twice from the console.
-        //•	Create an input stream for the first file and read its contents.
-        //•	Then create an output stream for the first file. And an input stream for the second file.
-        //•	The contents of the first and second files must be combined in the first file.
-        //•	The contents of the second file should go first, then the contents of the first file.
-        //•	The file streams must be closed
     }
 }
