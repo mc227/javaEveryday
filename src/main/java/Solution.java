53245db9d8f07f424dfeb5ac1ef2c48a3603e096
mc227 <markcostales.mark@gmail.com>
2022-06-24 00:55:08 -0400

    Symbol frequency - 1,2,3,4,5

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,38 +1,28 @@
 
 
 /*
-Rounding numbers
-Read 2 file names from the console.
-The first file contains real (fractional)
-numbers, separated by spaces.
-For example, 3.1415.
-Round the numbers to integers and write them,
-separated by spaces,
-to the second file.
-Close the streams.
-The rounding should work like this: 3.49 => 3 3.50 => 4 3.51 => 4
+Symbol frequency
+The program is started with one argument:
+the name of a file that contains text.
+Calculate how often each symbol is encountered.
+Sort the results by increasing ASCII code (read about it online).
+Example: ','=44, 's'=115, 't'=116.
+Display the sorted results: [symbol1] frequency1 [symbol2] frequency2
 */
 
 import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // Read 2 filenames from the console.
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String filename1 = bufferedReader.readLine();
-        String filename2 = bufferedReader.readLine();
-
-        try(BufferedReader bufferedFileReader= new BufferedReader(new FileReader(filename1));
-            PrintWriter printWriter = new PrintWriter(new FileWriter(filename2))) {
-            while(bufferedFileReader.ready()) {
-                String[] splittedLine = bufferedFileReader.readLine().split(" ");
-                for(String item: splittedLine) {
-                    double parsedDouble = Double.parseDouble(item);
-                    long roundedNumber = Math.round(parsedDouble);
-                    printWriter.print(roundedNumber + " ");
-                }
+        FileReader fileReader = new FileReader(args[0]);
+        int[] aSCII = new int[128];
+        while(fileReader.ready()) {
+            aSCII[fileReader.read()]++;
+        }
+        for(int i = 0; i < aSCII.length; i++) {
+            if(aSCII[i]!=0){
+                System.out.println((char)i+" "+aSCII[i]);
             }
         }
-
     }
 }
