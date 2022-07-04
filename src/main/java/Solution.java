032debab4ba54e93968338e7800b70e1f2ae487d
mc227 <markcostales.mark@gmail.com>
2022-07-04 00:29:11 -0400

    Symbol frequency - 1,2,3,4,5,6,7,8,9,10

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,91 +1,31 @@
-//import java.io.*;
-//import java.nio.charset.StandardCharsets;
-//import java.util.Arrays;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class Solution {
-////    Read file names from the console until the word "exit" is entered.
-////Pass the file name to the ReadThread thread.
-////The ReadThread thread should find the byte that occurs most frequently in the file, and add it to resultMap,
-////where the String parameter is the file name and the Integer parameter is the relevant byte.
-////Close the streams.
-////
-////Requirements:
-////•	The program must read file names from the console until the word "exit" is entered.
-////•	For each file, create and start a ReadThread thread.
-////•	After each thread is start, ReadThread should create its own file input stream.
-////•	Then each thread must find the most frequently occurring byte in its file and add it to resultMap.
-////•	The file input stream in each thread must be closed.
-//    public static Map<String,Integer> resultMap = new HashMap<>();
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        String str;
-//        while(!(str = bufferedReader.readLine()).equals("exit")){
-//            new ReadThread(str).start();
-//        }
-//    }
-//
-//
-//    private static class ReadThread extends Thread{
-//        private String filename;
-//
-//        public ReadThread(String filename) {
-//            this.filename = filename;
-//        }
-//
-//        @Override
-//        public void run() {
-//            byte[] aByte = new byte[256];
-//            try(FileInputStream fileInputStream = new FileInputStream(filename)){
-//                while(fileInputStream.available()>0){
-//                    aByte[fileInputStream.read()]++;
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            int maxCount = 0;
-//            int maxCountByte = 0;
-//            for(int i = 0; i < aByte.length; i++) {
-//                if(aByte[i] > maxCount) {
-//                    maxCount = aByte[i];
-//                    maxCountByte = i;
-//                }
-//            }
-//            resultMap.put(filename,maxCountByte);
-//            System.out.println(resultMap);
-//        }
-//
-//    }
-//}
+
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 /*
-Finding data inside a file
-Read a file name from the console.
-Search the file for information
-related to the specified id.
-Display it in the format used in the file.
+Symbol frequency
 The program is started with one argument:
-id (an int). Close the streams.
-The file data is separated
-by spaces and stored in the following order: id productName
-*/
-
+the name of a file that contains text.
+Calculate how often each symbol is
+encountered. Sort the results by increasing
+ASCII code (read about it online).
+Example: ','=44, 's'=115, 't'=116.
+Display the sorted results: [symbol1] frequency1 [symbol2] frequency2
+Example output:
+, 19
+- 7
+f 361
+* */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String filename = bufferedReader.readLine();
-        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename))){
-            while(bufferedFileReader.ready()){
-                String str;
-                while((str = bufferedFileReader.readLine())!=null){
-                    if(str.startsWith(args[1] + " ")){
-                        System.out.println(str);
-                        break;
-                    }
+        FileReader fileReader = new FileReader(args[0]);
+        int[] aSCII = new int[128];
+        try(BufferedReader bufferedReader = new BufferedReader(fileReader)){
+            while(bufferedReader.ready()){
+                aSCII[bufferedReader.read()]++;
+            }
+            for(int i = 0; i < aSCII.length; i++) {
+                if(aSCII[i]!=0){
+                    System.out.println((char) i + " " + aSCII[i]);
                 }
             }
         }
