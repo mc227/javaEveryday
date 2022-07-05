273c2493efd467675364793994fffdc4440659ef
mc227 <markcostales.mark@gmail.com>
2022-07-05 17:32:48 -0400

    Finding data inside a file - 1,2,3,4,5,6,7,8,9

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,113 +1,30 @@
-//
-//import java.io.*;
-//import java.util.HashMap;
-//import java.util.HashSet;
-//import java.util.Map;
-//import java.util.Set;
-//
-///*
-//Threads and bytes
-//Read file names from the console until the word "exit" is entered.
-//Pass the file name to the ReadThread thread.
-//The ReadThread thread should find the byte that occurs most frequently in the file, and add it to resultMap,
-//where the String parameter is the file name and the Integer parameter is the relevant byte.
-//Close the streams.
-//* */
-//public class Solution {
-//    public static Map<String,Integer> resultMap = new HashMap<>();
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        String readLine;
-//        while(!(readLine = bufferedReader.readLine()).equals("exit")){
-////            System.out.println("hello there");
-//            new ReadThread(readLine).start();
-//        }
-//    }
-//
-//    private static class ReadThread extends Thread{
-//        private String filename;
-//
-//        public ReadThread(String filename) {
-//            this.filename = filename;
-//        }
-//
-//        @Override
-//        public void run() {
-//            byte[] bytesCount = new byte[256];
-//            try(FileInputStream fileInputStream = new FileInputStream(filename)){
-//                while(fileInputStream.available() > 0) {
-//                    bytesCount[fileInputStream.read()]++;
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            int maxCount = 0;
-//            int maxCountByte = 0;
-//            for(int i = 0; i < bytesCount.length; i++){
-//                if(bytesCount[i] > maxCount){
-//                    maxCount = bytesCount[i];
-//                    maxCountByte = i;
-//                }
-//            }
-//            resultMap.put(filename,maxCountByte);
-//            System.out.println(resultMap);
-//        }
-//    }
-//}
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 /*
-Threads and bytes
-
+Finding data inside a file
+Read a file name from the console.
+Search the file for information related to the specified id.
+Display it in the format used in the file.
+The program is started with one argument:
+id (an int). Close the streams.
+The file data is separated by spaces and stored in the following order: id productName
 */
 
 public class Solution {
-    public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
-
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String readString;
-        while (!(readString = reader.readLine()).equals("exit")) {
-            new ReadThread(readString).start();
-        }
-    }
-
-    public static class ReadThread extends Thread {
-        private String fileName;
-
-        public ReadThread(String fileName) {
-            this.fileName = fileName;
-        }
-
-        @Override
-        public void run() {
-            byte[] bytesCount = new byte[256];
-            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
-                while (fileInputStream.available() > 0) {
-                    int aByte = fileInputStream.read();
-                    bytesCount[aByte]++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            int maxCount = 0;
-            int maxCountByte = 0;
-            for (int i = 0; i < bytesCount.length; i++) {
-                if (bytesCount[i] > maxCount) {
-                    maxCount = bytesCount[i];
-                    maxCountByte = i;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = bufferedReader.readLine();
+        try(BufferedReader bufferedFileReader = new BufferedReader(new FileReader(filename))){
+            String str;
+            while((str = bufferedFileReader.readLine())!=null){
+                if(str.startsWith(args[1] + " ")){
+                    System.out.println(str);
+                    break;
                 }
             }
-            resultMap.put(fileName, maxCountByte);
-            System.out.println(resultMap);
         }
     }
 }
-
-
