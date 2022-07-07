7f179867575210eed9adbb5c5968bf422ecbb896
mc227 <markcostales.mark@gmail.com>
2022-07-07 06:04:09 -0400

    Finding data inside a file - 1,2,3,4,5,6,7,8,9,10

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,26 +2,34 @@ import java.io.*;
 import java.util.*;
 
 /*
-Files and exceptions
-Read file names from the console.
-If the file does not exist
-(i.e. an invalid file name is given),
-then catch the FileNotFoundException,
-display the invalid file name,
-and exit the program.
-Close the streams. Don't use System.exit();
+Finding data inside a file - 1,2,3,4,5,6,7,8,9,10
+Read a file name from the console.
+Search the file for information related to the specified id. Display it in the format used in the file.
+The program is started with one argument: id (an int).
+Close the streams.
+
+The file data is separated by spaces and stored in the following order:
+id productName price quantity
+where id is an int
+productName is a String — it can contain spaces
+price is a double
+quantity is an int
+
+The information for each product is stored on a separate line.
+
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        while(true) {
-            String filename = bufferedReader.readLine();
-            try(FileInputStream fileInputStream = new FileInputStream(filename)){
-
-            } catch (FileNotFoundException fileNotFoundException) {
-                System.out.println(filename);
-                break;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = reader.readLine();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))){
+            String str;
+            while((str= bufferedReader.readLine())!=null){
+                if(str.startsWith(args[1] + " ")){
+                    System.out.println(str);
+                    break;
+                }
             }
         }
     }
