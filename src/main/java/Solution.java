6346a82fd2822cd256a37dc7758cf74ece1aa9a5
mc227 <markcostales.mark@gmail.com>
2022-07-09 09:29:00 -0400

    Files and exceptions - 1,2,3

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,64 +1,30 @@
-
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 /*
-Threads and bytes
-Read file names from the console until the word "exit" is entered.
-Pass the file name to the ReadThread thread.
-The ReadThread thread should find the byte that occurs most frequently in the file, and add it to resultMap,
-where the String parameter is the file name and the Integer parameter is the relevant byte.
-Close the streams.
-
-Requirements:
+Files and exceptions - 1,2,3
+Read file names from the console.
+If the file does not exist
+(i.e. an invalid file name is given),
+then catch the FileNotFoundException,
+display the invalid file name,
+and exit the program. Close the streams.
+Don't use System.exit();
 
 */
 
 public class Solution {
-    public static Map<String,Integer> resultMap = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String filename;
-        // Read file names from the console until the word "exit" is entered.
-        while(!(filename = bufferedReader.readLine()).equals("exit")){
-            // Pass the file name to the ReadThread thread.
-            new ReadThread(filename).start();
-        }
-    }
-
-    private static class ReadThread extends Thread {
-        private String filename;
-
-        public ReadThread(String filename) {
-            this.filename = filename;
-        }
-        //The ReadThread thread should find the byte that occurs most frequently in the file, and add it to resultMap,
-        //where the String parameter is the file name and the Integer parameter is the relevant byte.
-        @Override
-        public void run() {
-            byte[] bytesCount = new byte[256];
+        while(true) {
+            String filename = bufferedReader.readLine();
             try(FileInputStream fileInputStream = new FileInputStream(filename)){
-                while(fileInputStream.available() > 0) {
-                    bytesCount[fileInputStream.read()]++;
-                }
-            }catch(IOException e){
-                e.printStackTrace();
+
+            } catch (FileNotFoundException f) {
+                System.out.println(filename);
+                break;
             }
-            int maxCount = 0;
-            int maxCountByte = 0;
-            for(int i = 0; i < bytesCount.length; i++) {
-                if(bytesCount[i] > maxCount){
-                    maxCount = bytesCount[i];
-                    maxCountByte = i;
-                }
-            }
-            resultMap.put(filename, maxCountByte);
-            System.out.println(resultMap);
         }
     }
 }
