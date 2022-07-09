4eba6c77b5943387e8f6903ad78a7255598a756b
mc227 <markcostales.mark@gmail.com>
2022-07-09 10:01:43 -0400

    Building a file practice

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,30 +1,12 @@
-import java.io.*;
-import java.util.*;
-
 /*
-Files and exceptions - 1,2,3
-Read file names from the console.
-If the file does not exist
-(i.e. an invalid file name is given),
-then catch the FileNotFoundException,
-display the invalid file name,
-and exit the program. Close the streams.
-Don't use System.exit();
+Building a file
 
 */
 
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        while(true) {
-            String filename = bufferedReader.readLine();
-            try(FileInputStream fileInputStream = new FileInputStream(filename)){
-
-            } catch (FileNotFoundException f) {
-                System.out.println(filename);
-                break;
-            }
-        }
     }
 }
