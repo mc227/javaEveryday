135c5249f9fae9d57f6a02bece2671cebccd34a3
mc227 <markcostales.mark@gmail.com>
2022-07-10 17:20:28 -0400

    Threads and bytes - 1,2,3,4,5,6,7

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,12 +1,52 @@
 /*
-Building a file
-
+Threads and bytes
+Read file names from the console until the word "exit" is entered.
+Pass the file name to the ReadThread thread.
+The ReadThread thread should find the byte that occurs most frequently in the file, and add it to resultMap,
+where the String parameter is the file name and the Integer parameter is the relevant byte.
+Close the streams.
 */
 
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
+    public static Map<String,Integer> resultMap = new HashMap<>();
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename;
+        while(!(filename=bufferedReader.readLine()).equals("exit")) {
+            new ReadThread(filename).start();
+        }
+    }
 
+    private static class ReadThread extends Thread{
+        private String filename;
+        public ReadThread(String filename) {
+            this.filename = filename;
+        }
+
+        @Override
+        public void run() {
+            byte[] bytesCount = new byte[256];
+            try(FileInputStream fileInputStream = new FileInputStream(filename)){
+                while(fileInputStream.available() > 0) {
+                    bytesCount[fileInputStream.read()]++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int maxCount = 0;
+            int macCountByte = 0;
+            for(int i = 0; i < bytesCount.length; i++) {
+                if(bytesCount[i] > maxCount){
+                    maxCount = bytesCount[i];
+                    macCountByte = i;
+                }
+            }
+            resultMap.put(filename,macCountByte);
+            System.out.println(resultMap);
+        }
     }
 }
