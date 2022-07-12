ca4a5f4a8cad653702867e73426123748f061c4f
mc227 <markcostales.mark@gmail.com>
2022-07-12 17:13:25 -0400

    Files and exceptions - 1,2,3,4,5

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,54 +1,26 @@
 /*
-Threads and bytes
-Read file names from the console until
-the word "exit" is entered. Pass the file
-name to the ReadThread thread. The ReadThread
-thread should find the byte that occurs
-most frequently in the file, and add
-it to resultMap, where the String parameter
-is the file name and the Integer parameter is the resultMap
+Files and exceptions
+Read file names from the console. If the file does not exist
+(i.e. an invalid file name is given), then catch the
+FileNotFoundException, display the invalid file name,
+and exit the program. Close the streams. Don't use System.exit();
 */
 
+
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Solution {
-    public static Map<String,Integer> resultMap = new HashMap<>();
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String filename;
-        while(!(filename = reader.readLine()).equals("exit")){
-            new ReadThread(filename).start();
-        }
-    }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    private static class ReadThread extends Thread{
-        public String filename;
-        public ReadThread(String filename) {
-            this.filename = filename;
-        }
-
-        @Override
-        public void run() {
-            byte[] bytesCount = new byte[256];
+        while(true) {
+            String filename = bufferedReader.readLine();
             try(FileInputStream fileInputStream = new FileInputStream(filename)){
-                while(fileInputStream.available() > 0) {
-                    bytesCount[fileInputStream.read()]++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+
+            } catch (FileNotFoundException fileNotFoundException) {
+                System.out.println(filename);
+                break;
             }
-            int maxCount = 0;
-            int maxCountByte = 0;
-            for(int i = 0; i < bytesCount.length; i++) {
-                if(bytesCount[i] > maxCount) {
-                    maxCount = bytesCount[i];
-                    maxCountByte = i;
-                }
-            }
-            resultMap.put(filename,maxCountByte);
-            System.out.println(resultMap);
         }
     }
 }
