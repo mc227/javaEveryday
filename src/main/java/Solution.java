e1238f920007586c7ddb4dce514ca5cb66a43e1a
mc227 <markcostales.mark@gmail.com>
2022-07-12 17:29:58 -0400

    26

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,12 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        Set<String> files = new TreeSet<>();
+        Set<String> files = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] parts1 = o1.split(".part");
+                String[] parts2 = o2.split(".part");
+                int number1 = Integer.parseInt(parts1[parts1.length()-1]);
+                int number2 = Integer.parseInt(parts2[parts2.length()-1]);
+                return number1 - number2;
+            }
+        });
+        String outputFile = null;
     }
 }
