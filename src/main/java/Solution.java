89861c3bc3680d8ba49a3e4242382aeb9f33d2d3
mc227 <markcostales.mark@gmail.com>
2022-07-11 23:11:45 -0400

    Threads and bytes - 1,2,3,4,5,6,7,8

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,9 +2,9 @@
 Threads and bytes
 Read file names from the console until the word "exit" is entered.
 Pass the file name to the ReadThread thread.
-The ReadThread thread should find the byte that occurs most frequently in the file, and add it to resultMap,
-where the String parameter is the file name and the Integer parameter is the relevant byte.
-Close the streams.
+The ReadThread thread should find the byte that occurs most frequently
+in the file, and add it to resultMap, where the String parameter
+is the file name and the Integer parameter is the re
 */
 
 import java.io.*;
@@ -12,17 +12,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    public static Map<String,Integer> resultMap = new HashMap<>();
+    public static Map<String, Integer> resultMap = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String filename;
-        while(!(filename=bufferedReader.readLine()).equals("exit")) {
+        while(!(filename = bufferedReader.readLine()).equals("exit")){
             new ReadThread(filename).start();
         }
     }
 
     private static class ReadThread extends Thread{
         private String filename;
+
         public ReadThread(String filename) {
             this.filename = filename;
         }
@@ -38,14 +39,14 @@ public class Solution {
                 e.printStackTrace();
             }
             int maxCount = 0;
-            int macCountByte = 0;
+            int maxCountByte = 0;
             for(int i = 0; i < bytesCount.length; i++) {
-                if(bytesCount[i] > maxCount){
+                if(bytesCount[i] > maxCount) {
                     maxCount = bytesCount[i];
-                    macCountByte = i;
+                    maxCountByte = i;
                 }
             }
-            resultMap.put(filename,macCountByte);
+            resultMap.put(filename,maxCountByte);
             System.out.println(resultMap);
         }
     }
