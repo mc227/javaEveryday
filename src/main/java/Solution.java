53328f214abec276f99273ba4c9a455accef911c
mc227 <markcostales.mark@gmail.com>
2022-07-13 17:15:16 -0400

    I can't find the difference

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,59 +1,61 @@
 
 
 /*
-Threads and bytes
-Read file names from the console until the word "exit" is entered.
-Pass the file name to the ReadThread thread.
-The ReadThread thread should find the byte that occurs most frequently in the file, and add it to resultMap,
-where the String parameter is the file name and the Integer parameter is the relevant byte.
+Building a file
+Let's build a file from various pieces.
+Read file names from the console.
+Each file has a name: <someName>.partN.
+
+For example, Lion.avi.part1, Lion.avi.part2, ..., Lion.avi.part37.
+
+The file names are supplied in random order. The word "end" is used to stop reading in file names.
+In the folder where all the files are located, create a file without the "part" suffix, i.e. without ".<partN>".
+
+For example, Lion.avi.
+
+Use a buffer to copy all the bytes from the partial files to the created file.
+Copy the first in the proper order, first the first part, then the second, ..., finally - the last part.
 Close the streams.
 * */
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Solution {
-    public static Map<String,Integer> resultMap = new HashMap<>();
     public static void main(String[] args) throws IOException {
-        // Read file names from the console until the word "exit" is entered.
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String filename;
-        while(!(filename = bufferedReader.readLine()).equals("exit")){
-            new ReadThread(filename).start();
-        }
-    }
-
-    private static class ReadThread extends Thread{
-        public String filename;
-
-        public ReadThread(String filename) {
-            this.filename = filename;
-        }
-
-        @Override
-        public void run() {
-            byte[] bytesCount = new byte[256];
-            try(FileInputStream fileInputStream = new FileInputStream(filename)){
-                while(fileInputStream.available() > 0) {
-                    bytesCount[fileInputStream.read()]++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        Set<String> files = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] parts1 = o1.split(".part");
+                String[] parts2 = o2.split(".part");
+                int number1 = Integer.parseInt(parts1[parts1.length-1]);
+                int number2 = Integer.parseInt(parts2[parts2.length-1]);
+                return number1 - number2;
             }
-            int maxCount = 0;
-            int maxCountByte = 0;
-            for(int i = 0; i < bytesCount.length; i++) {
-                if(bytesCount[i] > maxCount){
-                    maxCount = bytesCount[i];
-                    maxCountByte = i;
+        });
+        String outputFile = null;
+        String readString;
+        while(!(readString = bufferedReader.readLine()).equals("ends")){
+            files.add(readString);
+            if(outputFile == null) {
+                int indexOfSuffix = readString.lastIndexOf(".part");
+                outputFile = readString.substring(0,indexOfSuffix);
+            }
+        }
+        if(outputFile == null) return;
+        try(FileOutputStream fileOutputStream = new FileOutputStream(outputFile)){
+            for(String file: files) {
+                try(FileInputStream fileInputStream = new FileInputStream(file)){
+                    byte[] buffer = new byte[fileInputStream.available()];
+                    while(fileInputStream.available() > 0) {
+                        int bytesRead = fileInputStream.read(buffer);
+                        fileOutputStream.write(buffer, 0, bytesRead);
+                    }
                 }
             }
-            resultMap.put(filename,maxCountByte);
-            System.out.println(resultMap);
         }
     }
 }
