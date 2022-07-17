208c7065c4cbc224c39528f88a9a2ac2f722250c
mc227 <markcostales.mark@gmail.com>
2022-07-17 16:49:42 -0400

    Files and exceptions 6

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,95 +1,31 @@
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /* 
-Prices
-
-19847   Swim trunks, blue             159.00  12.0
-19848   product                       10      10
-
+Files and exceptions
+Read file names from the console.
+If the file does not exist
+(i.e. an invalid file name is given),
+then catch the FileNotFoundException,
+display the invalid file name,
+and exit the program.
+Close the streams.
+Don't use System.exit();
 
 */
 
+import java.io.*;
+
 public class Solution {
-    public static class Product {
-        int id;
-        String name;
-        String price;
-        String quantity;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            String readString = bufferedReader.readLine();
+            try(FileInputStream fileInputStream = new FileInputStream(readString)){
 
-        public Product(int id, String name, String price, String quantity) {
-            this.id = id;
-            this.name = name;
-            this.price = price;
-            this.quantity = quantity;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%-8d%-30s%-8s%-4s", id, name, price, quantity);
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        if (args.length == 0) {
-            return;
-        }
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = reader.readLine();
-
-        List<Product> products = new ArrayList<>();
-
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
-            while (fileReader.ready()) {
-                Product product = getProduct(fileReader.readLine());
-                products.add(product);
+            }catch (FileNotFoundException fileNotFoundException){
+                System.out.println(readString);
+                break;
             }
         }
-
-        switch (args[0]) {
-            case "-c":
-                int id = 0;
-                for (Product product : products) {
-                    if (product.id > id) id = product.id;
-                }
-                String name = "";
-                for (int i = 1; i < args.length - 2; i++) {
-                    name += args[i] + " ";
-                }
-                if (name.length() > 30) {
-                    name = name.substring(0, 30);
-                }
-                String price =  args[args.length - 2];
-                if (price.length() > 8) {
-                    price = price.substring(0, 8);
-                }
-                String quantity = args[args.length - 1];
-                if (quantity.length() > 4) {
-                    quantity = quantity.substring(0, 4);
-                }
-                Product product = new Product(++id, name.trim(), price, quantity);
-                products.add(product);
-        }
-
-        try (FileWriter fileWriter = new FileWriter(fileName)) {
-            for (Product product : products) {
-                fileWriter.write(product.toString());
-                fileWriter.write("\n");
-            }
-        }
-    }
-
-    public static Product getProduct(String string) {
-        String id = string.substring(0, 8).trim();
-        String name = string.substring(8, 38).trim();
-        String price = string.substring(38, 46).trim();
-        String quantity = string.substring(46, 50).trim();
-        return new Product(Integer.parseInt(id), name, price, quantity);
     }
 }
