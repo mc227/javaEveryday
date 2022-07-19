9fc4ff19088de9d11576be9f2d5a25f18b3fe712
mc227 <markcostales.mark@gmail.com>
2022-07-18 21:13:13 -0400

    encryption decryption shadi po

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -28,17 +28,17 @@ import java.io.IOException;
 public class Solution {
     public static void main(String[] args) throws IOException {
         String mode = args[0];
-        if(!(mode.equals("-e") || mode.equals("-d"))) return;
-        try(FileInputStream fileInputStream = new FileInputStream(args[1]);
-            FileOutputStream fileOutputStream = new FileOutputStream(args[2])){
+        if (!(mode.equals("-e") || mode.equals("-d"))) return;
+        try (FileInputStream fileInputStream = new FileInputStream(args[1]);
+             FileOutputStream fileOutputStream = new FileOutputStream(args[2])) {
 
-            if(mode.equals("-e")){
-                while(fileInputStream.available() > 0) {
-                    fileOutputStream.write(fileInputStream.read()+32);
+            if (mode.equals("-e")) {
+                while (fileInputStream.available() > 0) {
+                    fileOutputStream.write(fileInputStream.read() + 32);
                 }
             } else {
-                while(fileInputStream.available() > 0) {
-                    fileOutputStream.write(fileInputStream.read()-32);
+                while (fileInputStream.available() > 0) {
+                    fileOutputStream.write(fileInputStream.read() - 32);
                 }
             }
         }
