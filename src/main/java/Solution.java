dd65c1293a5ac0da53e07cb3228c27a0a1302bbe
mc227 <markcostales.mark@gmail.com>
2022-07-19 07:08:22 -0400

    Files and exceptions - 1,2,3,4,5,6,7,8

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,46 +1,32 @@
+import java.io.*;
+
 /*
-Encryption
-Come up with an encryption/decryption mechanism.
+Files and exceptions
 
-The program runs with one of the following sets of arguments:
--e fileName fileOutputName
--d fileName fileOutputName
 
-where:
-fileName is the name of a file to be encrypted/decrypted.
-fileOutputName is the name of the file where you need to write the result of the encryption/decryption process.
--e indicates that you need to encrypt the data.
--d indicates that you need to decrypt the data.
 
-Requirements:
-•	You don't need to read anything from the console.
-•	Create an input stream for the file passed as the second argument (fileName).
-•	Create an output stream for the file passed as the third argument (fileOutputName).
-•	In "-e" mode, the program should encrypt fileName and write the result to fileOutputName.
-•	In "-d" mode, the program should decrypt fileName and write the result to fileOutputName.
-•	The file streams must be closed.
-*/
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
+Don't use System.exit();
+* */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        String mode = args[0];
-        if (!(mode.equals("-e") || mode.equals("-d"))) return;
-        try (FileInputStream fileInputStream = new FileInputStream(args[1]);
-             FileOutputStream fileOutputStream = new FileOutputStream(args[2])) {
-
-            if (mode.equals("-e")) {
-                while (fileInputStream.available() > 0) {
-                    fileOutputStream.write(fileInputStream.read() + 32);
-                }
-            } else {
-                while (fileInputStream.available() > 0) {
-                    fileOutputStream.write(fileInputStream.read() - 32);
-                }
+        // Read file names from the console.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            String filename = bufferedReader.readLine();
+            try(FileInputStream fileInputStream = new FileInputStream(filename)){
+                // If the file does not exist (i.e. an invalid file name is given),
+                //then catch the FileNotFoundException,
+                // Close the streams.
+                fileInputStream.close();
+            } catch (FileNotFoundException fileNotFoundException){
+                // display the invalid file name,
+                System.out.println(filename);
+                // and exit the program.
+                break;
             }
         }
+        // Close the streams.
+        bufferedReader.close();
     }
 }
