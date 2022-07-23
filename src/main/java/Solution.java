f90b7f9e96dfa776a64ee5933e6f0521ab67498c
mc227 <markcostales.mark@gmail.com>
2022-07-23 17:19:00 -0400

    price

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,31 +1,50 @@
-/*
-Encryption
-Come up with an encryption/decryption mechanism.
-The program runs with one of the following sets
-of arguments: -e fileName fileOutputName
--d fileName fileOutputName where:
-fileName is the name of a file to be encrypted/decrypted.
-* */
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        String mode = args[0];
-        if(!(mode.equals("-e") || mode.equals("-d"))) return;
-        try(FileInputStream fileInputStream = new FileInputStream(args[1]);
-            FileOutputStream fileOutputStream = new FileOutputStream(args[2]);){
-            if (mode.equals("-e")){
-                while(fileInputStream.available() > 0){
-                    fileOutputStream.write(fileInputStream.read() + 4);
-                }
-            } else {
-                while(fileInputStream.available() > 0){
-                    fileOutputStream.write(fileInputStream.read() - 4);
-                }
+    public static class Product{
+        int id;
+        String name;
+        String price;
+        String quantity;
+
+        public Product(int id, String name, String price, String quantity) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%-8d%-30s%-8s%-4s%", id, name, price, quantity);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        if(args.length == 0) return;
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = bufferedReader.readLine();
+        List<Product> products = new ArrayList<>();
+
+        try(BufferedReader fileReader = new BufferedReader(new FileReader(filename))){
+            while (fileReader.ready()){
+                Product product = getProduct(fileReader.readLine());
+                products.add(product);
             }
         }
+
+        switch (args[0]){
+            case "-c":
+
+        }
+    }
+
+    private static Product getProduct(String readLine) {
+        return new Product(1,"name","price","q");
     }
 }
