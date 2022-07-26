c94e72a24f47b6250730b64e5a2308a717b197bb
mc227 <markcostales.mark@gmail.com>
2022-07-25 22:26:53 -0400

    adapter technique

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,64 +1,9 @@
-//public class MyClass {
-//    private String name1 = "Ally";
-//    private String name2 = "Lena";
-//    public void swap()
-//    {
-//        String s = name1;
-//        name1 = name2;
-//        name2 = s;
-//    }
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
+public class MyClass implements Interface2{
+    private Interface1 object;
 
-//public class MyClass
-//{
-//    private String name1 = "Ally";
-//    private String name2 = "Lena";
-//
-//    public void swap()
-//    {
-//        synchronized (this)
-//        {
-//            String s = name1;
-//            name1 = name2;
-//            name2 = s;
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        MyClass myClass = new MyClass();
-//        myClass.swap();
-//        System.out.printf("name1: %s",myClass.name1);
-//        System.out.println();
-//        System.out.printf("name2: %s",myClass.name2);
-//    }
-//}
-
-class MyClass
-{
-    private String name1 = "Ally";
-    private String name2 = "Lena";
-
-    public void swap()
-    {
-        synchronized (this)
-        {
-            String s = name1;
-            name1 = name2;
-            name2 = s;
-        }
+    public MyClass(Interface1 object) {
+        this.object = object;
     }
-
-    public void swap2()
-    {
-        synchronized (this)
-        {
-            String s = name1;
-            name1 = name2;
-            name2 = s;
-        }
-    }
+    // This is where we put the Interface2 methods
+    // that call Interface1 methods
 }
