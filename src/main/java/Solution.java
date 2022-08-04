67fc1b2048ac1ade25461b3e32b1eeac6cfeb523
mc227 <markcostales.mark@gmail.com>
2022-08-03 23:05:07 -0400

    Code gym solution to Picking out numbers

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,44 +1,44 @@
+//package com.codegym.task.task19.task1908;
+
+
+import java.io.*;
+import java.util.ArrayList;
 
 /*
 Picking out numbers
 
 */
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName1 = reader.readLine();
-        String fileName2 = reader.readLine();
 
-        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(fileName1));
-             BufferedWriter printWriter = new BufferedWriter(new FileWriter(fileName2))) {
+        String inputFileName;
+        String outputFileName;
 
-            while (bufferedFileReader.ready()) {
-                String[] splittedLine = bufferedFileReader.readLine().replaceAll("\\W"," ").split(" ");
-                List<String> list = new ArrayList<>();
-                for(int i = 0; i < splittedLine.length; i++) {
-                    if (isInteger(splittedLine[i])){
-                        list.add(splittedLine[i]);
-                    }
-                }
-                for(int i = 0; i < list.size(); i++){
-                    printWriter.write(list.get(i) + " ");
-                }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            inputFileName = reader.readLine();
+            outputFileName = reader.readLine();
+        }
+
+        ArrayList<String> fileContent = new ArrayList<>();
+        try (BufferedReader inputFileReader = new BufferedReader(new FileReader(inputFileName))) {
+            while (inputFileReader.ready()) {
+                fileContent.add(inputFileReader.readLine());
             }
         }
-        reader.close();
-    }
-    public static boolean isInteger(String input) {
-        try {
-            Integer.parseInt( input );
-            return true;
-        }
-        catch( Exception e ) {
-            return false;
+
+        try (BufferedWriter outputFileWriter=new BufferedWriter(new FileWriter(outputFileName))){
+            for (String line : fileContent) {
+                String[] splitLine = line.trim().split(" ");
+                for (String word : splitLine) {
+                    try {
+                        int num = Integer.parseInt(word);
+                        outputFileWriter.write(word + " ");
+                    } catch (Exception ignore) {
+                        /* NOP */
+                    }
+                }
+            }
         }
     }
 }
