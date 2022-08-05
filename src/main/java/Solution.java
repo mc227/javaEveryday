7e2063dbbf23c62accc57e458b1a4cac7d28016a
mc227 <markcostales.mark@gmail.com>
2022-08-04 20:35:25 -0400

    code gym solution to Reader wrapper

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,53 +1,31 @@
 //package com.codegym.task.task19.task1911;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /*
 Reader wrapper
 
 */
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
-        //Save the current PrintStream in a special variable
-        PrintStream consoleStream = System.out;
+        PrintStream defaultPrintStream = System.out;
 
-        //Create a dynamic array
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        //Create an adapter for the PrintStream class
-        PrintStream stream = new PrintStream(outputStream);
-        //Set it as the current System.out
-        System.setOut(stream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream newPrintStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(newPrintStream);
 
-        //Call a function that knows nothing about our changes
         testString.printSomething();
 
-        //Convert the data written to our ByteArray into a string
-        String result = outputStream.toString();
+        System.setOut(defaultPrintStream);
 
-        //Put everything back to the way it was
-        System.setOut(consoleStream);
-
-        //all caps the string
-        StringBuilder stringBuilder = new StringBuilder(result);
-        toUpperCase(stringBuilder);
-        String reverseString = stringBuilder.toString();
-
-        //Output it to the console
-        System.out.println(reverseString);
+        String result = byteArrayOutputStream.toString().toUpperCase();
+        System.out.println(result);
     }
 
-    public static void toUpperCase(StringBuilder builder) {
-        for (int i = 0; i < builder.length(); i++) {
-            if (Character.isLowerCase(builder.charAt(i))) {
-                builder.setCharAt(i,
-                        Character.toUpperCase(builder.charAt(i)));
-            }
-        }
-    }
     public static class TestString {
         public void printSomething() {
             System.out.println("This is text for testing");
