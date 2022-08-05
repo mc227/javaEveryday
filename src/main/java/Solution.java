393cee22f1cbce0f33242e7eea29673eb434400a
mc227 <markcostales.mark@gmail.com>
2022-08-04 22:23:33 -0400

    code gym solution to Problem solving

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,55 +1,38 @@
 //package com.codegym.task.task19.task1914;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /*
 Problem solving
 
 */
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
         PrintStream defaultPrintStream = System.out;
-
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream newPrintStream = new PrintStream(byteArrayOutputStream);
-        System.setOut(newPrintStream);
-
+        System.setOut(new PrintStream(byteArrayOutputStream));
         testString.printSomething();
-
         System.setOut(defaultPrintStream);
 
-        String[] result = byteArrayOutputStream.toString().split(" ");
-        for(int i = 0; i < result.length; i++) {
-            result[i] = result[i].trim();
-        }
-        List<String> list = new ArrayList<String>(Arrays.asList(result));
-        list.remove("");
-        list.remove("=");
-        System.out.println(String.format("%s %s %s = %s", list.get(0),list.get(1), list.get(2),
-                operate(list.get(0),list.get(1), list.get(2))));
-    }
+        String result = byteArrayOutputStream.toString();
+        String[] resultArray = result.split(" ");
+        int firstNum = Integer.parseInt(resultArray[0]);
+        int secondNum = Integer.parseInt(resultArray[2]);
+        int arithmeticResult;
 
-    public static String operate(String first, String operand, String second){
-        if(operand.equals("+")){
-            int sum = Integer.parseInt(first)+Integer.parseInt(second);
-            if (sum >= 0) return String.valueOf(sum);
-        } else if (operand.equals("-")) {
-            int diff = Integer.parseInt(first) - Integer.parseInt(second);
-            if (diff >=0) return String.valueOf(diff);
-        } else if (operand.equals("*")){
-            int product = Integer.parseInt(first) * Integer.parseInt(second);
-            if (product >=0) return String.valueOf(product);
-        } else {
+        if (resultArray[1].trim().equals("+"))
+            arithmeticResult = firstNum + secondNum;
+        else if (resultArray[1].trim().equals("-"))
+            arithmeticResult = firstNum - secondNum;
+        else
+            arithmeticResult = firstNum * secondNum;
 
-        }
-        return null;
+        System.out.println(firstNum+" "+resultArray[1]+" "+secondNum+" = "+arithmeticResult);
     }
 
     public static class TestString {
@@ -58,4 +41,3 @@ public class Solution {
         }
     }
 }
-
