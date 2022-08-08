a3f4b761990a31f504e519e6297564c726b498a1
mc227 <markcostales.mark@gmail.com>
2022-08-07 21:36:11 -0400

    attempting brute force solution of 150. Evaluate Reverse Polish Notation

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,34 @@
 class Solution {
-    public int evalRPN(String[] tokens) {
-        return 0;
+    public static int evalRPN(String[] tokens) {
+        int arithmeticResult;
+        int firstNum = Integer.parseInt(tokens[0]);
+        int secondNum = Integer.parseInt(tokens[1]);
+
+        if (tokens[2].trim().equals("+"))
+            arithmeticResult = firstNum + secondNum;
+        else if (tokens[2].trim().equals("-"))
+            arithmeticResult = firstNum - secondNum;
+        else if (tokens[2].trim().equals("*"))
+            arithmeticResult = firstNum * secondNum;
+        else {
+            arithmeticResult = firstNum / secondNum;
+        }
+
+        int fourthNum = Integer.parseInt(tokens[3]);
+        if (tokens[4].trim().equals("+"))
+            arithmeticResult = arithmeticResult + fourthNum;
+        else if (tokens[4].trim().equals("-"))
+            arithmeticResult = arithmeticResult - fourthNum;
+        else if (tokens[4].trim().equals("*"))
+            arithmeticResult = arithmeticResult * fourthNum;
+        else {
+            arithmeticResult = arithmeticResult / fourthNum;
+        }
+        return arithmeticResult;
     }
 
     public static void main(String[] args) {
-        System.out.println("oh hi Mark");
+        String[] arr = new String[] { "2","1","+","3","*"};
+        System.out.println(evalRPN(arr));
     }
 }
