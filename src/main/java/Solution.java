433d22c5ccacf87f13858284e8f05e3ab445957d
mc227 <markcostales.mark@gmail.com>
2022-08-09 21:31:17 -0400

    solved the first two cases

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,34 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public static int evalRPN(String[] tokens) {
-        int arithmeticResult;
-        int firstNum = Integer.parseInt(tokens[0]);
-        int secondNum = Integer.parseInt(tokens[1]);
+        ArrayList<String> array_list = turnToArrayList(tokens);
+        List<String> arraylist = new ArrayList<>();
+        //here
+        do {
+            arraylist = solve(array_list);
+        } while(array_list.size() > 1);
+        return Integer.parseInt(arraylist.get(0));
+    }
+    public static int operate(int first, int second, String operator){
+        int answer;
+        if (operator.trim().equals("+"))
+            answer = first + second;
+        else if (operator.trim().equals("-"))
+            answer = first - second;
+        else if (operator.trim().equals("*"))
+            answer = first * second;
+        else
+            answer = first / second;
+        return answer;
+    }
+    public static ArrayList<String> turnToArrayList(String[] args) {
+        ArrayList<String> array_list = new ArrayList<String>();
+        for (int i = 0; i < args.length; i++)
+            array_list.add(args[i]);
+        return array_list;
+    }
 
-        if (tokens[2].trim().equals("+"))
-            arithmeticResult = firstNum + secondNum;
-        else if (tokens[2].trim().equals("-"))
-            arithmeticResult = firstNum - secondNum;
-        else if (tokens[2].trim().equals("*"))
-            arithmeticResult = firstNum * secondNum;
-        else {
-            arithmeticResult = firstNum / secondNum;
+    public static int locateOperatorIndex(ArrayList<String> array_list){
+        int locOfOperand = 0;
+        for (int j = 0; j < array_list.size(); j++){
+            if(array_list.get(j).contains("/")||array_list.get(j).contains("+")||array_list.get(j).contains("-")||
+                    array_list.get(j).contains("*")) {
+                locOfOperand = j;
+                break;
+            }
         }
-
-        int fourthNum = Integer.parseInt(tokens[3]);
-        if (tokens[4].trim().equals("+"))
-            arithmeticResult = arithmeticResult + fourthNum;
-        else if (tokens[4].trim().equals("-"))
-            arithmeticResult = arithmeticResult - fourthNum;
-        else if (tokens[4].trim().equals("*"))
-            arithmeticResult = arithmeticResult * fourthNum;
-        else {
-            arithmeticResult = arithmeticResult / fourthNum;
-        }
-        return arithmeticResult;
+        return locOfOperand;
+    }
+    public static List<String> solve(ArrayList<String> array_list){
+        // locate operator index
+        int locOfOperand = locateOperatorIndex(array_list);
+        int result = 0;
+        int first = Integer.parseInt(array_list.get(locOfOperand-2));
+        int second = Integer.parseInt(array_list.get(locOfOperand-1));
+        String operator = array_list.get(locOfOperand);
+        result = operate(first,second, operator);
+        array_list.set(locOfOperand, String.valueOf(result));
+        array_list.remove(locOfOperand-1);
+        array_list.remove(locOfOperand-2);
+        return array_list;
     }
 
     public static void main(String[] args) {
-        String[] arr = new String[] { "2","1","+","3","*"};
+
+        String[] arr = new String[] {"2","1","+","3","*"};
+//        String[] arr = new String[] { "4", "13", "5", "/", "+" };
+//        String[] arr = new String[] {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+
         System.out.println(evalRPN(arr));
     }
 }
