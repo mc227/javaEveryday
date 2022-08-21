e10056f583b6779c2e02d1b0264ca5554ff0c57d
mc227 <markcostales.mark@gmail.com>
2022-08-20 23:36:53 -0400

    books solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,15 +1,27 @@
 public class Solution {
+    ////below is my answer
+//    public static int convertToDecimal(String binary) {
+//        int result = 0;
+//        int conversion = 1;
+//        for(int i = binary.length()-1; i >= 0; i--) {
+//            if (binary.charAt(i)== '1')
+//                result += conversion;
+//            conversion *=2;
+//        }
+//        return result;
+//    }
+    // below is the code in the book
     public static int convertToDecimal(String binary) {
-        int result = 0;
         int conversion = 1;
-        for(int i = binary.length()-1; i >= 0; i--) {
-            if (binary.charAt(i)== '1')
+        int result = 0;
+        for (int i = 1; i <= binary.length(); i++) {
+            if (binary.charAt(binary.length() - i) == '1')
                 result += conversion;
-            conversion *=2;
+            conversion *= 2;
         }
         return result;
     }
     public static void main(String[] args) {
-        System.out.println(convertToDecimal("111"));
+        System.out.println(convertToDecimal("111111"));
     }
 }
