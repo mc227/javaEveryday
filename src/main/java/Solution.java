8a2bae6700f21fa7e5d5b0f856dd26d9cb17c00e
mc227 <markcostales.mark@gmail.com>
2022-08-20 23:30:03 -0400

    my own binary conversion

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,44 +1,15 @@
-import java.util.*;
-import java.util.function.BiFunction;
-// 2 1 + 3 *
-// 4 13 5 / +
-// 10 6 9 3 + -11 * / * 17 + 5 +
 public class Solution {
-    public static Map<String, BiFunction<Integer, Integer,Integer>> OPERATORS = new HashMap<>();
-    static {
-        OPERATORS.put("+",(x1,x2) -> x1+x2);
-        OPERATORS.put("-",(x1,x2) -> x1-x2);
-        OPERATORS.put("*",(x1,x2) -> x1*x2);
-        OPERATORS.put("/",(x1,x2) -> x1/x2);
+    public static int convertToDecimal(String binary) {
+        int result = 0;
+        int conversion = 1;
+        for(int i = binary.length()-1; i >= 0; i--) {
+            if (binary.charAt(i)== '1')
+                result += conversion;
+            conversion *=2;
+        }
+        return result;
     }
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<String>();
-        for(int i = 0; i < args.length; i++) {
-            if(!OPERATORS.containsKey(args[i])){
-                stack.push(args[i]);
-            } else {
-                int number_2 = Integer.parseInt(stack.pop());
-                int number_1 = Integer.parseInt(stack.pop());
-                BiFunction<Integer,Integer,Integer> operand = OPERATORS.get(args[i]);
-                int result = operand.apply(number_1,number_2);
-                stack.push(String.valueOf(result));
-            }
-        }
-        System.out.println(stack.pop());
-    }
-    public int evalRPN(String[] tokens) {
-        Stack<String> stack = new Stack<String>();
-        for(int i = 0; i < tokens.length; i++) {
-            if(!OPERATORS.containsKey(tokens[i])){
-                stack.push(tokens[i]);
-            } else {
-                int number_2 = Integer.parseInt(stack.pop());
-                int number_1 = Integer.parseInt(stack.pop());
-                BiFunction<Integer,Integer,Integer> operand = OPERATORS.get(tokens[i]);
-                int result = operand.apply(number_1,number_2);
-                stack.push(String.valueOf(result));
-            }
-        }
-        return Integer.parseInt(stack.pop());
+        System.out.println(convertToDecimal("111"));
     }
 }
