71e089495f0b448ae8a1146649a38c9e5ef9bccf
mc227 <markcostales.mark@gmail.com>
2022-09-14 13:14:04 -0400

    day 1| Jsoup

--- src/main/java/BinaryToDecimal.java
+++ src/main/java/BinaryToDecimal.java
@@ -1,11 +1,14 @@
 public class BinaryToDecimal {
-    public int convertToDecimal(String binary) {
+    public static int convertToDecimal(String binary) {
         int conversion = 1;
         int result = 0;
+        // 16 8 4 2 1
+        //  1 0 1 1 0
+        // 16+0+4+2+0
         return result;
     }
 
-//    public static void main(String[] args) {
-//        System.out.println(convertToDecimal("0"));
-//    }
+    public static void main(String[] args) {
+        System.out.println(convertToDecimal("10110"));
+    }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,27 +1,13 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Solution {
-    ////below is my answer
-//    public static int convertToDecimal(String binary) {
-//        int result = 0;
-//        int conversion = 1;
-//        for(int i = binary.length()-1; i >= 0; i--) {
-//            if (binary.charAt(i)== '1')
-//                result += conversion;
-//            conversion *=2;
-//        }
-//        return result;
-//    }
-    // below is the code in the book
-    public static int convertToDecimal(String binary) {
-        int conversion = 1;
-        int result = 0;
-        for (int i = 1; i <= binary.length(); i++) {
-            if (binary.charAt(binary.length() - i) == '1')
-                result += conversion;
-            conversion *= 2;
-        }
-        return result;
-    }
-    public static void main(String[] args) {
-        System.out.println(convertToDecimal("111111"));
+
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://www.ufc.com").get();
+        String title = doc.title();
+        System.out.println(title);
     }
 }
