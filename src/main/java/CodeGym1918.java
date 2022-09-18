0d92aebde620092f71b4bbf04d1d61a7f18757a3
mc227 <markcostales.mark@gmail.com>
2022-09-17 22:36:12 -0400

    official solution to introducing tags

--- src/main/java/CodeGym1918.java
+++ src/main/java/CodeGym1918.java
@@ -1,83 +1,72 @@
+
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+/*
+Introducing tags
+
+*/
 
 public class CodeGym1918 {
-    public static void main(String[] args) throws IOException {
-
-        String fileName;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        fileName = reader.readLine();
-
-        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
-        String newLine;
-        String fileString;
-        StringBuilder sb = new StringBuilder();
-
-        while ((newLine=fileReader.readLine())!=null) {
-            sb.append(newLine);
-        }
-        fileString = sb.toString();
-        reader.close();
-        fileReader.close();
-
-
-        Pattern pattern = Pattern.compile("<.+?>");
-        Matcher matcher = pattern.matcher(fileString);
-        List<List<String>> tagList = new ArrayList<>();
-
-        while (matcher.find()) {
-            List<String> tempList = new ArrayList<>();
-            tempList.add(matcher.group());
-            tempList.add(String.valueOf(matcher.start()));
-            tempList.add(String.valueOf(matcher.end()));
-            tagList.add(tempList);
+    public static void main(String[] args) {
+        String fileName = null;
+        try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = consoleReader.readLine();
+        } catch (IOException ignore) {
         }
 
+        StringBuilder readFileContent = new StringBuilder();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            while (fileReader.ready()) {
+                readFileContent.append(fileReader.readLine());
+            }
+        } catch (IOException ignore) {
+        }
 
+        String fileContent = readFileContent.toString().replaceAll("[\\r\\n]+", "");
+        System.out.println(fileContent); //M
+        System.out.println("******************"); //M
 
-        Stack<String> tagStack = new Stack<>();
-        List<String> printList = new ArrayList<>();
+        String tag = args[0];
+        String openedTag = "<" + tag;
+        String closedTag = "</" + tag;
+        int openedTagIndex = fileContent.indexOf(openedTag);
+        System.out.println(openedTagIndex); //M
+        System.out.println("******************"); //M
+        int closedTagIndex = fileContent.indexOf(closedTag);
+        System.out.println(closedTagIndex); //M
+        System.out.println("******************"); //M
+        int openedTagCount = 0;
+        int closedTagCount = 0;
+        ArrayList<Integer> openedTagsIndexes = new ArrayList<>();
+        ArrayList<Integer> closedTagsIndexes = new ArrayList<>();
+        ArrayList<Integer> tempClosedTagsIndexes = new ArrayList<>();
 
-
-        for (int i = 0; i < tagList.size(); i++) {
-            String tag = tagList.get(i).get(0);
-
-            for (String s : args) {
-                if (tag.startsWith("<"+s)) {
-                    Integer startIndex = Integer.valueOf(tagList.get(i).get(1));
-                    tagStack.push(tagList.get(i).get(0));
-
-                    innerWhileBlock:
-                    while (!tagStack.empty()) {
-                        for (int j = i + 1; j < tagList.size(); j++) {
-                            if (!tagList.get(j).get(0).startsWith("</")) {
-                                tagStack.push(tagList.get(j).get(0));
-                                System.out.println(tagStack);
-                            } else if (tagList.get(j).get(0).startsWith("</")) {
-                                if (tagStack.peek().contains(tagList.get(j).get(0).replace("/", "")
-                                        .replace("<", "").replace(">", ""))) {
-                                    tagStack.pop();
-                                    if (tagStack.empty()) {
-                                        Integer endIndex = Integer.valueOf(tagList.get(j).get(2));
-                                        String outputString = fileString.substring(startIndex, endIndex);
-                                        printList.add(outputString);
-                                        break innerWhileBlock;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+        while (openedTagIndex != -1 || closedTagIndex != -1) {
+            if (openedTagIndex != -1 && openedTagIndex < closedTagIndex) {
+                openedTagsIndexes.add(openedTagIndex);
+                openedTagCount++;
+                openedTagIndex = fileContent.indexOf(openedTag, openedTagIndex + 1);
+            } else if (closedTagCount != -1 && (closedTagIndex < openedTagIndex || openedTagIndex == -1)) {
+                tempClosedTagsIndexes.add(closedTagIndex + tag.length() + 3);
+                closedTagCount++;
+                closedTagIndex = fileContent.indexOf(closedTag, closedTagIndex + 1);
             }
 
+            if (closedTagCount == openedTagCount) {
+                for (int i = tempClosedTagsIndexes.size() - 1; i >= 0; i--) {
+                    closedTagsIndexes.add(tempClosedTagsIndexes.get(i));
+                }
+                tempClosedTagsIndexes.clear();
+            }
         }
-        printList.forEach(System.out::println);
+
+        for (int i = 0; i < openedTagsIndexes.size(); i++) {
+            System.out.println(fileContent.substring(openedTagsIndexes.get(i), closedTagsIndexes.get(i)));
+        }
     }
 }
---
+++ src/test/java/CodeGym1918Test.java
@@ -0,0 +1,5 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CodeGym1918Test {
+
+}
