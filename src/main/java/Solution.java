1c4e6d79be6aa600d09a71dc8f40d83caba5c587
mc227 <markcostales.mark@gmail.com>
2022-10-05 12:27:39 -0400

    created swap function in Solution class

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,31 +1,24 @@
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         /*
-        Quicksort Partitioning
-          Partitioning is the process by which we reorder
-          our array so that elements with a value less
-          than our pivot are moved to the left of the
-          pivot and those with a larger value are
-          moved to the right (see Figure 2.2).
-          There are numerous manners in which we can do this.
-          Here, we will describe an easy-to-understand scheme known as Lomuto Partitioning.
-          To get a good perception on this partitioning scheme,
-          it is best to simplify what the algorithm is doing in five simple steps, as follows:
-            1. Pick the right most element of the array as the pivot.
-            2. Start from the left and find the next element that is larger than the pivot.
-            3. Swap this element with the next, which is smaller than pivot element.
-            4. Repeat steps 2 and 3 until no more swapping is possible.
-            5. Swap the first item which is larger than the pivot's value with the pivot itself.
-            To perform efficient partitioning using the steps mentioned, we can make use of
-            two pointers, one pointing to the first item larger than the pivot value and
-            the other used to search for the value that is smaller than pivot value.
+        In the following code,
+        these are the integer pointers named x and i, respectively.
+        The algorithm starts by choosing the pivot as the last item on the input array.
+        It then processes the array from left to right in a single pass using the variable i.
+        If the element currently being processed at i is smaller than the pivot, x is incremented and swapped.
+        Using this technique, variable x is either pointing to a value larger than the pivot or
+        the value of x is the same as i, in which case swapping will not modify the array.
+        Once the loop exits, we perform the final step of swapping the first item that is larger than the
+        pivot's value with the pivot.
         */
 
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
-        int largerThanPivot = 0;
-        int smallerThanPivot = 0;
-        int pivot = numbers[numbers.length-1];
-        System.out.printf("pivot: %s, largerThanPivot: %s, smallerThanPivot: %s%n", pivot, largerThanPivot, smallerThanPivot);
+        Solution solution = new Solution();
+        solution.swap(numbers, 1,2);
+        System.out.println(Arrays.toString(numbers));
+
 
     }
     public void swap(int[] array, int first, int second) {
