cab7d79588507d68fab2281c58a9c21fc47213f2
mc227 <markcostales.mark@gmail.com>
2022-10-07 10:32:59 -0400

    added sortImprovement test

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -12,5 +12,11 @@ class SolutionTest {
                 new int[] {3, 5, 7, 9, 12, 14, 15, 16, 18, 21, 23}, numbers );
     }
 
-
+    @Test
+    void improvementTest() {
+        int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
+        solution.sortImprovement(numbers);
+        Assert.assertArrayEquals(
+                new int[] {3, 5, 7, 9, 12, 14, 15, 16, 18, 21, 23}, numbers );
+    }
 }
