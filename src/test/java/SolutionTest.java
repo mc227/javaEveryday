372a753e3f3353dad1510710f98f43e523664a75
mc227 <markcostales.mark@gmail.com>
2022-10-07 10:07:41 -0400

    bubble sort unit test

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,6 +1,14 @@
-
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     Solution solution = new Solution();
 
+    @Test
+    void firstTest() {
+        int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
+        solution.sort(numbers);
+        Assert.assertArrayEquals(
+                new int[] {3, 5, 7, 9, 12, 14, 15, 16, 18, 21, 23}, numbers );
+    }
 }
