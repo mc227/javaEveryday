6d4ac04e68155db304eea7bfbf54c7a9b0099cd0
mc227 <markcostales.mark@gmail.com>
2022-10-07 10:01:41 -0400

    bubble sort implementation

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,10 +1,11 @@
-
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
         Solution solution = new Solution();
-
+        solution.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 
     /*
@@ -15,8 +16,21 @@ public class Solution {
             if(array[j]>array[j+1])
                 swap(array,j,j+1)
      * */
-    public void bubbleSort(int[] input) {
+    public void sort(int[] input) {
+        int n = input.length;
+        for(int i = 1; i <=n; i++) {
+            for(int j = 0; j < n - 1;j++) {
+                if(input[j] > input[j+1]){
+                    swap(input, j, j+1);
+                }
+            }
+        }
+    }
 
+    private void swap(int[] input, int i, int j) {
+        int temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
     }
 
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,12 +1,6 @@
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+
 
 class SolutionTest {
     Solution solution = new Solution();
-    @Test
-    void swapFirstAndLast() {
-        int[] numbers = new int[]{5, 3, 5, 1, 6, 8, 1, 9, 0, 1, 3, 12};
-        solution.swap(numbers, 0, numbers.length-1);
-        Assert.assertArrayEquals(new int[]{12, 3, 5, 1, 6, 8, 1, 9, 0, 1, 3, 5},numbers);
-    }
+
 }
