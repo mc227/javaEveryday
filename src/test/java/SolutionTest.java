06ee23426af98867ddc693660c62d93d133e02b1
mc227 <markcostales.mark@gmail.com>
2022-10-07 16:03:05 -0400

    binary search unit tests

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,11 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 class SolutionTest {
     Solution solution = new Solution();
 
     @Test
-    void SelectionSortTest() {
+    void TestTrue() {
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
+        assertEquals(solution.binarySearch(7,numbers,0,10),true);
+    }
 
+    @Test
+    void TestFalse() {
+        int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
+        assertEquals(solution.binarySearch(99,numbers,0,10),false);
     }
 }
