c0b472ed5fa951a734f4f18b33d5ac41cc02f8ea
mc227 <markcostales.mark@gmail.com>
2022-10-07 16:35:04 -0400

    QuickSort partition from scratch

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,28 +4,12 @@ public class Solution {
     public static void main(String[] args) {
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
         Solution solution = new Solution();
-        System.out.println(solution.binarySearch(7, numbers,0,10));
+//        System.out.println(solution.binarySearch(7, numbers,0,10));
 
     }
-    /*
-    binarySearch(x, array, start, end)
-    if(start < end)
-        mid = (end - start)/2 + start
-        if (array[mid] == x) return true
-        if (array[mid] > x) return binarySearch(x, array, start, mid - 1)
-        return binarySearch(x, array, mid + 1, end)
-    return false
 
-    * */
-
-    public boolean binarySearch(int x, int[] array, int start, int end){
-        if(start < end) {
-            int mid = (end - start)/2 + start;
-            if (array[mid] == x) return true;
-            if (array[mid] > x) return binarySearch(x, array, start, mid - 1);
-            return binarySearch(x, array, mid+1, end);
-        }
-        return false;
+    private int partition(int[] numbers, int start, int end) {
+        return 0;
     }
 
     private void swap(int[] input, int i, int j) {
