e5e4edc1dc6908584b500512188d6c7e92b61d09
mc227 <markcostales.mark@gmail.com>
2022-10-07 15:38:00 -0400

    binary search recursion from scratch

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,34 +1,10 @@
-import java.util.Arrays;
+
 
 public class Solution {
     public static void main(String[] args) {
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
         Solution solution = new Solution();
-        solution.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-    }
 
-    /*
-    selectionSort(array)
-    n = length(array)
-    for(i = 0 to n - 1)
-        pointer = i
-        for(j = i  to n - 1)
-            if(array[pointer] > array[j])
-                pointer = j
-        swap(array,i,pointer)
-
-    * */
-    public void sort(int[] array) {
-        for(int i = 0; i < array.length; i++){
-            int minPtr = i;
-            for(int j = i; j < array.length; j++){
-                if(array[minPtr] > array[j]){
-                    minPtr = j;
-                }
-            }
-            swap(array, minPtr, i);
-        }
     }
 
     private void swap(int[] input, int i, int j) {
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
@@ -7,8 +6,6 @@ class SolutionTest {
     @Test
     void SelectionSortTest() {
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
-        solution.sort(numbers);
-        Assert.assertArrayEquals(
-                new int[] {3, 5, 7, 9, 12, 14, 15, 16, 18, 21, 23}, numbers );
+
     }
 }
