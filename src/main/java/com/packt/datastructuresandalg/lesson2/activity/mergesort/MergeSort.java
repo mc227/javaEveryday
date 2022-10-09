44698132ebe6bd822e9ab68cdb7224188e5082b7
mc227 <markcostales.mark@gmail.com>
2022-10-09 13:21:15 -0400

    merge pseudocode

--- src/main/java/com/packt/datastructuresandalg/lesson2/activity/mergesort/MergeSort.java
+++ src/main/java/com/packt/datastructuresandalg/lesson2/activity/mergesort/MergeSort.java
@@ -2,6 +2,20 @@ package com.packt.datastructuresandalg.lesson2.activity.mergesort;
 
 public class MergeSort {
 
+    /*
+    merge(array, start, middle, end)
+        i = start
+        j = middle + 1
+        arrayTemp = initArrayOfSize(end - start + 1)
+        for(k = 0 to end - start)
+            if(i <= middle && (j < end || (array[i] <= array[j])
+                arrayTemp[k] = array[i]
+                i++
+            else
+                arrayTemp[k] = array[j]
+                j++
+        copyArray(arrayTemp,array,start)
+    * */
     private void merge(int[] array, int start, int middle, int end) {
 
     }
