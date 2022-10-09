46874aaa2239602e4a68b58e5d4f0271b57ceaba
mc227 <markcostales.mark@gmail.com>
2022-10-09 19:28:06 -0400

    mergeSort finally

--- src/main/java/com/packt/datastructuresandalg/lesson2/activity/mergesort/MergeSort.java
+++ src/main/java/com/packt/datastructuresandalg/lesson2/activity/mergesort/MergeSort.java
@@ -2,42 +2,32 @@ package com.packt.datastructuresandalg.lesson2.activity.mergesort;
 
 public class MergeSort {
 
-    /*
-    merge(array, start, middle, end)
-        i = start
-        j = middle + 1
-        arrayTemp = initArrayOfSize(end - start + 1)
-        for(k = 0 to end - start)
-            if(i <= middle && (j > end || (array[i] <= array[j])
-                arrayTemp[k] = array[i]
-                i++
-            else
-                arrayTemp[k] = array[j]
-                j++
-        copyArray(arrayTemp,array,start)
-    * */
     private void merge(int[] array, int start, int middle, int end) {
-
+        int i = start;
+        int j = middle + 1;
+        int[] arrayTemp = new int[end-start+1];
+        for(int k = 0; k < arrayTemp.length; k++) {
+            if(i <= middle && (j > end || array[i] < array[j])){
+                arrayTemp[k] = array[i];
+                i++;
+            } else {
+                arrayTemp[k] = array[j];
+                j++;
+            }
+        }
+        System.arraycopy(arrayTemp,0,array,start,arrayTemp.length);
     }
 
-    /*
-    mergeSort(array,start,end)
-    if(start < end)
-        middle = (end - start) / 2 + start
-        mergeSort(array,start,middle)
-        mergeSort(array,middle+1,end)
-        merge(array,start,middle,end)
-    * */
     private void mergeSort(int[] array, int start, int end) {
         if(start < end) {
-            int middle = (end - start)/2 + start;
+            int middle = (end - start) / 2 + start;
             mergeSort(array, start, middle);
-            mergeSort(array, middle + 1, end);
+            mergeSort(array, middle+1, end);
             merge(array,start,middle,end);
         }
     }
 
     public void mergeSort(int[] array) {
-        mergeSort(array,0, array.length - 1);
+        mergeSort(array,0, array.length-1);
     }
 }
