64412b47b610f500cfba49506d3019e50cc6b623
mc227 <markcostales.mark@gmail.com>
2022-10-10 10:08:15 -0400

    Bubble sort pseudo code

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,5 +5,11 @@ public class Solution {
         Solution solution = new Solution();
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
     }
-
+    // pseudo code for bubble sort
+    // bubbleSort(array)
+    //  n = length(array)
+    //  for(i = 1 to n )
+    //      for(j = 0 to n - 1)
+    //          if(array[j] > array[j+1])
+    //              swap(array, j, j+1)
 }
