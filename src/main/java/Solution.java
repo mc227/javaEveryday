c3ba5053581d855e15c23330ad712a5f2a1cc4da
mc227 <markcostales.mark@gmail.com>
2022-10-10 22:29:09 -0400

    sortImprovement

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
-        solution.sort(numbers);
+        solution.sortImprovement(numbers);
         System.out.println(Arrays.toString(numbers));
     }
     // pseudo code for bubble sort
@@ -23,6 +23,17 @@ public class Solution {
             }
         }
     }
+
+    public void sortImprovement(int[] numbers){
+        for(int i = 1; i < numbers.length; i++) {
+            for(int  j = 0; j < numbers.length - i; j++){
+                if(numbers[j] > numbers[j+1]){
+                    swap(numbers,j,j+1);
+                }
+            }
+        }
+    }
+
     public void swap(int[] array, int first, int second) {
         int temp = array[first];
         array[first] = array[second];
