5c5065c4d2c5449ac81001c0a74da4078683657f
mc227 <markcostales.mark@gmail.com>
2022-10-11 18:55:19 -0400

    selectionSort sorts correctly but with wrong pseudo code. done through debugger

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
+        solution.selectionSort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
     public void swap(int[] array, int first, int second) {
@@ -13,14 +14,26 @@ public class Solution {
     }
     /*SelectionSort PseudoCode
 
-    [5,4,3,2,1]
     selectionSort(array)
         minPtr = 0;
         while(minPtr < array.length)
             for(i = minPtr to array.length - 1)
-            if(array[minPtr] < array[i])
+            if(array[minPtr] > array[i])
                 minPtr = i
             swap(array,minPtr,i)
             minPtr++;
     * */
+    public void selectionSort(int[] array) {
+        int start = 0;
+        while(start < array.length) {
+            int smallestPtr = start;
+            for(int i = start; i < array.length; i++) {
+                if(array[smallestPtr] > array[i]){
+                    smallestPtr = i;
+                }
+            }
+            swap(array,start,smallestPtr);
+            start++;
+        }
+    }
 }
