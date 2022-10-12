368ea951de3c519d75ca691dbc0a0fd8b45ee3f6
mc227 <markcostales.mark@gmail.com>
2022-10-12 18:33:46 -0400

    the struggle is real

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,27 +1,41 @@
-import com.packt.datastructuresandalg.lesson1.binarysearch.BinarySearch;
+
 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
 //        int[] numbers = new int[]{7, 15, 14, 5, 9, 18, 3, 21, 23, 16, 12};
 //        System.out.println(Arrays.toString(numbers));
-        BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.binarySearch(7, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-        System.out.println(binarySearch.binarySearch(0, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+
+
+        System.out.println(solution.binarySearch(7, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        System.out.println(solution.binarySearch(0, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
 
     }
-    public boolean binarySearch(int[] array, int start,int end, int value) {
+    public boolean binarySearch(int value, int[] array,int start,int end) {
         /*
-        binarySearch(array, start, end, value){
-            middle = (end - start) / 2 + start
-            if(start<end)
+        binarySearch(value,array, start, end){
+            if(start<=end)
+                middle = (end - start) / 2 + start
                 if(array[value]==middle) return true
                 if(array[value] > middle) return binarySearch(array, value, middle+1,end)
                 return binarySearch(array,value,start, middle-1)
+            return false
         }
         * */
-        return true;
+
+        if(start <= end) {
+            int middle = (end - start) / 2 + start;
+            if(array[middle] == value) return true;
+            if(array[middle] > value) return binarySearch(value,array,start, middle-1);
+            return binarySearch(value, array, middle+1,end);
+        }
+        return false;
     }
+
+    public boolean binarySearch(int value, int[] array){
+        return binarySearch(value,array,0,array.length-1);
+    }
+
     public void swap(int[] array, int first, int second) {
         int temp = array[first];
         array[first] = array[second];
