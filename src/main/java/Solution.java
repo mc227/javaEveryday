f65cf4c93f1d6152352cb7c491cf390cb5eee02b
mc227 <markcostales.mark@gmail.com>
2022-10-14 00:28:43 -0400

    created test for partition of quicksort

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,12 +5,23 @@ public class Solution {
         Solution solution = new Solution();
         int[] numbers = new int[]{7,15,14,5,9,18,3,21,23,16,12};
         // 7,15,14,5,9,18,3,21,23,16,12
+//        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,3,12};
         System.out.println(solution.partition(numbers,0,10));
+//        System.out.println(solution.partition(numbers,0,11));
         System.out.println(Arrays.toString(numbers));
 
     }
-    public int partition(int[] array, int start,int end) {
-        return 0;
+    public int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int x = start - 1;
+        for(int i = start; i < end; i++) {
+            if(pivot > array[i]){
+                x++;
+                swap(array,x,i);
+            }
+        }
+        swap(array,end,x+1);
+        return x+1;
     }
     public void swap(int[] array, int first, int second) {
         int temp = array[first];
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,6 +1,5 @@
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.assertArrayEquals;
 
 public class SolutionTest{
     Solution solution = new Solution();
@@ -8,39 +7,40 @@ public class SolutionTest{
     @Test
     public void testEvenElements() {
         int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,3,12};
-
-        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12}, numbers);
+//        solution.partition(numbers,0,11);
+//        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12}, numbers);
+        Assert.assertEquals(solution.partition(numbers,0,11),11);
     }
-    @Test
-    public void testOddsElements() {
-        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,16,3,12};
-        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12,16}, numbers);
-
-    }
-
-    @Test
-    public void testAlreadySorted() {
-        int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, numbers);
-
-    }
-
-    @Test
-    public void testSameNumber() {
-        int[] numbers = new int[]{3,3,3,3,3};
-        assertArrayEquals(new int[]{3,3,3,3,3}, numbers);
-    }
-
-    @Test
-    public void testEmpty() {
-        int[] numbers = new int[]{};
-        assertArrayEquals(new int[]{}, numbers);
-    }
-
-    @Test
-    public void testOneElement() {
-        int[] numbers = new int[]{8};
-        assertArrayEquals(new int[]{8}, numbers);
-    }
+//    @Test
+//    public void testOddsElements() {
+//        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,16,3,12};
+//        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12,16}, numbers);
+//
+//    }
+//
+//    @Test
+//    public void testAlreadySorted() {
+//        int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+//        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, numbers);
+//
+//    }
+//
+//    @Test
+//    public void testSameNumber() {
+//        int[] numbers = new int[]{3,3,3,3,3};
+//        assertArrayEquals(new int[]{3,3,3,3,3}, numbers);
+//    }
+//
+//    @Test
+//    public void testEmpty() {
+//        int[] numbers = new int[]{};
+//        assertArrayEquals(new int[]{}, numbers);
+//    }
+//
+//    @Test
+//    public void testOneElement() {
+//        int[] numbers = new int[]{8};
+//        assertArrayEquals(new int[]{8}, numbers);
+//    }
 
 }
