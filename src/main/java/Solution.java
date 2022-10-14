5aa50d9aec7886619bdb85f681d100cdd6584b39
mc227 <markcostales.mark@gmail.com>
2022-10-13 23:12:18 -0400

    about to do quicksort paritioning

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,10 @@
-
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-
+        int[] numbers = new int[]{7,15,14,5,9,18,3,21,23,16,12};
+        System.out.println(Arrays.toString(numbers));
 
     }
     public void swap(int[] array, int first, int second) {
@@ -11,4 +12,5 @@ public class Solution {
         array[first] = array[second];
         array[second] = temp;
     }
+
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,57 +1,46 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class SolutionTest{
     Solution solution = new Solution();
 
     @Test
     public void testEvenElements() {
-//        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,3,12};
-        int[] numbers = new int[]{0,1,1,1,3,3,5,5,6,8,9,12};
-//        solution.selectionSort(numbers);
-//        solution.binarySearch(7,numbers);
-        assertEquals(solution.binarySearch(7,numbers), false);
+        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,3,12};
+
+        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12}, numbers);
     }
     @Test
     public void testOddsElements() {
-//        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,16,3,12};
-        int[] numbers = new int[]{0,1,1,1,3,3,5,5,6,8,9,12,16};
-//        solution.selectionSort(numbers);
-//        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12,16}, numbers);
-        assertEquals(solution.binarySearch(16,numbers), true);
+        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,16,3,12};
+        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12,16}, numbers);
+
     }
-    //
+
     @Test
     public void testAlreadySorted() {
-//        int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
         int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-//        solution.selectionSort(numbers);
-//        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, numbers);
-        assertEquals(solution.binarySearch(16,numbers), false);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, numbers);
+
     }
 
     @Test
     public void testSameNumber() {
         int[] numbers = new int[]{3,3,3,3,3};
-//        solution.selectionSort(numbers);
-//        assertArrayEquals(new int[]{3,3,3,3,3}, numbers);
-        assertEquals(solution.binarySearch(16,numbers), false);
+        assertArrayEquals(new int[]{3,3,3,3,3}, numbers);
     }
 
     @Test
     public void testEmpty() {
         int[] numbers = new int[]{};
-//        solution.selectionSort(numbers);
-//        assertArrayEquals(new int[]{}, numbers);
-        assertEquals(solution.binarySearch(16,numbers), false);
+        assertArrayEquals(new int[]{}, numbers);
     }
 
     @Test
     public void testOneElement() {
         int[] numbers = new int[]{8};
-//        solution.selectionSort(numbers);
-//        assertArrayEquals(new int[]{8}, numbers);
+        assertArrayEquals(new int[]{8}, numbers);
     }
 
 }
