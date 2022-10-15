51f48e9666a08383ee401916be27d62e25940925
mc227 <markcostales.mark@gmail.com>
2022-10-14 23:28:48 -0400

    deleted practice pseudo code

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -9,25 +9,7 @@ public class Solution {
     }
 
     /*
-    mergeSort(array, start,end)
-        if(start<end)
-            middle = (end - start)/2 + start
-            mergeSort(array, start, middle)
-            mergeSort(array, middle+1, end)
-            merge(array, start, middle, end)
 
-    merge(array, start,middle,end)
-        i = start
-        j = middle + 1
-        arrayTemp = initializeArrayofSize(end-start+1)
-        for(k = 0 to end - start)
-            if(i <= middle && (j < end || array[i] <= array[j])
-                arrayTemp[k] = array[i]
-                i++
-            else
-                arrayTemp[k] = array[j]
-                j++
-        copyArray(arrayTemp,array,array.length)
     * */
     public void swap(int[] array, int first, int second) {
         int temp = array[first];
