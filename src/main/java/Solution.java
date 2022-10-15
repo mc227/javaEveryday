99d0ac895657bdb2a05ba0d385af2fa22d7139bb
mc227 <markcostales.mark@gmail.com>
2022-10-14 23:35:09 -0400

    almost

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -9,7 +9,25 @@ public class Solution {
     }
 
     /*
+    mergeSort(array, start,end)
+        if(start<end)
+            midPoint = (end - start)/2 + start
+            mergeSort(array,start,midPoint)
+            mergeSort(array, midPoint+1, end)
+            merge(array,start, midPoint, end)
 
+    merge(array,start, middle, end)
+        i = start
+        j = middle + 1
+        arrayTemp = initArrayOfSize(end - start + 1)
+        for(k = 0 to end - start)
+            if(i <= middle && (j < end || array[i] <= array[j])
+                arrayTemp[k] = array[i]
+                i++
+            else
+                arrayTemp[k] = array[j]
+                j++
+        copyArray(arrayTemp, array,start)
     * */
     public void swap(int[] array, int first, int second) {
         int temp = array[first];
