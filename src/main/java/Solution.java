c7ca4a774cbebdeb1427de629e6a105bc33cd754
mc227 <markcostales.mark@gmail.com>
2022-10-17 16:31:12 -0400

    studying protobuf

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,38 +1,7 @@
-import java.util.Arrays;
+
 
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] numbers = new int[]{7,15,14,5,9,18,3,21,23,16,12};
-
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("oh hi Mark");
     }
-
-    /*
-    mergeSort(array, start,end)
-        if(start<end)
-            midPoint = (end - start)/2 + start
-            mergeSort(array,start,midPoint)
-            mergeSort(array, midPoint+1, end)
-            merge(array,start, midPoint, end)
-
-    merge(array,start, middle, end)
-        i = start
-        j = middle + 1
-        arrayTemp = initArrayOfSize(end - start + 1)
-        for(k = 0 to end - start)
-            if(i <= middle && (j < end || array[i] <= array[j])
-                arrayTemp[k] = array[i]
-                i++
-            else
-                arrayTemp[k] = array[j]
-                j++
-        copyArray(arrayTemp, array,start)
-    * */
-    public void swap(int[] array, int first, int second) {
-        int temp = array[first];
-        array[first] = array[second];
-        array[second] = temp;
-    }
-
 }
