27874bcc27cb1d6d96a145ef821f2c77739904c7
mc227 <markcostales.mark@gmail.com>
2022-10-17 18:41:15 -0400

    correct pseudo code for mergeSort

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,6 +2,18 @@
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("oh hi Mark");
+        int[] numbers = new int[] {89,3,66,1,2,54,65,7,11};
+//        System.out.println(Arrays.toString(numbers));
+//        System.out.println(numbers.length);
     }
+    /*
+    mergeSort Pseudo code
+
+    mergeSort(array, start,end)
+        if(start < end)
+            midPoint = (end - start)/2 + start;
+            mergeSort(array, start, midPoint)
+            mergeSort(array,midPoint+1,end)
+            merge(array, start, midPoint, end)
+    * */
 }
