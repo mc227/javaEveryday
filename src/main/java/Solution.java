79e7726d75ace0d2f104fe414300456aef28fd36
mc227 <markcostales.mark@gmail.com>
2022-10-18 18:21:32 -0400

    i think i got merge sort

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,16 +3,53 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] numbers = new int[] {2,1};
+        int[] numbers = new int[] {3,2,1};
         solution.mergeSort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
+
+    private void mergeSort(int[] array) {
+        mergeSort(array,0,array.length-1);
+    }
+
+    // pseudo code
+    /*
+    mergeSort(array, start, end)
+        if(start < end)
+            middle = (end - start)/2 + start
+            mergeSort(array, start, middle)
+            mergeSort(array, middle+1, end)
+            merge(array, start, middle, end)
+    * */
+    private void mergeSort(int[] array, int start, int end) {
+        if(start < end){
+            int middle = (end - start) / 2 + start;
+            mergeSort(array, start, middle);
+            mergeSort(array, middle+1,end);
+            merge(array, start,middle,end);
+        }
+    }
+
+    /* pseudocode
+    merge(array, start, middle, end)
+        i = start
+        j = middle + 1
+        arrayTemp = initArrayOfSize(end - start + 1)
+        for(k = 0 until end - start)
+            if(i <= middle && (j > end || array[i] <= array[j])
+                arrayTemp[k] = array[i]
+                i++
+            else
+                arrayTemp[k] = array[j]
+                j++
+        copyArray(arrayTemp,0,array,start,array.length)
+    * */
     private void merge(int[] array, int start, int middle, int end) {
         int i = start;
         int j = middle + 1;
         int[] arrayTemp = new int[end - start + 1];
-        for (int k = 0; k < arrayTemp.length; k++) {
-            if (i <= middle && (j > end || array[i] <= array[j])) {
+        for(int k = 0; k < arrayTemp.length; k++) {
+            if(i <= middle && (j > end || array[i] <= array[j])){
                 arrayTemp[k] = array[i];
                 i++;
             } else {
@@ -20,19 +57,8 @@ public class Solution {
                 j++;
             }
         }
-        System.arraycopy(arrayTemp, 0, array, start, arrayTemp.length);
+        System.arraycopy(arrayTemp,0,array,start,arrayTemp.length);
     }
 
-    private void mergeSort(int[] array, int start, int end) {
-        if (start < end) {
-            int middle = (end - start) / 2 + start;
-            mergeSort(array, start, middle);
-            mergeSort(array, middle + 1, end);
-            merge(array, start, middle, end);
-        }
-    }
 
-    public void mergeSort(int[] array) {
-        mergeSort(array, 0, array.length - 1);
-    }
 }
