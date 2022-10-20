3390d049fa607f68df307b7fc133734f90272f17
mc227 <markcostales.mark@gmail.com>
2022-10-20 15:52:04 -0400

    linked list from scratch

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,64 +1,13 @@
-import java.util.Arrays;
+import java.util.Optional;
 
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] numbers = new int[] {3,2,1};
-        solution.mergeSort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        String name = null;
+        Optional<String> opt = Optional.ofNullable(name);
+        System.out.println(opt);
     }
-
-    public void mergeSort(int[] array) {
-        mergeSort(array,0,array.length-1);
+    public String getMyDefault() {
+        System.out.println("Getting Default Value");
+        return "Default Value";
     }
-
-    // pseudo code
-    /*
-    mergeSort(array, start, end)
-        if(start < end)
-            middle = (end - start)/2 + start
-            mergeSort(array, start, middle)
-            mergeSort(array, middle+1, end)
-            merge(array, start, middle, end)
-    * */
-    public void mergeSort(int[] array, int start, int end) {
-        if(start < end){
-            int middle = (end - start) / 2 + start;
-            mergeSort(array, start, middle);
-            mergeSort(array, middle+1,end);
-            merge(array, start,middle,end);
-        }
-    }
-
-    /* pseudocode
-    merge(array, start, middle, end)
-        i = start
-        j = middle + 1
-        arrayTemp = initArrayOfSize(end - start + 1)
-        for(k = 0 until end - start)
-            if(i <= middle && (j > end || array[i] <= array[j])
-                arrayTemp[k] = array[i]
-                i++
-            else
-                arrayTemp[k] = array[j]
-                j++
-        copyArray(arrayTemp,0,array,start,array.length)
-    * */
-    public void merge(int[] array, int start, int middle, int end) {
-        int i = start;
-        int j = middle + 1;
-        int[] arrayTemp = new int[end - start + 1];
-        for(int k = 0; k < arrayTemp.length; k++) {
-            if(i <= middle && (j > end || array[i] <= array[j])){
-                arrayTemp[k] = array[i];
-                i++;
-            } else {
-                arrayTemp[k] = array[j];
-                j++;
-            }
-        }
-        System.arraycopy(arrayTemp,0,array,start,arrayTemp.length);
-    }
-
-
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,14 +1,101 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import java.util.Optional;
+
 
 public class SolutionTest{
-    Solution solution = new Solution();
+    public String getMyDefault() {
+        System.out.println("Getting Default Value");
+        return "Default Value";
+    }
+    @Test
+    void whenCreatesEmptyOptional_thenCorrect() {
+        Optional<String> empty = Optional.empty();
+        Assert.assertFalse(empty.isPresent());
+    }
 
     @Test
-    void testEvenElements() {
-        int[] numbers = new int[]{5,3,5,1,6,8,1,9,0,1,3,12};
-        solution.mergeSort(numbers);
-        assertArrayEquals(new int[]{0,1,1,1,3,3,5,5,6,8,9,12}, numbers);
+    void givenNonNull_whenCreatesNonNullable_thenCorrect() {
+        String name = "baeldung";
+        Optional<String> opt = Optional.of(name);
+        Assert.assertTrue(opt.isPresent());
+    }
+
+    @Test
+    void givenNonNull_whenCreatesNullable_thenCorrect() {
+        String name = "baeldung";
+        Optional<String> opt = Optional.ofNullable(name);
+        Assert.assertTrue(opt.isPresent());
+    }
+
+    @Test
+    void givenNull_whenCreatesNullable_thenCorrect() {
+        String name = null;
+        Optional<String> opt = Optional.ofNullable(name);
+        Assert.assertFalse(opt.isPresent());
+    }
+
+    @Test
+    void givenOptional_whenIsPresentWorks_thenCorrect() {
+        Optional<String> opt = Optional.of("Baeldung");
+        Assert.assertTrue(opt.isPresent());
+
+        opt = Optional.ofNullable(null);
+        Assert.assertFalse(opt.isPresent());
+    }
+
+    @Test
+    void givenAnEmptyOptional_thenIsEmptyBehavesAsExpected() {
+        Optional<String> opt = Optional.of("Baeldung");
+        Assert.assertFalse(opt.isEmpty());
+
+        opt = Optional.ofNullable(null);
+        Assert.assertTrue(opt.isEmpty());
+    }
+
+    @Test
+    void givenOptional_whenIfPresentWorks_thenCorrect() {
+        Optional<String> opt = Optional.of("baeldung");
+        opt.ifPresent(name -> System.out.println(name.length()));
+    }
+
+    @Test
+    public void whenOrElseWorks_thenCorrect() {
+        String nullName = null;
+        String name = Optional.ofNullable(nullName).orElse("john");
+        Assert.assertEquals("john", name);
+    }
+
+    @Test
+    public void whenOrElseGetWorks_thenCorrect() {
+        String nullName = null;
+        String name = Optional.ofNullable(nullName).orElseGet(() -> "john");
+        Assert.assertEquals("john", name);
+    }
+
+    @Test
+    public void whenOrElseGetAndOrElseOverlap_thenCorrect() {
+        String text = null;
+
+        String defaultText = Optional.ofNullable(text).orElseGet(this::getMyDefault);
+        Assert.assertEquals("Default Value", defaultText);
+
+        defaultText = Optional.ofNullable(text).orElse(getMyDefault());
+        Assert.assertEquals("Default Value", defaultText);
+    }
+
+    @Test
+    public void whenOrElseGetAndOrElseDiffer_thenCorrect() {
+        String text = "Text present";
+
+        System.out.println("Using orElseGet:");
+        String defaultText
+                = Optional.ofNullable(text).orElseGet(this::getMyDefault);
+        Assert.assertEquals("Text present", defaultText);
+
+        System.out.println("Using orElse:");
+        defaultText = Optional.ofNullable(text).orElse(getMyDefault());
+        Assert.assertEquals("Text present", defaultText);
     }
 }
