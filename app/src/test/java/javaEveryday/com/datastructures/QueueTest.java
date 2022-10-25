fd97491090b295f242db03db820427767ad00025
mc227 <markcostales.mark@gmail.com>
2022-10-25 18:21:48 -0400

    not sure'

--- app/src/test/java/javaEveryday/com/datastructures/QueueTest.java
+++ app/src/test/java/javaEveryday/com/datastructures/QueueTest.java
@@ -27,4 +27,26 @@ class QueueTest {
             Optional.of(name);
         });
     }
+
+    @Test
+    void givenNonNull_whenCreatesNullable_thenCorrect() {
+        String name = "Baeldung";
+        Optional<String> opt = Optional.ofNullable(name);
+        assertTrue(opt.isPresent());
+    }
+
+    @Test
+    void givenOptional_whenIsPresentWorks_thenCorrect() {
+        Optional<String> opt = Optional.of("Baeldung");
+        assertTrue(opt.isPresent());
+
+        opt = Optional.ofNullable(null);
+        assertFalse(opt.isPresent());
+    }
+
+    @Test
+    public void givenOptional_whenIfPresentWorks_thenCorrect() {
+        Optional<String> opt = Optional.of("baeldung");
+        opt.ifPresent(name -> System.out.println(name.length()));
+    }
 }
