dd88264cd24efc18f2616ffc341729526096c2c0
mc227 <markcostales.mark@gmail.com>
2022-10-25 18:20:52 -0400

    not sure

---
+++ app/src/main/java/javaEveryday/com/random/walaLang/Solution.java
@@ -0,0 +1,9 @@
+package javaEveryday.com.random.walaLang;
+
+public class Solution {
+    public static void main(String[] args) {
+        for(int i = 0; i <args.length;i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
