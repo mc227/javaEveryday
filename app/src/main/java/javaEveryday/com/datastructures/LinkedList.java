0ebbae8e8e3ab47c2fd0d7ca49af50ca8c760a73
mc227 <markcostales.mark@gmail.com>
2022-10-30 22:23:16 -0400

    LinkedList except for the mistake in addFront method

--- app/src/main/java/javaEveryday/com/datastructures/LinkedList.java
+++ app/src/main/java/javaEveryday/com/datastructures/LinkedList.java
@@ -25,4 +25,30 @@ public class LinkedList<V> {
     public void addAfter(LinkedListNode<V> aNode, V item) {
         aNode.setNext(new LinkedListNode<V>(item, aNode.getNext().orElse(null)));
     }
+
+    @Override
+    public String toString() {
+        Optional<LinkedListNode<V>> node = Optional.ofNullable(this.head);
+        StringBuffer result = new StringBuffer("[");
+        while(node.isPresent()){
+            node.ifPresent(n->result.append(n.getValue().toString()));
+            node = node.flatMap(LinkedListNode::getNext);
+            node.ifPresent(n->result.append(", "));
+        }
+        return result.append("]").toString();
+    }
+
+    /**
+     We have a linked list containing some elements and we need to build a string of the
+     form [3,6,4,2,4]. If the list is empty, it should output [].
+     */
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.addFront(4);
+        linkedList.addFront(2);
+        linkedList.addFront(4);
+        linkedList.addFront(6);
+        linkedList.addFront(3);
+        System.out.println(linkedList.toString());
+    }
 }
