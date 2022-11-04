aaff62a891c4fe89be49b42578f5d67a75cc5fd3
mc227 <markcostales.mark@gmail.com>
2022-11-04 06:34:26 -0400

    stack

--- app/src/main/java/javaEveryday/com/datastructures/Queue.java
+++ app/src/main/java/javaEveryday/com/datastructures/Queue.java
@@ -1,5 +1,7 @@
 package javaEveryday.com.datastructures;
 
+import java.util.Optional;
+
 public class Queue<V> {
     private DblLinkedListNode<V> head;
     private DblLinkedListNode<V> tail;
@@ -18,4 +20,25 @@ public class Queue<V> {
             return node.value
         return
      * */
+    public Optional<V> dequeue(){
+        Optional<DblLinkedListNode<V>> node = Optional.ofNullable(head);
+        head = node.flatMap(DblLinkedListNode::getNext).orElse(null);
+        Optional.ofNullable(head).ifPresent(n->n.setPrevious(null));
+        if(head==null) tail = null;
+        return node.map(DblLinkedListNode::getValue);
+    }
+    /**
+     enqueue(tail, item)
+        node = new Node(item)
+        node.previous = tail
+        if(tail !=null) tail.next = node
+        if(head!=null) head = node
+        tail = node
+     * */
+    public void enqueue(V item) {
+        DblLinkedListNode<V> node = new DblLinkedListNode<V>(item, null, tail);
+        Optional.ofNullable(tail).ifPresent(n->n.setNext(node));
+        tail = node;
+        if(head==null) head = node;
+    }
 }
