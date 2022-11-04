91c546a2a5f5ea7ffda3c6e85a9f3f9f650da91a
mc227 <markcostales.mark@gmail.com>
2022-11-04 06:51:30 -0400

    passed the dequeue and enqueue pseudo code

--- app/src/main/java/javaEveryday/com/datastructures/Queue.java
+++ app/src/main/java/javaEveryday/com/datastructures/Queue.java
@@ -1,7 +1,5 @@
 package javaEveryday.com.datastructures;
 
-import java.util.Optional;
-
 public class Queue<V> {
     private DblLinkedListNode<V> head;
     private DblLinkedListNode<V> tail;
@@ -18,27 +16,15 @@ public class Queue<V> {
             head = head.next
             if(head!=null) head.previous = null
             return node.value
-        return
+        return null
      * */
-    public Optional<V> dequeue(){
-        Optional<DblLinkedListNode<V>> node = Optional.ofNullable(head);
-        head = node.flatMap(DblLinkedListNode::getNext).orElse(null);
-        Optional.ofNullable(head).ifPresent(n->n.setPrevious(null));
-        if(head==null) tail = null;
-        return node.map(DblLinkedListNode::getValue);
-    }
+
     /**
      enqueue(tail, item)
         node = new Node(item)
         node.previous = tail
-        if(tail !=null) tail.next = node
-        if(head!=null) head = node
+        if (tail != null) tail.next = node
+        if (head != null) head = node
         tail = node
      * */
-    public void enqueue(V item) {
-        DblLinkedListNode<V> node = new DblLinkedListNode<V>(item, null, tail);
-        Optional.ofNullable(tail).ifPresent(n->n.setNext(node));
-        tail = node;
-        if(head==null) head = node;
-    }
 }
