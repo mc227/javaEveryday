d3a31e169d7313339734ca9d48c0308f300bc376
mc227 <markcostales.mark@gmail.com>
2022-11-06 10:28:10 -0500

    I think I'm good with Stack arrays

--- app/src/main/java/javaEveryday/com/datastructures/StackArray.java
+++ app/src/main/java/javaEveryday/com/datastructures/StackArray.java
@@ -1,11 +1,24 @@
 package javaEveryday.com.datastructures;
 
+import java.util.Optional;
+
 public class StackArray<V> {
     private V[] array;
     private int headPtr = 0;
 
+    // Implementing a Stack using an array involves first initializing an empty array
+    // with a fixed size.
     public StackArray(int capacity) {
         array = (V[]) new Object[capacity];
     }
+    //keep an index pointer at the top of the stack
+    public void push(V item) {
+        array[headPtr++] = item;
+    }
 
+    // we reduce this pointer by one and read the value
+    public Optional<V> pop(){
+        if(headPtr>0) return Optional.of(array[--headPtr]);
+        else return Optional.empty();
+    }
 }
