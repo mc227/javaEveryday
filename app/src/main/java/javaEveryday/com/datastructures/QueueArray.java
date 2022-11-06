7dd53d6ccac6f07151c18636238dd2079daa1dfe
mc227 <markcostales.mark@gmail.com>
2022-11-06 12:15:22 -0500

    from scratch correctly implemented enqueue and dequeue in QueueArray

--- app/src/main/java/javaEveryday/com/datastructures/QueueArray.java
+++ app/src/main/java/javaEveryday/com/datastructures/QueueArray.java
@@ -1,72 +1,40 @@
 package javaEveryday.com.datastructures;
 
-
-import java.util.Arrays;
 import java.util.Optional;
 
 public class QueueArray<V> {
-    private V[] array;
     private int headPtr = 0;
     private int tailPtr = 0;
-    private boolean full = false;
+    private V[] array;
 
     public QueueArray(int capacity) {
         array = (V[]) new Object[capacity];
     }
 
-    public void enqueue(V item) {
+    /**
+     * Notice that when we enqueue an item,
+     * we place it at the tail position and
+     * increment the tail pointer using the
+     * mod operator. When the pointer is
+     * larger or equal to the size of the
+     * array, it wraps around and starts
+     * again from zero. The same happens
+     * on the dequeue method, where we
+     * access and increment the head
+     * pointer in a similar fashion.
+     */
+    public void enqueue(V item){
         array[tailPtr] = item;
         tailPtr = (tailPtr + 1) % array.length;
     }
 
-    public Optional<V> dequeue() {
-        if (headPtr != tailPtr) {
+    public Optional<V> dequeue(){
+        if(headPtr != tailPtr) {
             Optional<V> item = Optional.of(array[headPtr]);
-            headPtr = (headPtr + 1) % array.length;
+            headPtr = (headPtr+1) % array.length;
             return item;
-        } else return Optional.empty();
-    }
-
-
-    public static void main(String[] args) {
-        QueueArray<String> q = new QueueArray<>(5);
-
-        q.enqueue("one");
-        System.out.println(q.toString());
-        q.enqueue("two");
-        System.out.println(q.toString());
-        q.enqueue("three");
-        System.out.println(q.toString());
-        q.enqueue("four");
-        System.out.println(q.toString());
-        q.enqueue("five");
-        System.out.println(q.toString());
-        q.enqueue("six");
-        System.out.println(q.toString());
-
-        System.out.println(q.dequeue());
-        System.out.println(q.toString());
-        System.out.println(q.dequeue());
-        System.out.println(q.toString());
-        System.out.println(q.dequeue());
-        System.out.println(q.toString());
-        System.out.println(q.dequeue());
-        System.out.println(q.toString());
-        System.out.println(q.dequeue());
-        System.out.println(q.toString());
-        System.out.println(q.dequeue());
-        System.out.println(q.toString());
-
-
-    }
-
-    @Override
-    public String toString() {
-        return "QueueArray{" +
-                "array=" + Arrays.toString(array) +
-                ", headPtr=" + headPtr +
-                ", tailPtr=" + tailPtr +
-                ", full=" + full +
-                '}';
+        } else {
+            return Optional.empty();
+        }
     }
 }
--- app/src/main/java/javaEveryday/com/datastructures/StackArray.java
+++ app/src/main/java/javaEveryday/com/datastructures/StackArray.java
@@ -3,20 +3,30 @@ package javaEveryday.com.datastructures;
 import java.util.Optional;
 
 public class StackArray<V> {
+    /**
+     * Implementing a stack using
+     * an array involves first
+     * initializing an empty
+     * array with a fixed size.
+     * Then, it's a matter of
+     * keeping an index pointer
+     * to the top of the stack,
+     * initially pointing to zero.
+     * As we push items on the stack,
+     * we place the item at this index
+     * and increment the pointer by one.
+     * When we need to pop an element,
+     * we reduce this pointer by one and read the value.
+     */
     private V[] array;
     private int headPtr = 0;
 
-    // Implementing a Stack using an array involves first initializing an empty array
-    // with a fixed size.
     public StackArray(int capacity) {
         array = (V[]) new Object[capacity];
     }
-    //keep an index pointer at the top of the stack
     public void push(V item) {
         array[headPtr++] = item;
     }
-
-    // we reduce this pointer by one and read the value
     public Optional<V> pop(){
         if(headPtr>0) return Optional.of(array[--headPtr]);
         else return Optional.empty();
