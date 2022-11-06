28e1d158b65d6cb52ea6ad45fffe2dd5719a152f
mc227 <markcostales.mark@gmail.com>
2022-11-05 20:27:34 -0400

    i got the String reverse method correct

--- app/src/main/java/javaEveryday/com/datastructures/Stack.java
+++ app/src/main/java/javaEveryday/com/datastructures/Stack.java
@@ -14,4 +14,35 @@ public class Stack<V> {
         head = node.flatMap(LinkedListNode::getNext).orElse(null);
         return node.map(LinkedListNode::getValue);
     }
+    /**
+     * Reversing a String
+     * We need to make use of a stack data structure for reversing a string.
+     *
+     * Follow these steps:
+     *
+     * To reverse the string,
+     * push of each character of the input string and then pop everything out, one at a time,
+     * building a reversed string. The method signature can be as follows:
+     * public String reverse(String str)
+     * The following code shows how a string can be reversed using the stack data structure:
+     */
+    public String reverse(String str) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        for(char i: str.toCharArray()){
+            stack.push(i);
+        }
+        Optional<Character> optChar = stack.pop();
+        while(optChar.isPresent()){
+            result.append(optChar.get());
+            optChar = stack.pop();
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        System.out.println(stack.reverse("kraM"));
+
+    }
 }
