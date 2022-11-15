337901e0b3d27015430edd2f8cedeb0609f86300
mc227 <markcostales.mark@gmail.com>
2022-11-14 22:50:48 -0500

    passed my test for get and put for SimpleBinaryTree

--- app/src/main/java/javaEveryday/com/datastructures/SimpleBinaryTree.java
+++ app/src/main/java/javaEveryday/com/datastructures/SimpleBinaryTree.java
@@ -7,25 +7,25 @@ public class SimpleBinaryTree<K,V> implements BinaryTree<K,V> {
 
     public void put(K key, V value) {
         if(root == null) {
-            root = new BinaryTreeNode<>(key,value);
+            root = new BinaryTreeNode<>(key, value);
         } else {
-            put(key, value, root);
+            put(key,value,root);
         }
     }
 
     private void put(K key, V value, BinaryTreeNode<K,V> node) {
-        if(((Comparable) key).compareTo(node.getKey()) == 0) {
+        if(((Comparable) key).compareTo(node.getKey())==0){
             node.setKey(key);
             node.setValue(value);
-        } else if(((Comparable) key).compareTo(node.getKey()) < 0) {
-            if(node.getLeft().isPresent()) {
-                put(key,value,node.getLeft().get());
+        } else if(((Comparable) key).compareTo(node.getKey())<0){
+            if(node.getLeft().isPresent()){
+                put(key, value, node.getLeft().get());
             } else {
                 node.setLeft(new BinaryTreeNode<>(key,value));
             }
         } else {
-            if(node.getRight().isPresent()) {
-                put(key,value,node.getRight().get());
+            if(node.getRight().isPresent()){
+                put(key, value, node.getRight().get());
             } else {
                 node.setRight(new BinaryTreeNode<>(key,value));
             }
@@ -33,16 +33,16 @@ public class SimpleBinaryTree<K,V> implements BinaryTree<K,V> {
     }
 
     public Optional<V> get(K key) {
-        return Optional.ofNullable(root).flatMap(n -> get(key, n));
+        return Optional.ofNullable(root).flatMap(n->get(key,n));
     }
 
     private Optional<V> get(K key, BinaryTreeNode<K,V> node) {
-        if(((Comparable) key).compareTo(node.getKey()) == 0) {
+        if(((Comparable) key).compareTo(node.getKey())==0){
             return Optional.ofNullable(node.getValue());
-        } else if(((Comparable) key).compareTo(node.getKey()) < 0) {
-            return node.getLeft().flatMap(n -> get(key, n));
+        } else if(((Comparable) key).compareTo(node.getKey())<0){
+            return node.getLeft().flatMap(n->get(key,n));
         } else {
-            return node.getRight().flatMap(n -> get(key, n));
+            return node.getRight().flatMap(n->get(key,n));
         }
     }
 }
