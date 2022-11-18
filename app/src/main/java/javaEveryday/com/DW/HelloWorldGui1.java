ef98af8a831bcd5ef36bd134254ea6eb8b2ad191
mc227 <markcostales.mark@gmail.com>
2022-11-18 16:39:32 -0500

    endeavoring to master trees

---
+++ app/src/main/java/javaEveryday/com/DW/HelloWorldGui1.java
@@ -0,0 +1,9 @@
+package javaEveryday.com.DW;
+
+import javax.swing.*;
+
+public class HelloWorldGui1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Oh, Hi Mark!");
+    }
+}
