842bfece1cc0368d5d698313a9e2a03148413108
mc227 <markcostales.mark@gmail.com>
2022-11-21 22:06:07 -0500

    inordersuccessor 3 time

--- app/src/main/java/javaEveryday/com/datastructures/InOrderSuccessorBinaryTree.java
+++ app/src/main/java/javaEveryday/com/datastructures/InOrderSuccessorBinaryTree.java
@@ -6,24 +6,15 @@ public class InOrderSuccessorBinaryTree<K,V> extends SimpleBinaryTree<K,V> {
     public Optional<K> inOrderSuccessorKey(K key) {
         Optional<BinaryTreeNode<K,V>> node = Optional.ofNullable(root);
         Optional<K> successor = Optional.empty();
-        while(node.isPresent() && !node.get().getKey().equals(key)){
-            if(((Comparable) node.get().getKey()).compareTo(key) > 0) {
+        while(node.isPresent() && !node.get().getKey().equals(key)) {
+            if(((Comparable)node.get().getKey()).compareTo(key)>0){
                 successor = node.map(BinaryTreeNode::getKey);
                 node = node.flatMap(BinaryTreeNode::getLeft);
             } else {
-                node =node.flatMap(BinaryTreeNode::getRight);
+                node = node.flatMap(BinaryTreeNode::getRight);
             }
-            return node.flatMap(BinaryTreeNode::getRight).map(this::minKey).map(Optional::of).orElse(successor);
         }
-
-//    public static void main(String[] args) {
-//        InOrderSuccessorBinaryTree<Integer, String> binaryTree = new InOrderSuccessorBinaryTree<Integer, String>();
-//        binaryTree.put(1, "Isabel");
-//        binaryTree.put(2, "John");
-//        binaryTree.put(3, "Ruth");
-//        binaryTree.put(4, "Sarah");
-//        binaryTree.put(5, "Peter");
-//        binaryTree.put(6, "Tom");
-//        System.out.println(binaryTree.inOrderSuccessorKey(1));
-//    }
+        return node.flatMap(BinaryTreeNode::getRight).map(this::minKey)
+                .map(Optional::of).orElse(successor);
+    }
 }
