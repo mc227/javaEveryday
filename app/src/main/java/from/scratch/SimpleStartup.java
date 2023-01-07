c2458908d7484c7837fb68c8a3552846d295fa50
mc227 <markcostales.mark@gmail.com>
2023-01-07 07:12:15 -0500

    SimpleStartup prep code

--- app/src/main/java/from/scratch/SimpleStartup.java
+++ app/src/main/java/from/scratch/SimpleStartup.java
@@ -4,28 +4,26 @@ import java.util.Arrays;
 
 public class SimpleStartup {
     /**
-     DECLARE int array to represent the location of the cells. Call it locationCells
-     DECLARE int to represent the number of accurate hits. Call it numOfHits
-     DECLARE checkYourself function that takes in int as an input parameter.
-     DECLARE setLocation function that takes in int array as an input parameter
+     DECLARE int array called locationCells to hold the location of the startup
+     DECLARE int called numOfHits to hold the number of hits and set it equal to 0.
+     DECLARE checkYourself method that compares the user's guess to each item of the locationCells.
+     DECLARE setLocationCells that sets an int array and assigns it to the locationCells
 
      METHOD checkYourself(int guess)
-        GET user's guess
-        LOOP through the array
-            if user guess == current item in the array
+        GET user guess
+        LOOP through each item in locationcells int array
+            if guess == item
                 numOfHits++
-                if numOfHits == length(array)
+                if locationcell.length == numOfHits
                     return kill
                 else
                     return hit
             else
                 return miss
 
-     METHOD setLocation(int[] loc)
 
+     METHOD voidSetLocation(int[] loc)
+        set loc to locationCells
      */
-    public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7};
-        System.out.println(Arrays.toString(array));
-    }
+
 }
