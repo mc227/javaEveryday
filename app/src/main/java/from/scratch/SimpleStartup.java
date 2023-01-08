9c31f2df68d385cd3ae3d9ae41e9f823514e53fd
mc227 <markcostales.mark@gmail.com>
2023-01-08 04:49:38 -0500

    my implementation of the SimpleStartup class using TDD

--- app/src/main/java/from/scratch/SimpleStartup.java
+++ app/src/main/java/from/scratch/SimpleStartup.java
@@ -6,8 +6,10 @@ public class SimpleStartup {
     /**
      DECLARE int array called locationCells to hold the location of the startup
      DECLARE int called numOfHits to hold the number of hits and set it equal to 0.
-     DECLARE checkYourself method that compares the user's guess to each item of the locationCells.
-     DECLARE setLocationCells that sets an int array and assigns it to the locationCells
+     DECLARE checkYourself method that compares the user's guess to each
+     item of the locationCells.
+     DECLARE setLocationCells that sets an int array and assigns
+     it to the locationCells
 
      METHOD checkYourself(int guess)
         GET user guess
@@ -22,8 +24,27 @@ public class SimpleStartup {
                 return miss
 
 
-     METHOD voidSetLocation(int[] loc)
+     METHOD void setLocation(int[] loc)
         set loc to locationCells
      */
+    public int[] locationCells;
+    public int numOfHits = 0;
+
+    public String checkYourself(int guess) {
+        for(int item: locationCells) {
+            if(guess == item) {
+                numOfHits++;
+                if(numOfHits == locationCells.length) {
+                    return "kill";
+                } else {
+                    return "hit";
+                }
+            }
+        }
+        return "miss";
+    }
+    public void setLocation(int[] loc) {
+        locationCells = loc;
+    }
 
 }
--- app/src/test/java/from/scratch/SimpleStartupTestDrive.java
+++ app/src/test/java/from/scratch/SimpleStartupTestDrive.java
@@ -4,19 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleStartupTestDrive {
     /**
-     * Instantiate a SimpleStartup object.
+     * [x] Instantiate a SimpleStartup object.
      *
-     * Assign it a location (an array of 3 ints, like {2, 3, 4}).
+     * [x] Assign it a location (an array of 3 ints, like {2, 3, 4}).
      *
      * Create an int to represent a user guess (2, 0, etc.).
      *
      * Invoke the checkYourself() method passing it the fake user guess.
      *
-     * Print out the result to see if it’s correct (“passed” or “failed”).
+     * Print out the result to see if it’s correct
      */
     public static void main(String[] args) {
         // Instantiate a SimpleStartup object.
         SimpleStartup simpleStartup = new SimpleStartup();
+        // Assign it a location (an array of 3 ints, like {2, 3, 4}).
+        simpleStartup.setLocation(new int[]{2, 3, 4});
+        // Create an int to represent a user guess (2, 0, etc.).
+        int userGuess = 2;
+        // Invoke the checkYourself() method passing it the fake user guess.
+        String foo = simpleStartup.checkYourself(userGuess);
+        System.out.println(foo);
+        // Print out the result to see if it’s correct
+        if(foo.equals("hit")){
+            System.out.println("passed");
+        } else {
+            System.out.println("failed");
+        }
 
     }
 }
