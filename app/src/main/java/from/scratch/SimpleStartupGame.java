6b54370075eed3298fb2b808e581016bd36f0e3e
mc227 <markcostales.mark@gmail.com>
2023-01-08 05:18:54 -0500

    prep code for SimpleStartupGame provided by hfj3e. still incomplete

--- app/src/main/java/from/scratch/SimpleStartupGame.java
+++ app/src/main/java/from/scratch/SimpleStartupGame.java
@@ -1,4 +1,21 @@
 package from.scratch;
 
 public class SimpleStartupGame {
+    /**
+     PREP CODE
+     METHOD psvm
+     DECLARE an int variable to hold the number of guesses, named numOfGuesses
+     COMPUTE a random number between 0 and 4 that will be the starting position
+     of the cell
+     WHILE the startup is alive:
+     GET user input from the command line
+     Make the single SimpleStartup object.
+     Make a location for it
+     (three consecutive cells on a single row of seven virtual cells).
+     Ask the user for a guess.
+     Check the guess.
+     Repeat until the Startup is sunk.
+     Tell the user how many guesses it took.
+     */
+
 }
