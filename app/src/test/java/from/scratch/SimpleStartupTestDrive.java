92818b429a5878e519363119cd2b40efa50aa32a
mc227 <markcostales.mark@gmail.com>
2023-01-08 04:25:42 -0500

    moved SimpleStartupTestDrive to test folder

---
+++ app/src/test/java/from/scratch/SimpleStartupTestDrive.java
@@ -0,0 +1,22 @@
+package from.scratch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleStartupTestDrive {
+    /**
+     * Instantiate a SimpleStartup object.
+     *
+     * Assign it a location (an array of 3 ints, like {2, 3, 4}).
+     *
+     * Create an int to represent a user guess (2, 0, etc.).
+     *
+     * Invoke the checkYourself() method passing it the fake user guess.
+     *
+     * Print out the result to see if it’s correct (“passed” or “failed”).
+     */
+    public static void main(String[] args) {
+        // Instantiate a SimpleStartup object.
+        SimpleStartup simpleStartup = new SimpleStartup();
+
+    }
+}
