595b6e03080e29cc20db3bfc85b099bcc64cfb85
mc227 <markcostales.mark@gmail.com>
2023-01-20 12:46:27 -0500

    need a better name for the test

--- app/src/main/java/projects/project1/StartUp.java
+++ app/src/main/java/projects/project1/StartUp.java
@@ -1,4 +1,11 @@
 package projects.project1;
 
+import java.util.ArrayList;
+
 public class StartUp {
+    ArrayList<Integer> location;
+
+    public void setLocation(ArrayList<Integer> location) {
+        this.location = location;
+    }
 }
---
+++ app/src/test/java/projects/project1/StartUpTest.java
@@ -0,0 +1,17 @@
+package projects.project1;
+
+import from.scratch.hfj3e.ch6.Startup;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartUpTest {
+    @Test
+    void firstTest() {
+        // initialize StartUp object
+        StartUp startUp = new StartUp();
+        // give it a location
+        // get user input
+        // check if user input is correct
+    }
+}
