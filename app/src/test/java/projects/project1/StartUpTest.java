9a93f6be8cb57654758c0346840ce0faf7484cfe
mc227 <markcostales.mark@gmail.com>
2023-01-21 19:43:38 -0500

    created secondTest using assertEquals

--- app/src/test/java/projects/project1/StartUpTest.java
+++ app/src/test/java/projects/project1/StartUpTest.java
@@ -22,4 +22,21 @@ class StartUpTest {
         System.out.println(startUp.checkYourself(1));
         System.out.println(startUp.checkYourself(2));
     }
+
+    @Test
+    void secondTest() {
+        // initialize StartUp object
+        StartUp startUp = new StartUp();
+        // give startUp a location
+        ArrayList<Integer> location = new ArrayList<Integer>(
+                Arrays.asList(3,4,5));
+        startUp.setLocation(location);
+        // get user input and check
+//        System.out.println(startUp.checkYourself(3));
+        assertEquals(startUp.checkYourself(3),"hit");
+//        System.out.println(startUp.checkYourself(4));
+        assertEquals(startUp.checkYourself(4),"hit");
+//        System.out.println(startUp.checkYourself(5));
+        assertEquals(startUp.checkYourself(5),"kill");
+    }
 }
