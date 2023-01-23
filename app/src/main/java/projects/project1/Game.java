4aba2c93cc39b7c28dc25913f4be93d74e3a5289
mc227 <markcostales.mark@gmail.com>
2023-01-22 20:18:22 -0500

    used a BufferedReader instead of a scanner in GameHelper class

--- app/src/main/java/projects/project1/Game.java
+++ app/src/main/java/projects/project1/Game.java
@@ -2,6 +2,15 @@ package projects.project1;
 
 public class Game {
     public static void main(String[] args) {
-
+        // Create a start up
+        StartUp startUp = new StartUp();
+        // give it a random location
+        // i need a random number between 0 and 4
+        // and then i will have to add 2 more positions, as in i + 1 and i + 2
+        // set the start up location
+        // ask user for a guess
+        // keep asking a user for a guess until the kill
+        // a while loop
+        // then i have to
     }
 }
--- app/src/main/java/projects/project1/GameHelper.java
+++ app/src/main/java/projects/project1/GameHelper.java
@@ -1,4 +1,15 @@
 package projects.project1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class GameHelper {
+    public int getUserInput(String prompt) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(bufferedReader.readLine());
+
+
+    }
 }
