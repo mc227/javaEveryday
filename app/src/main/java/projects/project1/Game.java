09fb3ae75933cb3b12fd53c049973cab2080cefd
mc227 <markcostales.mark@gmail.com>
2023-01-23 20:27:55 -0500

    I'm trying to do it without the stillAlive boolean

--- app/src/main/java/projects/project1/Game.java
+++ app/src/main/java/projects/project1/Game.java
@@ -10,14 +10,13 @@ public class Game {
         // Create a start up
         StartUp startUp = new StartUp();
         GameHelper gameHelper = new GameHelper();
-        Boolean stillAlive = true;
         // give it a random location
         Random rn = new Random();
         // i need a random number between 0 and 4
         int start = rn.nextInt(4 + 1);
         ArrayList<Integer> location = new ArrayList<>(Arrays.asList(start, start+1, start+2));
         startUp.setLocation(location);
-        while(stillAlive) {
+        while(!startUp.getLocation().isEmpty()) {
             System.out.println(startUp.getLocation());
             // ask user for a guess
             int userGuess = gameHelper.getUserInput("Please enter your guess: ");
