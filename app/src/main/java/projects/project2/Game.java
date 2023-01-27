dfdf0b548ac9d7ceabab007bfe05962e2ae81abd
mc227 <markcostales.mark@gmail.com>
2023-01-26 22:54:41 -0500

    I'm impressed with how the location producing method was built

--- app/src/main/java/projects/project2/Game.java
+++ app/src/main/java/projects/project2/Game.java
@@ -2,8 +2,20 @@ package projects.project2;
 
 public class Game {
     public static void main(String[] args) {
-        // create three startups
-        // cabista, poniez, hacqi
+        // create three startups (cabista, poniez, hacqi)
+        GameHelper helper = new GameHelper();
+        StartUp first = new StartUp();
+        StartUp second = new StartUp();
+        StartUp third = new StartUp();
+        first.setName("cabista");
+        first.setLocation(helper.placeStartup(3));
+        second.setName("poniez");
+        second.setLocation(helper.placeStartup(3));
+        third.setName("hacqi");
+        third.setLocation(helper.placeStartup(3));
+        System.out.println(first.getLocation());
+        System.out.println(second.getLocation());
+        System.out.println(third.getLocation());
 
         // give them all names
         // assign locations
