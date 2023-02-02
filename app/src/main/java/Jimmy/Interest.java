582991c63ad200482fed19339d6c3ca2cc3db4e5
mc227 <markcostales.mark@gmail.com>
2023-02-02 09:49:12 -0500

    Check each user guess with all three Startups, instead of just one.

--- app/src/main/java/Jimmy/Interest.java
+++ app/src/main/java/Jimmy/Interest.java
@@ -1,24 +1,29 @@
 package Jimmy;
 
-/**
- * This class calculates the interest and total amounts
- */
 public class Interest {
     public static void main(String[] args) {
-        // here are my attributes
+        /**
+         * declare variables
+         */
         double principal;
+        double rate;
         double interest;
-        double amount;
 
+        /**
+         * do calculations
+         */
         principal = 17000;
-        interest = 0.027;
-        amount = principal * interest; // this is where the interest in dollars is calculated
-        principal = principal + amount; // this is the total amount at the end of the year
-        System.out.print("The interest earned is $");
-        System.out.println(amount);
-        System.out.print("The total at the end of the year is $");
-        System.out.println(principal);
+        rate = 0.027;
+        interest = principal * rate;
+        principal = principal + interest;
 
+        /**
+         * display results
+         */
+        System.out.print("THe interest earned is $");
+        System.out.println(interest);
+        System.out.print("THe total is $");
+        System.out.println(principal);
 
 
     }
--- app/src/main/java/projects/project2/Game.java
+++ app/src/main/java/projects/project2/Game.java
@@ -1,23 +1,37 @@
 package projects.project2;
 
+import java.util.ArrayList;
+
 public class Game {
     public static void main(String[] args) {
-        // create three startups (cabista, poniez, hacqi)
+        // create three startups (cabista, poniez, hacqi) instead of one.
         GameHelper helper = new GameHelper();
+        ArrayList<StartUp> startups = new ArrayList<>();
         StartUp first = new StartUp();
         StartUp second = new StartUp();
         StartUp third = new StartUp();
+        startups.add(first);
+        startups.add(second);
+        startups.add(third);
+        // Give each of the three Startups a name.
         first.setName("cabista");
-        first.setLocation(helper.placeStartup(3));
         second.setName("poniez");
-        second.setLocation(helper.placeStartup(3));
         third.setName("hacqi");
+        // Put the Startups on a grid rather than just a single row, and do it for all three Startups.
+        first.setLocation(helper.placeStartup(3));
+        second.setLocation(helper.placeStartup(3));
         third.setLocation(helper.placeStartup(3));
+        /**
+         * delete below when shipping to production
+         */
         System.out.println(first.getLocation());
         System.out.println(second.getLocation());
         System.out.println(third.getLocation());
-
-        // give them all names
-        // assign locations
+        /***/
+        // i need to keep asking guesses until
+//        String guess = helper.getUserInput("please enter your guess");
+        /**
+         * Check each user guess with all three Startups, instead of just one.
+         */
     }
 }
