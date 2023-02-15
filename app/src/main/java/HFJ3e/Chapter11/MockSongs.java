40f98b4b18e79e4f76ff47e4d2f22aaede0c7d58
mc227 <markcostales.mark@gmail.com>
2023-02-14 19:59:49 -0500

    Generics means more type-safety

--- app/src/main/java/HFJ3e/Chapter11/MockSongs.java
+++ app/src/main/java/HFJ3e/Chapter11/MockSongs.java
@@ -14,4 +14,15 @@ public class MockSongs {
         songs.add("50 Ways");
         return songs;
     }
+
+    public static List<SongV2> getSongsV2() {
+        List<SongV2> songs = new ArrayList<>();
+        songs.add(new SongV2("somersault", "zero 7", 147));
+        songs.add(new SongV2("cassidy", "grateful dead", 158));
+        songs.add(new SongV2("$10", "hitchhiker", 140));
+        songs.add(new SongV2("havana", "cabello",105));
+        songs.add(new SongV2("Cassidy", "grateful dead", 158));
+        songs.add(new SongV2("50 Ways", "simon", 102));
+        return songs;
+    }
 }
