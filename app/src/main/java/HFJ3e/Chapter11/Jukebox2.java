743590c591f5d634f15b7645ff1bc4c2c2fde9fc
mc227 <markcostales.mark@gmail.com>
2023-02-16 22:13:47 -0500

    fixed bug

--- app/src/main/java/HFJ3e/Chapter11/Jukebox2.java
+++ app/src/main/java/HFJ3e/Chapter11/Jukebox2.java
@@ -4,14 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Jukebox2 {
-    public static void main(String[] args) {
-        new Jukebox2().go();
-    }
-
-    public void go() {
-        List<SongV2> songList = MockSongs.getSongsV2();
-        System.out.println(songList);
-        Collections.sort(songList);
-        System.out.println(songList);
-    }
+//    public static void main(String[] args) {
+//        new Jukebox2().go();
+//    }
+//
+//    public void go() {
+//        List<SongV2> songList = MockSongs.getSongsV2();
+//        System.out.println(songList);
+//        Collections.sort(songList);
+//        System.out.println(songList);
+//    }
 }
