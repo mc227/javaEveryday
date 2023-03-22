d1d1dc3748e3af98055da247205e5e42b1872f52
mc227 <markcostales.mark@gmail.com>
2023-03-21 22:24:12 -0400

    finished ch 17

--- app/src/main/java/HFJ3e/Chapter15/Button1.java
+++ app/src/main/java/HFJ3e/Chapter15/Button1.java
@@ -12,21 +12,25 @@ public class Button1 {
     public void go() {
         JFrame frame = new JFrame();
 
-//    JButton east = new JButton("East");
-//    JButton west = new JButton("West");
-//    JButton north = new JButton("North");
-//    JButton south = new JButton("South");
-//    JButton center = new JButton("Center");
+    JButton east = new JButton("East");
+    JButton west = new JButton("West");
+    JButton north = new JButton("North");
+    JButton south = new JButton("South");
+    JButton center = new JButton("Center");
 
-//    frame.getContentPane().add(BorderLayout.EAST, east);
-//    frame.getContentPane().add(BorderLayout.WEST, west);
-//    frame.getContentPane().add(BorderLayout.NORTH, north);
-//    frame.getContentPane().add(BorderLayout.SOUTH, south);
-//    frame.getContentPane().add(BorderLayout.CENTER, center);
+    frame.getContentPane().add(BorderLayout.EAST, east);
+    frame.getContentPane().add(BorderLayout.WEST, west);
+    frame.getContentPane().add(BorderLayout.NORTH, north);
+    frame.getContentPane().add(BorderLayout.SOUTH, south);
+    frame.getContentPane().add(BorderLayout.CENTER, center);
 
-        JButton button = new JButton("click like you mean it");
-        frame.getContentPane().add(BorderLayout.EAST,button);
-        frame.setSize(200, 200);
+//        JButton button = new JButton("click like you mean it");
+//        JButton button = new JButton("There is no spoon...");
+//        JButton button = new JButton("Click This!");
+//        Font bigFont = new Font("serif",Font.BOLD, 28);
+//        button.setFont(bigFont);
+//        frame.getContentPane().add(BorderLayout.NORTH,button);
+        frame.setSize(300, 300);
         frame.setVisible(true);
     }
 }
---
+++ app/src/main/java/HFJ3e/Chapter16/QuizCard.java
@@ -0,0 +1,19 @@
+package HFJ3e.Chapter16;
+
+public class QuizCard {
+    private final String question;
+    private final String answer;
+
+    public QuizCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
